4fa2ec22b525ea8b0a553b1643a6e059da3e6180
RedPlus <redplus@redplus.net>
2020-10-22 16:54:04 +0900

    Notices Table Add

---
+++ Hawaso.SqlServer/dbo/Tables/Notices/00_Notices.sql
@@ -0,0 +1,15 @@
+﻿--[1] Table: Notices(공지사항) 테이블
+CREATE TABLE [dbo].[Notices]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity(1, 1),		-- Serial Number
+	[ParentId] Int Null,								-- ParentId, AppId, SiteId, ...
+	[Name] NVarChar(255) Not Null,						-- 작성자
+	[Title] NVarChar(255) Null,							-- 제목
+	[Content] NVarChar(Max) Null,						-- 내용
+	[IsPinned] Bit Null Default(0),						-- 공지글로 올리기 
+	[CreatedBy] NVarChar(255) Null,						-- 등록자(Creator)
+	[Created] DateTime Default(GetDate()) Null,  		-- 생성일(PostDate)
+	[ModifiedBy] NVarChar(255) Null,					-- 수정자(LastModifiedBy)
+	[Modified] DateTime Null,							-- 수정일(LastModified)
+)
+Go
