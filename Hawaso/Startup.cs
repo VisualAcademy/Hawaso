12f92f478d816132b207730ef5ae8fbee184a029
RedPlus <redplus@redplus.net>
2021-02-22 06:45:48 +0900

    Get User-Agent

--- Hawaso/Startup.cs
+++ Hawaso/Startup.cs
@@ -41,6 +41,9 @@ namespace Hawaso
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddHttpContextAccessor(); //[1]
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
