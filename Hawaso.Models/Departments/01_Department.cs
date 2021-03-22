91c1c1b3a4e3eacd0ba854f17f2dde7f3792c252
RedPlus <redplus@redplus.net>
2021-03-22 14:01:57 +0900

    Departments

---
+++ Hawaso.Models/Departments/01_Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hawaso.Models.Departments
+{
+    [Table("Departments")]
+    public class Department
+    {
+        public long Id { get; set; }
+        public bool? Active { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Hawaso.SqlServer/Departments/Departments.sql
@@ -0,0 +1,8 @@
+﻿-- 부서
+CREATE TABLE [dbo].[Departments] (
+    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL Primary Key,
+    [Active]    BIT                DEFAULT ((1)) NOT NULL,
+    [CreatedAt] DATETIMEOFFSET (7) NOT NULL,
+    [CreatedBy] NVARCHAR (70)      NULL,
+    [Name]      NVARCHAR (MAX)     NULL
+);
