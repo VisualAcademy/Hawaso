3726480249d967f26f36b488c85071856f14c417
RedPlus <redplus@redplus.net>
2021-03-24 01:26:57 +0900

    Departments Code Cleanup

--- Hawaso.Models/Departments/01_Department.cs
+++ Hawaso.Models/Departments/01_Department.cs
@@ -10,9 +10,11 @@ namespace Hawaso.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+
         public bool? Active { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public string CreatedBy { get; set; }
+
         public string Name { get; set; }
     }
 }
