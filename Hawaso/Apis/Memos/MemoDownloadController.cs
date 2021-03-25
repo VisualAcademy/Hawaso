8b5b9d2365df4b5e5122bef71d21a15ee86a4021
RedPlus <redplus@redplus.net>
2021-03-25 11:59:30 +0900

    Code Cleanup

--- Hawaso/Apis/Memos/MemoDownloadController.cs
+++ Hawaso/Apis/Memos/MemoDownloadController.cs
@@ -69,7 +69,7 @@ namespace Hawaso.Controllers
                     var worksheet = package.Workbook.Worksheets.Add("Memos");
 
                     var tableBody = worksheet.Cells["B2:B2"].LoadFromCollection(
-                        (from m in models select new { m.Created, m.Name, m.Title, m.DownCount, m.FileName })
+                        (from m in models select new { m.Id, Created = m.Created?.LocalDateTime.ToString(), m.Name, m.Title, m.DownCount, m.FileName })
                         , true);
 
                     var uploadCol = tableBody.Offset(1, 1, models.Count, 1);
