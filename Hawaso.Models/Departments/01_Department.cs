e5dee2a6ed9e1a0572518cd0b9b03844b3019117
RedPlus <redplus@redplus.net>
2021-08-07 23:54:12 +0900

    Code Cleanup

--- Hawaso.Models/Departments/01_Department.cs
+++ Hawaso.Models/Departments/01_Department.cs
@@ -4,6 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hawaso.Models
 {
+    /// <summary>
+    /// [1][1] 테이블과 일대일로 매핑되는 모델 클래스
+    /// </summary>
     [Table("Departments")]
     public class DepartmentModel
     {
--- Hawaso.SqlServer/Departments/Departments.sql
+++ Hawaso.SqlServer/Departments/Departments.sql
@@ -1,5 +1,6 @@
-﻿-- 부서
-CREATE TABLE [dbo].[Departments] (
+﻿--[0][0] 부서
+CREATE TABLE [dbo].[Departments] 
+(
     [Id]        BIGINT             IDENTITY (1, 1) NOT NULL Primary Key,
     [Active]    BIT                DEFAULT ((1)) NOT NULL,
     [CreatedAt] DATETIMEOFFSET (7) NOT NULL,
