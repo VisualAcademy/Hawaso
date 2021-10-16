23d1a9bf16b35f80c6208ca7a4ad1f675bf06803
Park YongJune <redplus@redplus.net>
2021-10-16 13:51:12 +0900

    Code Cleanup

--- Hawaso/Models/ApplicationRole.cs
+++ Hawaso/Models/ApplicationRole.cs
@@ -4,6 +4,8 @@ namespace Hawaso.Models
 {
     /// <summary>
     /// 인증 기능
+    /// 역할 기반 인증을 사용할 때에는 기본 클래스인 IdentityRole을 상속한
+    /// ApplicationRole에 역할(그릅) 설명 등과 같은 추가 속성들 제공
     /// </summary>
     public class ApplicationRole : IdentityRole
     {
