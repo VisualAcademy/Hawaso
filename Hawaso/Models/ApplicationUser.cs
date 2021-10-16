3c88ce02a32a7d8364d8d22b9fb75715dfa24db9
Park YongJune <redplus@redplus.net>
2021-10-16 13:50:12 +0900

    Code Cleanup

--- Hawaso/Models/ApplicationUser.cs
+++ Hawaso/Models/ApplicationUser.cs
@@ -4,6 +4,8 @@ namespace Hawaso.Models
 {
     /// <summary>
     /// 인증 기능
+    /// Blazor Server 기본 템플릿에는 IdentityUser로 로그인하지만,
+    /// 추가적인 정보를 저장할 때에는 ApplicationUser로 속성들을 추가해서 사용
     /// </summary>
     public class ApplicationUser : IdentityUser
     {
