32db826d77eed5eb939b7d3d81070754c3094c96
Park YongJune <redplus@redplus.net>
2021-11-28 16:08:40 +0900

    Code Cleanup

--- Hawaso/Apis/Memos/ArchiveDownloadController.cs
+++ Hawaso/Apis/Memos/ArchiveDownloadController.cs
@@ -8,95 +8,94 @@ using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Hawaso.Controllers
+namespace Hawaso.Controllers;
+
+[Authorize]
+public class ArchiveDownloadController : Controller
 {
-    [Authorize]
-    public class ArchiveDownloadController : Controller
+    private readonly IArchiveRepository _repository;
+    private readonly IArchiveFileStorageManager _fileStorageManager;
+
+    public ArchiveDownloadController(IArchiveRepository repository, IArchiveFileStorageManager fileStorageManager)
     {
-        private readonly IArchiveRepository _repository;
-        private readonly IArchiveFileStorageManager _fileStorageManager;
+        _repository = repository;
+        _fileStorageManager = fileStorageManager;
+    }
 
-        public ArchiveDownloadController(IArchiveRepository repository, IArchiveFileStorageManager fileStorageManager)
+    /// <summary>
+    /// 게시판 파일 강제 다운로드 기능(/BoardDown/:Id)
+    /// </summary>
+    public async Task<IActionResult> FileDown(int id)
+    {
+        var model = await _repository.GetByIdAsync(id);
+
+        if (model == null)
         {
-            _repository = repository;
-            _fileStorageManager = fileStorageManager;
+            return null;
         }
-
-        /// <summary>
-        /// 게시판 파일 강제 다운로드 기능(/BoardDown/:Id)
-        /// </summary>
-        public async Task<IActionResult> FileDown(int id)
+        else
         {
-            var model = await _repository.GetByIdAsync(id);
-
-            if (model == null)
+            if (!string.IsNullOrEmpty(model.FileName))
             {
-                return null;
-            }
-            else
-            {
-                if (!string.IsNullOrEmpty(model.FileName))
+                byte[] fileBytes = await _fileStorageManager.DownloadAsync(model.FileName, "Archives");
+                if (fileBytes != null)
                 {
-                    byte[] fileBytes = await _fileStorageManager.DownloadAsync(model.FileName, "Archives");
-                    if (fileBytes != null)
-                    {
-                        model.DownCount = model.DownCount + 1;
-                        await _repository.EditAsync(model);
+                    model.DownCount = model.DownCount + 1;
+                    await _repository.EditAsync(model);
 
-                        return File(fileBytes, "application/octet-stream", model.FileName);
-                    }
-                    else
-                    {
-                        return Redirect("/");
-                    }
+                    return File(fileBytes, "application/octet-stream", model.FileName);
                 }
-
-                return Redirect("/");
-            }
-        }
-
-        /// <summary>
-        /// 엑셀 파일 강제 다운로드 기능(/ExcelDown)
-        /// </summary>
-        public async Task<IActionResult> ExcelDown()
-        {
-            var results = await _repository.GetAllAsync(0, 100);
-
-            var models = results.Records.ToList();
-
-            if (models != null)
-            {
-                using (var package = new ExcelPackage())
+                else
                 {
-                    var worksheet = package.Workbook.Worksheets.Add("Archives");
-
-                    var tableBody = worksheet.Cells["B2:B2"].LoadFromCollection(
-                        (from m in models select new { m.Id, Created = m.Created?.LocalDateTime.ToString(), m.Name, m.Title, m.DownCount, m.FileName })
-                        , true);
-
-                    var uploadCol = tableBody.Offset(1, 1, models.Count, 1);
-
-                    // 그라데이션 효과 부여 
-                    var rule = uploadCol.ConditionalFormatting.AddThreeColorScale();
-                    rule.LowValue.Color = Color.SkyBlue;
-                    rule.MiddleValue.Color = Color.White;
-                    rule.HighValue.Color = Color.Red;
-
-                    var header = worksheet.Cells["B2:F2"];
-                    worksheet.DefaultColWidth = 25;
-                    worksheet.Cells[3, 2, models.Count + 2, 2].Style.Numberformat.Format = "yyyy MMM d DDD";
-                    tableBody.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
-                    tableBody.Style.Fill.PatternType = ExcelFillStyle.Solid;
-                    tableBody.Style.Fill.BackgroundColor.SetColor(Color.WhiteSmoke);
-                    tableBody.Style.Border.BorderAround(ExcelBorderStyle.Medium);
-                    header.Style.Font.Bold = true;
-                    header.Style.Font.Color.SetColor(Color.White);
-                    header.Style.Fill.BackgroundColor.SetColor(Color.DarkBlue);
-
-                    return File(package.GetAsByteArray(), "application/octet-stream", $"{DateTime.Now.ToString("yyyyMMddhhmmss")}_Archives.xlsx");
+                    return Redirect("/");
                 }
             }
+
             return Redirect("/");
         }
+    }
+
+    /// <summary>
+    /// 엑셀 파일 강제 다운로드 기능(/ExcelDown)
+    /// </summary>
+    public async Task<IActionResult> ExcelDown()
+    {
+        var results = await _repository.GetAllAsync(0, 100);
+
+        var models = results.Records.ToList();
+
+        if (models != null)
+        {
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Archives");
+
+                var tableBody = worksheet.Cells["B2:B2"].LoadFromCollection(
+                    (from m in models select new { m.Id, Created = m.Created?.LocalDateTime.ToString(), m.Name, m.Title, m.DownCount, m.FileName })
+                    , true);
+
+                var uploadCol = tableBody.Offset(1, 1, models.Count, 1);
+
+                // 그라데이션 효과 부여 
+                var rule = uploadCol.ConditionalFormatting.AddThreeColorScale();
+                rule.LowValue.Color = Color.SkyBlue;
+                rule.MiddleValue.Color = Color.White;
+                rule.HighValue.Color = Color.Red;
+
+                var header = worksheet.Cells["B2:F2"];
+                worksheet.DefaultColWidth = 25;
+                worksheet.Cells[3, 2, models.Count + 2, 2].Style.Numberformat.Format = "yyyy MMM d DDD";
+                tableBody.Style.HorizontalAlignment = ExcelHorizontalAlignment.Left;
+                tableBody.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                tableBody.Style.Fill.BackgroundColor.SetColor(Color.WhiteSmoke);
+                tableBody.Style.Border.BorderAround(ExcelBorderStyle.Medium);
+                header.Style.Font.Bold = true;
+                header.Style.Font.Color.SetColor(Color.White);
+                header.Style.Fill.BackgroundColor.SetColor(Color.DarkBlue);
+
+                return File(package.GetAsByteArray(), "application/octet-stream", $"{DateTime.Now.ToString("yyyyMMddhhmmss")}_Archives.xlsx");
+            }
+        }
+        return Redirect("/");
     }
 }
