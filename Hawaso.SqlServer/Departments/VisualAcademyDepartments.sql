9e80bea4f66d7c766b7ce07cf26b30febc8cca32
Park YongJune <redplus@redplus.net>
2021-11-30 09:01:26 +0900

    Departments 모듈 분리 시작

---
+++ Hawaso.SqlServer/Departments/VisualAcademyDepartments.sql
@@ -0,0 +1,9 @@
+﻿--[0][0] 부서
+CREATE TABLE [dbo].[VisualAcademyDepartments]
+(
+    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL Primary Key,
+    [Active]    BIT                DEFAULT ((1)) NOT NULL,
+    [CreatedAt] DATETIMEOFFSET (7) NOT NULL,
+    [CreatedBy] NVARCHAR (70)      NULL,
+    [Name]      NVARCHAR (MAX)     NULL
+);
