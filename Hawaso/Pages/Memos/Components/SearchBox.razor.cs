cf37df301a56cfc08ff6a6f7e74c067ad5b9a25f
Park YongJune <redplus@redplus.net>
2022-01-28 11:55:09 +0900

    .

--- Hawaso/Pages/Memos/Components/SearchBox.razor.cs
+++ Hawaso/Pages/Memos/Components/SearchBox.razor.cs
@@ -8,23 +8,28 @@ namespace VisualAcademy.Pages.Memos.Components
     public partial class SearchBox : IDisposable
     {
         #region Fields
-        private string searchQuery;
-        private System.Timers.Timer debounceTimer;
+        private string searchQuery; // 검색어 담을 그릇 
+        private System.Timers.Timer debounceTimer; // 디바운스 타임: 응답 대기 시간(300밀리초)
         #endregion
 
         #region Parameters
+        #region 부모에서 전달된 기타 특성들을 모두 받아서 사용
         [Parameter(CaptureUnmatchedValues = true)]
         public IDictionary<string, object> AdditionalAttributes { get; set; }
+        #endregion
 
         // 자식 컴포넌트에서 발생한 정보를 부모 컴포넌트에게 전달
         [Parameter]
         public EventCallback<string> SearchQueryChanged { get; set; }
 
         [Parameter]
-        public int Debounce { get; set; } = 300; 
+        public int Debounce { get; set; } = 300;
         #endregion
 
         #region Properties
+        /// <summary>
+        /// 부모(외부) 컴포넌트에 공개할 속성 이름
+        /// </summary>
         public string SearchQuery
         {
             get => searchQuery;
@@ -34,7 +39,7 @@ namespace VisualAcademy.Pages.Memos.Components
                 debounceTimer.Stop(); // 텍스트박스에 값을 입력하는 동안 타이머 중지
                 debounceTimer.Start(); // 타이머 실행(300밀리초 후에 딱 한 번 실행)
             }
-        } 
+        }
         #endregion
 
         #region Lifecycle Methods
@@ -66,7 +71,7 @@ namespace VisualAcademy.Pages.Memos.Components
         public void Dispose()
         {
             debounceTimer.Dispose();
-        } 
+        }
         #endregion
     }
 }
