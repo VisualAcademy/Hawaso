213c1944dda692bb44c13cd55cc6a1e8189ccdbe
Park YongJune <redplus@redplus.net>
2022-05-13 15:37:14 +0900

    .

--- Hawaso/Controllers/AboutController.cs
+++ Hawaso/Controllers/AboutController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Hawaso.Controllers
+namespace Hawaso.Controllers;
+
+public class AboutController : Controller
 {
-    public class AboutController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
     }
 }
