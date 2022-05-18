20b705fac973868151c0ddb2da360e89a275f8ce
Park YongJune <redplus@redplus.net>
2022-05-18 14:04:19 +0900

    expression body for constructors

--- Hawaso/Controllers/HomeController.cs
+++ Hawaso/Controllers/HomeController.cs
@@ -8,10 +8,7 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
     public IActionResult Index()
     {
