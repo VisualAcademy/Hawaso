1290755ac357ae529147b6bbf0d5600f8f738b8b
Park YongJune <redplus@redplus.net>
2022-05-22 07:17:41 +0900

    Use expression body for methods

--- Hawaso/Controllers/HomeController.cs
+++ Hawaso/Controllers/HomeController.cs
@@ -12,10 +12,7 @@ public class HomeController : Controller
 
     public IActionResult Index() => View();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
