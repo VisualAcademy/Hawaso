11f8998de2b68b68dbe6055927b6d0c04c9597c9
Park YongJune <redplus@redplus.net>
2022-06-06 00:32:02 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Administrations/Roles/RoleDetails.razor.cs
+++ Hawaso/Pages/Administrations/Roles/RoleDetails.razor.cs
@@ -4,36 +4,35 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Administrations.Roles
+namespace Hawaso.Pages.Administrations.Roles;
+
+public partial class RoleDetails
 {
-    public partial class RoleDetails
+    [Parameter]
+    public string Id { get; set; }
+
+    [Inject]
+    public RoleManager<ApplicationRole> RoleManager { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerRef { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    private ApplicationRole model = new ApplicationRole(); 
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public string Id { get; set; }
+        model = await RoleManager.FindByIdAsync(Id);
+    }
 
-        [Inject]
-        public RoleManager<ApplicationRole> RoleManager { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManagerRef { get; set; }
-
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        private ApplicationRole model = new ApplicationRole(); 
-
-        protected override async Task OnInitializedAsync()
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && model == null)
         {
-            model = await RoleManager.FindByIdAsync(Id);
-        }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender && model == null)
-            {
-                await JSRuntime.InvokeVoidAsync("alert", "잘못된 요청입니다.");
-                NavigationManagerRef.NavigateTo("/Administrations/Roles");
-            }
+            await JSRuntime.InvokeVoidAsync("alert", "잘못된 요청입니다.");
+            NavigationManagerRef.NavigateTo("/Administrations/Roles");
         }
     }
 }
