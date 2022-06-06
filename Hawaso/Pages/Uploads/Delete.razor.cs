04ec74a3ca5c0fc06461d224f2e3f86177ee0669
Park YongJune <redplus@redplus.net>
2022-06-07 05:05:04 +0900

    Remove Unnecessary Usings

--- Hawaso/Pages/Uploads/Delete.razor.cs
+++ Hawaso/Pages/Uploads/Delete.razor.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
-using VisualAcademy.Models.Uploads;
 using VisualAcademy.Models.Replys;
 
 namespace Hawaso.Pages.Uploads
