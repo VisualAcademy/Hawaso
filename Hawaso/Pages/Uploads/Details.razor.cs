352b5b9242d4a50fed4fef95b2c5e155dc76fb80
Park YongJune <redplus@redplus.net>
2022-06-08 01:20:52 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Uploads/Details.razor.cs
+++ Hawaso/Pages/Uploads/Details.razor.cs
@@ -2,24 +2,23 @@
 using System.Threading.Tasks;
 using VisualAcademy.Models.Uploads;
 
-namespace Hawaso.Pages.Uploads
+namespace Hawaso.Pages.Uploads;
+
+public partial class Details
 {
-    public partial class Details
+    [Parameter]
+    public int Id { get; set; }
+
+    [Inject]
+    public IUploadRepository UploadRepositoryAsyncReference { get; set; }
+
+    protected Upload model = new Upload();
+
+    protected string content = "";
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public int Id { get; set; }
-
-        [Inject]
-        public IUploadRepository UploadRepositoryAsyncReference { get; set; }
-
-        protected Upload model = new Upload();
-
-        protected string content = "";
-
-        protected override async Task OnInitializedAsync()
-        {
-            model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
-            content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
-        }
+        model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
+        content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
     }
 }
