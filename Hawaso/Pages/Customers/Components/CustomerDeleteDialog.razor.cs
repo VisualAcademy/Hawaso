c628d83bfe25f17274ad7b70e73434a37ed3fbfc
Park YongJune <redplus@redplus.net>
2022-06-19 02:53:21 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Customers/Components/CustomerDeleteDialog.razor.cs
+++ Hawaso/Pages/Customers/Components/CustomerDeleteDialog.razor.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
-namespace Hawaso.Pages.Customers.Components
+namespace Hawaso.Pages.Customers.Components;
+
+public partial class CustomerDeleteDialog
 {
-    public partial class CustomerDeleteDialog
-    {
-        public bool IsShow { get; set; }
+    public bool IsShow { get; set; }
 
-        public void Show() => IsShow = true;
+    public void Show() => IsShow = true;
 
-        public void Close() => IsShow = false;
+    public void Close() => IsShow = false;
 
-        [Parameter]
-        public EventCallback<MouseEventArgs> OnClick { get; set; }
-    }
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
 }
