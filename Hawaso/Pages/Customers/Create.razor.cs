193bc8504838a2780189286fc314ce9532714989
Park YongJune <redplus@redplus.net>
2022-06-21 02:51:54 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Customers/Create.razor.cs
+++ Hawaso/Pages/Customers/Create.razor.cs
@@ -3,25 +3,24 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Customers
+namespace Hawaso.Pages.Customers;
+
+public partial class Create
 {
-    public partial class Create
+    [Inject]
+    public ICustomerRepository CustomerRepositoryReference { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerInjector { get; set; }
+
+    private Customer customer = new Customer();
+
+    private string[] genders = { "Male", "Female" };
+
+    protected async Task btnSubmit_Click()
     {
-        [Inject]
-        public ICustomerRepository CustomerRepositoryReference { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManagerInjector { get; set; }
-
-        private Customer customer = new Customer();
-
-        private string[] genders = { "Male", "Female" };
-
-        protected async Task btnSubmit_Click()
-        {
-            customer.Created = DateTime.Now; 
-            await CustomerRepositoryReference.AddAsync(customer);
-            NavigationManagerInjector.NavigateTo("/Customers");
-        }
+        customer.Created = DateTime.Now; 
+        await CustomerRepositoryReference.AddAsync(customer);
+        NavigationManagerInjector.NavigateTo("/Customers");
     }
 }
