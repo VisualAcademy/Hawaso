866e2d2aeb1db3435f95730a64937627d1f03a5f
Park YongJune <redplus@redplus.net>
2022-06-22 07:46:29 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Customers/Delete.razor.cs
+++ Hawaso/Pages/Customers/Delete.razor.cs
@@ -3,41 +3,40 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Customers
+namespace Hawaso.Pages.Customers;
+
+public partial class Delete
 {
-    public partial class Delete
+    [Parameter]
+    public int CustomerId { get; set; }
+
+    [Inject]
+    public ICustomerRepository CustomerRepositoryAsync { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private Customer customer = new Customer();
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public int CustomerId { get; set; }
+        customer = await CustomerRepositoryAsync.GetByIdAsync(CustomerId);
+    }
 
-        [Inject]
-        public ICustomerRepository CustomerRepositoryAsync { get; set; }
-
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        private Customer customer = new Customer();
-
-        protected override async Task OnInitializedAsync()
+    protected async void btnDelete_Click()
+    {
+        bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", $"{CustomerId}번 고객 정보를 정말로 삭제하시겠습니까?");
+        if (isDelete)
         {
-            customer = await CustomerRepositoryAsync.GetByIdAsync(CustomerId);
+            await CustomerRepositoryAsync.DeleteAsync(CustomerId);
+            NavigationManager.NavigateTo("/Customers"); 
         }
-
-        protected async void btnDelete_Click()
+        else
         {
-            bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", $"{CustomerId}번 고객 정보를 정말로 삭제하시겠습니까?");
-            if (isDelete)
-            {
-                await CustomerRepositoryAsync.DeleteAsync(CustomerId);
-                NavigationManager.NavigateTo("/Customers"); 
-            }
-            else
-            {
-                await JSRuntime.InvokeAsync<object>("alert", "취소되었습니다.");
-            }
+            await JSRuntime.InvokeAsync<object>("alert", "취소되었습니다.");
         }
     }
 }
