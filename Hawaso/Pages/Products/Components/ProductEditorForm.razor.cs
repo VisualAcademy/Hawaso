9b39446c6ec90620e92991a4aa4108542e2c98c9
Park YongJune <redplus@redplus.net>
2022-06-27 02:25:46 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Products/Components/ProductEditorForm.razor.cs
+++ Hawaso/Pages/Products/Components/ProductEditorForm.razor.cs
@@ -4,72 +4,71 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Products.Components
+namespace Hawaso.Pages.Products.Components;
+
+public partial class ProductEditorForm
 {
-    public partial class ProductEditorForm
+    [Parameter]
+    public RenderFragment EditorFormTitle { get; set; }
+
+    [Parameter]
+    public Product Model { get; set; }
+
+    [Parameter]
+    public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
+
+    [Parameter]
+    public EventCallback<bool> ChangeCallback { get; set; }
+
+    [Inject]
+    public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
+
+    [Inject]
+    public ICategoryRepository CategoryRepositoryAsync { get; set; }
+
+    public bool IsShow { get; set; }
+
+    public string CategoryId { get; set; }
+
+    public List<Category> Categories { get; set; } = new List<Category>();
+
+    public void Show()
     {
-        [Parameter]
-        public RenderFragment EditorFormTitle { get; set; }
+        IsShow = true;
+    }
 
-        [Parameter]
-        public Product Model { get; set; }
+    public void Close()
+    {
+        IsShow = false; 
+    }
 
-        [Parameter]
-        public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
-
-        [Parameter]
-        public EventCallback<bool> ChangeCallback { get; set; }
-
-        [Inject]
-        public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
-
-        [Inject]
-        public ICategoryRepository CategoryRepositoryAsync { get; set; }
-
-        public bool IsShow { get; set; }
-
-        public string CategoryId { get; set; }
-
-        public List<Category> Categories { get; set; } = new List<Category>();
-
-        public void Show()
+    protected async void btnSaveOrUpdate_Click()
+    {
+        Model.CategoryId = Convert.ToInt32(CategoryId); 
+        if (Model.ProductId == 0)
         {
-            IsShow = true;
+            await ProductRepositoryAsync.AddAsync(Model);
+            SaveOrUpdated?.Invoke(); 
         }
-
-        public void Close()
+        else
         {
-            IsShow = false; 
+            await ProductRepositoryAsync.EditAsync(Model); 
+            await ChangeCallback.InvokeAsync(true);
         }
+        IsShow = false;
+    }
 
-        protected async void btnSaveOrUpdate_Click()
+    protected override void OnParametersSet()
+    {
+        CategoryId = Model.CategoryId.ToString(); // 기존 카테고리 설정     
+        if (CategoryId == "0")
         {
-            Model.CategoryId = Convert.ToInt32(CategoryId); 
-            if (Model.ProductId == 0)
-            {
-                await ProductRepositoryAsync.AddAsync(Model);
-                SaveOrUpdated?.Invoke(); 
-            }
-            else
-            {
-                await ProductRepositoryAsync.EditAsync(Model); 
-                await ChangeCallback.InvokeAsync(true);
-            }
-            IsShow = false;
+            CategoryId = ""; // --Select Category--
         }
+    }
 
-        protected override void OnParametersSet()
-        {
-            CategoryId = Model.CategoryId.ToString(); // 기존 카테고리 설정     
-            if (CategoryId == "0")
-            {
-                CategoryId = ""; // --Select Category--
-            }
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            Categories = await CategoryRepositoryAsync.GetAllAsync();
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        Categories = await CategoryRepositoryAsync.GetAllAsync();
     }
 }
