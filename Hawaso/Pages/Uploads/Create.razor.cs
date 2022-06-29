aabdd57e6f60aaf9cd36bd26f35c06c79a5497b1
Park YongJune <redplus@redplus.net>
2022-06-30 03:58:42 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Uploads/Create.razor.cs
+++ Hawaso/Pages/Uploads/Create.razor.cs
@@ -4,57 +4,56 @@ using System;
 using System.Linq;
 using VisualAcademy.Models.Replys;
 
-namespace Hawaso.Pages.Uploads
+namespace Hawaso.Pages.Uploads;
+
+public partial class Create
 {
-    public partial class Create
+    [Inject]
+    public IUploadRepository UploadRepositoryAsyncReference { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; }
+
+    protected Upload model = new Upload();
+
+    public string ParentId { get; set; }
+
+    protected int[] parentIds = { 1, 2, 3 };
+
+    protected async void FormSubmit()
     {
-        [Inject]
-        public IUploadRepository UploadRepositoryAsyncReference { get; set; }
+        int.TryParse(ParentId, out int parentId);
+        model.ParentId = parentId;
 
-        [Inject]
-        public NavigationManager NavigationManagerReference { get; set; }
-
-        protected Upload model = new Upload();
-
-        public string ParentId { get; set; }
-
-        protected int[] parentIds = { 1, 2, 3 };
-
-        protected async void FormSubmit()
+        #region 파일 업로드 관련 추가 코드 영역
+        if (selectedFiles != null && selectedFiles.Length > 0)
         {
-            int.TryParse(ParentId, out int parentId);
-            model.ParentId = parentId;
-
-            #region 파일 업로드 관련 추가 코드 영역
-            if (selectedFiles != null && selectedFiles.Length > 0)
+            // 파일 업로드
+            var file = selectedFiles.FirstOrDefault();
+            var fileName = "";
+            int fileSize = 0;
+            if (file != null)
             {
-                // 파일 업로드
-                var file = selectedFiles.FirstOrDefault();
-                var fileName = "";
-                int fileSize = 0;
-                if (file != null)
-                {
-                    fileName = file.Name;
-                    fileSize = Convert.ToInt32(file.Size);
+                fileName = file.Name;
+                fileSize = Convert.ToInt32(file.Size);
 
-                    fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
+                fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
 
-                    model.FileName = fileName;
-                    model.FileSize = fileSize;
-                } 
-            }
-            #endregion
-
-            await UploadRepositoryAsyncReference.AddAsync(model);
-            NavigationManagerReference.NavigateTo("/Uploads");
+                model.FileName = fileName;
+                model.FileSize = fileSize;
+            } 
         }
+        #endregion
 
-        [Inject]
-        public IFileStorageManager FileStorageManager { get; set; }
-        private IFileListEntry[] selectedFiles;
-        protected void HandleSelection(IFileListEntry[] files)
-        {
-            this.selectedFiles = files;
-        }
+        await UploadRepositoryAsyncReference.AddAsync(model);
+        NavigationManagerReference.NavigateTo("/Uploads");
+    }
+
+    [Inject]
+    public IFileStorageManager FileStorageManager { get; set; }
+    private IFileListEntry[] selectedFiles;
+    protected void HandleSelection(IFileListEntry[] files)
+    {
+        this.selectedFiles = files;
     }
 }
