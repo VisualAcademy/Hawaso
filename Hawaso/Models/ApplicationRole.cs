2443167fa204d2bda0405b41662242e285dd3213
Park YongJune <redplus@redplus.net>
2022-07-03 09:15:25 +0900

    Convert to file-scoped namespace

--- Hawaso/Models/ApplicationRole.cs
+++ Hawaso/Models/ApplicationRole.cs
@@ -1,15 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace Hawaso.Models
+namespace Hawaso.Models;
+
+/// <summary>
+/// ASP.NET Core Identity 인증과 권한 
+/// 인증 기능
+/// 역할 기반 인증을 사용할 때에는 기본 클래스인 IdentityRole을 상속한
+/// ApplicationRole에 역할(그릅) 설명 등과 같은 추가 속성들 제공
+/// </summary>
+public class ApplicationRole : IdentityRole
 {
-    /// <summary>
-    /// ASP.NET Core Identity 인증과 권한 
-    /// 인증 기능
-    /// 역할 기반 인증을 사용할 때에는 기본 클래스인 IdentityRole을 상속한
-    /// ApplicationRole에 역할(그릅) 설명 등과 같은 추가 속성들 제공
-    /// </summary>
-    public class ApplicationRole : IdentityRole
-    {
-        public string Description { get; set; }
-    }
+    public string Description { get; set; }
 }
