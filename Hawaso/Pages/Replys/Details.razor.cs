44d684d37c76ce6588e14d35efb24f02edf0d2da
Park YongJune <redplus@redplus.net>
2022-07-23 07:07:07 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Replys/Details.razor.cs
+++ Hawaso/Pages/Replys/Details.razor.cs
@@ -2,35 +2,34 @@
 using System.Threading.Tasks;
 using VisualAcademy.Models.Replys;
 
-namespace Hawaso.Pages.Replys
+namespace Hawaso.Pages.Replys;
+
+public partial class Details
 {
-    public partial class Details
+    #region Parameters
+    [Parameter]
+    public int Id { get; set; }
+    #endregion
+
+    #region Injectors
+    [Inject]
+    public IReplyRepository RepositoryReference { get; set; }
+    #endregion
+
+    #region Properties
+    public Reply Model { get; set; } = new Reply();
+
+    public string Content { get; set; } = "";
+    #endregion
+
+    #region Lifecycle Methods
+    /// <summary>
+    /// 페이지 초기화 이벤트 처리기
+    /// </summary>
+    protected override async Task OnInitializedAsync()
     {
-        #region Parameters
-        [Parameter]
-        public int Id { get; set; }
-        #endregion
-
-        #region Injectors
-        [Inject]
-        public IReplyRepository RepositoryReference { get; set; }
-        #endregion
-
-        #region Properties
-        public Reply Model { get; set; } = new Reply();
-
-        public string Content { get; set; } = "";
-        #endregion
-
-        #region Lifecycle Methods
-        /// <summary>
-        /// 페이지 초기화 이벤트 처리기
-        /// </summary>
-        protected override async Task OnInitializedAsync()
-        {
-            Model = await RepositoryReference.GetByIdAsync(Id);
-            Content = Dul.HtmlUtility.EncodeWithTabAndSpace(Model.Content);
-        } 
-        #endregion
-    }
+        Model = await RepositoryReference.GetByIdAsync(Id);
+        Content = Dul.HtmlUtility.EncodeWithTabAndSpace(Model.Content);
+    } 
+    #endregion
 }
