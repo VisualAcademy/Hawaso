333eeb47c699b2f1c74a5f09b26b978436d771c3
Park YongJune <redplus@redplus.net>
2022-10-06 02:01:28 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Administrations/Roles/RoleDelete.razor.cs
+++ Hawaso/Pages/Administrations/Roles/RoleDelete.razor.cs
@@ -4,92 +4,91 @@ using Microsoft.JSInterop;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Administrations.Roles
+namespace Hawaso.Pages.Administrations.Roles;
+
+public partial class RoleDelete
 {
-    public partial class RoleDelete
+    [Parameter]
+    public string Id { get; set; }
+
+    [Inject]
+    public RoleManager<ApplicationRole> RoleManager { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerRef { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    private ApplicationRole model = new ApplicationRole();
+
+    public List<string> ErrorMessages { get; set; } = new List<string>();
+
+    public bool ShowModal { get; set; } = false;
+    public bool ShowBuiltIn { get; set; } = false;
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public string Id { get; set; }
+        model = await RoleManager.FindByIdAsync(Id);
+    }
 
-        [Inject]
-        public RoleManager<ApplicationRole> RoleManager { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManagerRef { get; set; }
-
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        private ApplicationRole model = new ApplicationRole();
-
-        public List<string> ErrorMessages { get; set; } = new List<string>();
-
-        public bool ShowModal { get; set; } = false;
-        public bool ShowBuiltIn { get; set; } = false;
-
-        protected override async Task OnInitializedAsync()
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && model == null)
         {
-            model = await RoleManager.FindByIdAsync(Id);
+            await JSRuntime.InvokeVoidAsync("alert", "잘못된 요청입니다.");
+            NavigationManagerRef.NavigateTo("/Administrations/Roles");
         }
+    }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+    protected void HandleDelete()
+    {
+        ShowModal = true;
+    }
+
+    protected void CloseModal()
+    {
+        ShowModal = false;
+    }
+
+    private async Task DeleteProcess()
+    {
+        //bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", "정말로 삭제하시겠습니까?");
+        //if (model != null && isDelete)
+        if (model != null)
         {
-            if (firstRender && model == null)
+            string r = model.NormalizedName;
+            if (r == "ADMINISTRATORS" || r == "USERS" || r == "GUESTS" || r == "EVERYONE")
             {
-                await JSRuntime.InvokeVoidAsync("alert", "잘못된 요청입니다.");
-                NavigationManagerRef.NavigateTo("/Administrations/Roles");
+                // 빌트인 역할은 필수 구성 역할이기 때문에 삭제할 수 없습니다. 
+                ShowBuiltIn = true;
             }
-        }
-
-        protected void HandleDelete()
-        {
-            ShowModal = true;
-        }
-
-        protected void CloseModal()
-        {
-            ShowModal = false;
-        }
-
-        private async Task DeleteProcess()
-        {
-            //bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", "정말로 삭제하시겠습니까?");
-            //if (model != null && isDelete)
-            if (model != null)
+            else
             {
-                string r = model.NormalizedName;
-                if (r == "ADMINISTRATORS" || r == "USERS" || r == "GUESTS" || r == "EVERYONE")
+                IdentityResult identityResult = await RoleManager.DeleteAsync(model);
+                if (identityResult.Succeeded)
                 {
-                    // 빌트인 역할은 필수 구성 역할이기 때문에 삭제할 수 없습니다. 
-                    ShowBuiltIn = true;
+                    NavigationManagerRef.NavigateTo("/Administrations/Roles");
                 }
                 else
                 {
-                    IdentityResult identityResult = await RoleManager.DeleteAsync(model);
-                    if (identityResult.Succeeded)
+                    foreach (var error in identityResult.Errors)
                     {
-                        NavigationManagerRef.NavigateTo("/Administrations/Roles");
-                    }
-                    else
-                    {
-                        foreach (var error in identityResult.Errors)
-                        {
-                            ErrorMessages.Add(error.Description);
-                        }
+                        ErrorMessages.Add(error.Description);
                     }
                 }
             }
         }
+    }
 
-        protected void OpenBuiltIn()
-        {
-            ShowBuiltIn = true;
-        }
+    protected void OpenBuiltIn()
+    {
+        ShowBuiltIn = true;
+    }
 
-        protected void CloseBuiltIn()
-        {
-            ShowBuiltIn = false;
-            ShowModal = false;
-        }
+    protected void CloseBuiltIn()
+    {
+        ShowBuiltIn = false;
+        ShowModal = false;
     }
 }
