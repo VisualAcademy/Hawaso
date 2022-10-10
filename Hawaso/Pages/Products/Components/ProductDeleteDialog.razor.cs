d9d563c2a4e004824940835b9c1ecf12071f0b44
Park YongJune <redplus@redplus.net>
2022-10-11 00:14:30 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Products/Components/ProductDeleteDialog.razor.cs
+++ Hawaso/Pages/Products/Components/ProductDeleteDialog.razor.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
-namespace Hawaso.Pages.Products.Components
+namespace Hawaso.Pages.Products.Components;
+
+public partial class ProductDeleteDialog
 {
-    public partial class ProductDeleteDialog
+    public bool IsShow { get; set; }
+
+    public void Show() => IsShow = true;
+
+    public void Close()
     {
-        public bool IsShow { get; set; }
-
-        public void Show() => IsShow = true;
-
-        public void Close()
-        {
-            IsShow = false;
-        }
-
-        [Parameter]
-        public EventCallback<MouseEventArgs> OnClick { get; set; }
+        IsShow = false;
     }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
 }
