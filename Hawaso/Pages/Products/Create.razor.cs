82f2040b16d70df149cd7325e323deaf2251ca2f
Park YongJune <redplus@redplus.net>
2022-10-13 07:40:21 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Products/Create.razor.cs
+++ Hawaso/Pages/Products/Create.razor.cs
@@ -4,37 +4,36 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Products
+namespace Hawaso.Pages.Products;
+
+public partial class Create
 {
-    public partial class Create
+    [Inject]
+    public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public ICategoryRepository RepositoryReference { get; set; }
+
+    private Product Product = new Product();
+
+    //private string[] genders = { "Male", "Female" };
+
+    public string CategoryId { get; set; }
+
+    public List<Category> Categories { get; set; } = new List<Category>();
+
+    protected async Task btnSubmit_Click()
     {
-        [Inject]
-        public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
+        Product.CategoryId = Convert.ToInt32(CategoryId); // 선택한 카테고리
+        await ProductRepositoryAsync.AddAsync(Product);
+        NavigationManager.NavigateTo("/Products");
+    }
 
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        [Inject]
-        public ICategoryRepository RepositoryReference { get; set; }
-
-        private Product Product = new Product();
-
-        //private string[] genders = { "Male", "Female" };
-
-        public string CategoryId { get; set; }
-
-        public List<Category> Categories { get; set; } = new List<Category>();
-
-        protected async Task btnSubmit_Click()
-        {
-            Product.CategoryId = Convert.ToInt32(CategoryId); // 선택한 카테고리
-            await ProductRepositoryAsync.AddAsync(Product);
-            NavigationManager.NavigateTo("/Products");
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            Categories = await RepositoryReference.GetAllAsync(); 
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        Categories = await RepositoryReference.GetAllAsync(); 
     }
 }
