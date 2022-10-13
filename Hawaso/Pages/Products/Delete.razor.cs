b00bd671f62677cb3e784e49b086ce9677fc908b
Park YongJune <redplus@redplus.net>
2022-10-13 09:07:42 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Products/Delete.razor.cs
+++ Hawaso/Pages/Products/Delete.razor.cs
@@ -3,41 +3,40 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Products
+namespace Hawaso.Pages.Products;
+
+public partial class Delete
 {
-    public partial class Delete
+    [Parameter]
+    public int ProductId { get; set; }
+
+    [Inject]
+    public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private Product Product = new Product();
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public int ProductId { get; set; }
+        Product = await ProductRepositoryAsync.GetByIdAsync(ProductId);
+    }
 
-        [Inject]
-        public IProductRepositoryAsync ProductRepositoryAsync { get; set; }
-
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        private Product Product = new Product();
-
-        protected override async Task OnInitializedAsync()
+    protected async void btnDelete_Click()
+    {
+        bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", $"{ProductId}번 상품 정보를 정말로 삭제하시겠습니까?");
+        if (isDelete)
         {
-            Product = await ProductRepositoryAsync.GetByIdAsync(ProductId);
+            await ProductRepositoryAsync.DeleteAsync(ProductId);
+            NavigationManager.NavigateTo("/Products"); 
         }
-
-        protected async void btnDelete_Click()
+        else
         {
-            bool isDelete = await JSRuntime.InvokeAsync<bool>("confirm", $"{ProductId}번 상품 정보를 정말로 삭제하시겠습니까?");
-            if (isDelete)
-            {
-                await ProductRepositoryAsync.DeleteAsync(ProductId);
-                NavigationManager.NavigateTo("/Products"); 
-            }
-            else
-            {
-                await JSRuntime.InvokeAsync<object>("alert", "취소되었습니다.");
-            }
+            await JSRuntime.InvokeAsync<object>("alert", "취소되었습니다.");
         }
     }
 }
