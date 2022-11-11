402a4be5ee82b62a6e57ba7b84d31fca3d358955
Park YongJune <redplus@redplus.net>
2022-11-11 18:52:06 +0900

    Convert to file-scoped namespace

--- Hawaso/Areas/Identity/Models/ApplicationRole.cs
+++ Hawaso/Areas/Identity/Models/ApplicationRole.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace Hawaso.Areas.Identity.Models
+namespace Hawaso.Areas.Identity.Models;
+
+public class ApplicationRole : IdentityRole
 {
-    public class ApplicationRole : IdentityRole
-    {
-        public string? Description { get; set; }
-    }
+    public string? Description { get; set; }
 }
