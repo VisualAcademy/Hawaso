dd6a9e860a8b34f0caa789d8c09f541f84f73a2a
Park YongJune <redplus@redplus.net>
2022-11-21 13:32:21 +0900

    Use expression body for methods

--- Hawaso/Pages/Memos/Components/ModalForm.razor.cs
+++ Hawaso/Pages/Memos/Components/ModalForm.razor.cs
@@ -27,10 +27,7 @@ namespace VisualAcademy.Pages.Memos.Components
         /// <summary>
         /// 폼 보이기 
         /// </summary>
-        public void Show()
-        {
-            IsShow = true; // 현재 인라인 모달 폼 보이기
-        }
+        public void Show() => IsShow = true; // 현재 인라인 모달 폼 보이기
 
         /// <summary>
         /// 폼 닫기
