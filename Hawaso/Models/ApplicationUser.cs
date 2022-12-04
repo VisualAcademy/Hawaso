2dea480e2a3e98f567555fe3181f9f480ee3f9e1
Park YongJune <redplus@redplus.net>
2022-12-04 23:08:24 +0900

    Convert to file-scoped namespace

--- Hawaso/Models/ApplicationUser.cs
+++ Hawaso/Models/ApplicationUser.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace Hawaso.Models
+namespace Hawaso.Models;
+
+/// <summary>
+/// ASP.NET Core Identity 인증과 권한 
+/// 인증 기능
+/// ASP.NET Core / Blazor Server 기본 템플릿에는 IdentityUser로 로그인하지만,
+/// 추가적인 정보를 저장할 때에는 ApplicationUser로 속성들을 추가해서 사용
+/// </summary>
+public class ApplicationUser : IdentityUser
 {
-    /// <summary>
-    /// ASP.NET Core Identity 인증과 권한 
-    /// 인증 기능
-    /// ASP.NET Core / Blazor Server 기본 템플릿에는 IdentityUser로 로그인하지만,
-    /// 추가적인 정보를 저장할 때에는 ApplicationUser로 속성들을 추가해서 사용
-    /// </summary>
-    public class ApplicationUser : IdentityUser
-    {
-        [PersonalData]
-        public string Address { get; set; }
-    }
+    [PersonalData]
+    public string Address { get; set; }
 }
