c5b03be9263fc093faf782393cafd6e36bf94b41
Park YongJune <redplus@redplus.net>
2022-12-10 11:17:22 +0900

    Convert to file-scoped namespace

--- Hawaso/Services/EmailSender.cs
+++ Hawaso/Services/EmailSender.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using System.Threading.Tasks;
 
-namespace Hawaso.Services
+namespace Hawaso.Services;
+
+// ASP.NET Core Identity 인증과 권한 
+// Abstractions: Interfaces => IEmailSender
+// Implementations: Classes => EmailSender, SendGridEmailSender, ...
+public class EmailSender : IEmailSender
 {
-    // ASP.NET Core Identity 인증과 권한 
-    // Abstractions: Interfaces => IEmailSender
-    // Implementations: Classes => EmailSender, SendGridEmailSender, ...
-    public class EmailSender : IEmailSender
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
