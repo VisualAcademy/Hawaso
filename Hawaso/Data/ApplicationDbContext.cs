450279c5fd23579d3443b0d5c0ae2e1600f142e5
Park YongJune <redplus@redplus.net>
2022-12-31 08:47:52 +0900

    Convert to file-scoped namespace

--- Hawaso/Data/ApplicationDbContext.cs
+++ Hawaso/Data/ApplicationDbContext.cs
@@ -3,33 +3,32 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace Hawaso.Data
+namespace Hawaso.Data;
+
+#region ASP.NET Core Identity 인증 확장
+// ASP.NET Core Identity
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
 {
-    #region ASP.NET Core Identity 인증 확장
-    // ASP.NET Core Identity
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-
-        //public virtual DbSet<Department> Departments { get; set; }
-
-        #region Cascading DropDownList 
-        public DbSet<Property> Properties { get; set; } = null!;
-        public DbSet<Location> Locations { get; set; } = null!;
-        public DbSet<Sublocation> Sublocations { get; set; } = null!;
-        #endregion
     }
 
-    // 원본 모양 
-    //public class ApplicationDbContext : IdentityDbContext
-    //{
-    //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    //        : base(options)
-    //    {
-    //    }
-    //}
+    //public virtual DbSet<Department> Departments { get; set; }
+
+    #region Cascading DropDownList 
+    public DbSet<Property> Properties { get; set; } = null!;
+    public DbSet<Location> Locations { get; set; } = null!;
+    public DbSet<Sublocation> Sublocations { get; set; } = null!;
     #endregion
 }
+
+// 원본 모양 
+//public class ApplicationDbContext : IdentityDbContext
+//{
+//    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//        : base(options)
+//    {
+//    }
+//}
+#endregion
