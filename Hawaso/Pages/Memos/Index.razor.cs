3d663a9c822f13b36edac7832241518082bba732
Park YongJune <redplus@redplus.net>
2023-05-02 22:09:24 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Memos/Index.razor.cs
+++ Hawaso/Pages/Memos/Index.razor.cs
@@ -3,107 +3,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VisualAcademy.Pages.Memos
+namespace VisualAcademy.Pages.Memos;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public IMemoRepository RepositoryReference { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerInjector { get; set; }
+
+    protected List<Memo> models;
+
+    protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
     {
-        [Inject]
-        public IMemoRepository RepositoryReference { get; set; }
+        PageNumber = 1,
+        PageIndex = 0,
+        PageSize = 10,
+        PagerButtonCount = 5
+    };
 
-        [Inject]
-        public NavigationManager NavigationManagerInjector { get; set; }
+    #region Lifecycle Methods
+    /// <summary>
+    /// 페이지 초기화 이벤트 처리기
+    /// </summary>
+    protected override async Task OnInitializedAsync() => await DisplayData();
+    #endregion
 
-        protected List<Memo> models;
+    private async Task DisplayData()
+    {
+        var articleSet = await RepositoryReference.GetArticlesAsync<int>(pager.PageIndex, pager.PageSize, "", this.searchQuery, this.sortOrder, 0);
+        pager.RecordCount = articleSet.TotalCount;
+        models = articleSet.Items.ToList();
 
-        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
-        {
-            PageNumber = 1,
-            PageIndex = 0,
-            PageSize = 10,
-            PagerButtonCount = 5
-        };
-
-        #region Lifecycle Methods
-        /// <summary>
-        /// 페이지 초기화 이벤트 처리기
-        /// </summary>
-        protected override async Task OnInitializedAsync() => await DisplayData();
-        #endregion
-
-        private async Task DisplayData()
-        {
-            var articleSet = await RepositoryReference.GetArticlesAsync<int>(pager.PageIndex, pager.PageSize, "", this.searchQuery, this.sortOrder, 0);
-            pager.RecordCount = articleSet.TotalCount;
-            models = articleSet.Items.ToList();
-
-            StateHasChanged();
-        }
-
-        protected void NameClick(long id) => NavigationManagerInjector.NavigateTo($"/Memos/Details/{id}");
-
-        protected async void PageIndexChanged(int pageIndex)
-        {
-            pager.PageIndex = pageIndex;
-            pager.PageNumber = pageIndex + 1;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-
-        #region Search
-        private string searchQuery = "";
-
-        protected async void Search(string query)
-        {
-            pager.PageIndex = 0;
-
-            this.searchQuery = query;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-        #endregion
-
-        #region Sorting
-        private string sortOrder = "";
-
-        protected async void SortByName()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Name";
-            }
-            else if (sortOrder == "Name")
-            {
-                sortOrder = "NameDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-
-        protected async void SortByTitle()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Title";
-            }
-            else if (sortOrder == "Title")
-            {
-                sortOrder = "TitleDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-        #endregion
+        StateHasChanged();
     }
+
+    protected void NameClick(long id) => NavigationManagerInjector.NavigateTo($"/Memos/Details/{id}");
+
+    protected async void PageIndexChanged(int pageIndex)
+    {
+        pager.PageIndex = pageIndex;
+        pager.PageNumber = pageIndex + 1;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #region Search
+    private string searchQuery = "";
+
+    protected async void Search(string query)
+    {
+        pager.PageIndex = 0;
+
+        this.searchQuery = query;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+    #endregion
+
+    #region Sorting
+    private string sortOrder = "";
+
+    protected async void SortByName()
+    {
+        if (sortOrder == "")
+        {
+            sortOrder = "Name";
+        }
+        else if (sortOrder == "Name")
+        {
+            sortOrder = "NameDesc";
+        }
+        else
+        {
+            sortOrder = "";
+        }
+
+        await DisplayData();
+    }
+
+    protected async void SortByTitle()
+    {
+        if (sortOrder == "")
+        {
+            sortOrder = "Title";
+        }
+        else if (sortOrder == "Title")
+        {
+            sortOrder = "TitleDesc";
+        }
+        else
+        {
+            sortOrder = "";
+        }
+
+        await DisplayData();
+    }
+    #endregion
 }
