73b136427bbff6d2b572d9dbf07ca84305c2188e
Park YongJune <redplus@redplus.net>
2023-07-23 03:24:35 +0900

    Use expression body for method

--- Hawaso.Replys.Components/DeleteDialog.razor.cs
+++ Hawaso.Replys.Components/DeleteDialog.razor.cs
@@ -29,10 +29,7 @@ namespace Hawaso.Replys.Components
         /// <summary>
         /// 폼 닫기
         /// </summary>
-        public void Hide()
-        {
-            IsShow = false;
-        }
+        public void Hide() => IsShow = false;
         #endregion
     }
 }
