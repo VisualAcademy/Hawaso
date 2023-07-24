da2588658be31527fb919cda7ff8a94e3a28d305
Park YongJune <redplus@redplus.net>
2023-07-25 01:09:15 +0900

    Use expression body for method

--- Hawaso/Pages/Categories/Components/CategoryDeleteDialog.razor.cs
+++ Hawaso/Pages/Categories/Components/CategoryDeleteDialog.razor.cs
@@ -7,10 +7,7 @@ namespace Hawaso.Pages.Categories.Components
     {
         public bool IsShow { get; set; }
 
-        public void Show()
-        {
-            IsShow = true;
-        }
+        public void Show() => IsShow = true;
 
         public void Close()
         {
