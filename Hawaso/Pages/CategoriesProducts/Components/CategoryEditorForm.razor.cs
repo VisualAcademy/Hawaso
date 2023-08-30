f936ac01cba1f9e92b21f33915495fef15c60962
Park YongJune <redplus@redplus.net>
2023-08-31 04:30:45 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/CategoriesProducts/Components/CategoryEditorForm.razor.cs
+++ Hawaso/Pages/CategoriesProducts/Components/CategoryEditorForm.razor.cs
@@ -2,44 +2,43 @@
 using Microsoft.AspNetCore.Components;
 using System;
 
-namespace Hawaso.Pages.CategoriesProducts.Components
+namespace Hawaso.Pages.CategoriesProducts.Components;
+
+public partial class CategoryEditorForm
 {
-    public partial class CategoryEditorForm
+    [Parameter]
+    public RenderFragment EditorFormTitle { get; set; }
+
+    [Parameter]
+    public Category Model { get; set; }
+
+    [Parameter]
+    public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
+
+    [Parameter]
+    public EventCallback<bool> ChangeCallback { get; set; }
+
+    [Inject]
+    public ICategoryRepository CategoryRepositoryAsync { get; set; }
+
+    public bool IsShow { get; set; }
+
+    public void Show() => IsShow = true;
+
+    public void Close() => IsShow = false;
+
+    protected async void btnSaveOrUpdate_Click()
     {
-        [Parameter]
-        public RenderFragment EditorFormTitle { get; set; }
-
-        [Parameter]
-        public Category Model { get; set; }
-
-        [Parameter]
-        public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
-
-        [Parameter]
-        public EventCallback<bool> ChangeCallback { get; set; }
-
-        [Inject]
-        public ICategoryRepository CategoryRepositoryAsync { get; set; }
-
-        public bool IsShow { get; set; }
-
-        public void Show() => IsShow = true;
-
-        public void Close() => IsShow = false;
-
-        protected async void btnSaveOrUpdate_Click()
+        if (Model.CategoryId == 0)
         {
-            if (Model.CategoryId == 0)
-            {
-                await CategoryRepositoryAsync.AddAsync(Model);
-                SaveOrUpdated?.Invoke(); 
-            }
-            else
-            {
-                await CategoryRepositoryAsync.EditAsync(Model); 
-                await ChangeCallback.InvokeAsync(true);
-            }
-            IsShow = false;
+            await CategoryRepositoryAsync.AddAsync(Model);
+            SaveOrUpdated?.Invoke(); 
         }
+        else
+        {
+            await CategoryRepositoryAsync.EditAsync(Model); 
+            await ChangeCallback.InvokeAsync(true);
+        }
+        IsShow = false;
     }
 }
