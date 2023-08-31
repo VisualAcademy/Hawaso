528d1bf6d7ef514a0795eb05ae33c4f140ab8a2f
Park YongJune <redplus@redplus.net>
2023-09-01 02:09:33 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Customers/Components/CustomerEditorForm.razor.cs
+++ Hawaso/Pages/Customers/Components/CustomerEditorForm.razor.cs
@@ -2,59 +2,58 @@
 using Microsoft.AspNetCore.Components;
 using System;
 
-namespace Hawaso.Pages.Customers.Components
+namespace Hawaso.Pages.Customers.Components;
+
+public partial class CustomerEditorForm
 {
-    public partial class CustomerEditorForm
+    [Parameter]
+    public RenderFragment EditorFormTitle { get; set; }
+
+    [Parameter]
+    public Customer Model { get; set; }
+
+    public Customer ModelEdit { get; set; }
+
+    // 넘어온 Model 값을 수정 전용 ModelEdit에 담기 
+    protected override void OnParametersSet()
     {
-        [Parameter]
-        public RenderFragment EditorFormTitle { get; set; }
+        ModelEdit = new Customer(); 
+        ModelEdit.CustomerId = Model.CustomerId;
+        ModelEdit.CustomerName = Model.CustomerName;
+        ModelEdit.EmailAddress = Model.EmailAddress; 
+    }
 
-        [Parameter]
-        public Customer Model { get; set; }
+    [Parameter]
+    public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
 
-        public Customer ModelEdit { get; set; }
+    [Parameter]
+    public EventCallback<bool> ChangeCallback { get; set; }
 
-        // 넘어온 Model 값을 수정 전용 ModelEdit에 담기 
-        protected override void OnParametersSet()
+    [Inject]
+    public ICustomerRepository CustomerRepositoryAsync { get; set; }
+
+    public bool IsShow { get; set; }
+
+    public void Show() => IsShow = true;
+
+    public void Close() => IsShow = false;
+
+    protected async void btnSaveOrUpdate_Click()
+    {
+        // 변경 내용 저장
+        Model.CustomerName = ModelEdit.CustomerName;
+        Model.EmailAddress = ModelEdit.EmailAddress;
+
+        if (Model.CustomerId == 0)
         {
-            ModelEdit = new Customer(); 
-            ModelEdit.CustomerId = Model.CustomerId;
-            ModelEdit.CustomerName = Model.CustomerName;
-            ModelEdit.EmailAddress = Model.EmailAddress; 
+            await CustomerRepositoryAsync.AddAsync(Model);
+            SaveOrUpdated?.Invoke(); 
         }
-
-        [Parameter]
-        public Action SaveOrUpdated { get; set; } // EventCallback<bool> 
-
-        [Parameter]
-        public EventCallback<bool> ChangeCallback { get; set; }
-
-        [Inject]
-        public ICustomerRepository CustomerRepositoryAsync { get; set; }
-
-        public bool IsShow { get; set; }
-
-        public void Show() => IsShow = true;
-
-        public void Close() => IsShow = false;
-
-        protected async void btnSaveOrUpdate_Click()
+        else
         {
-            // 변경 내용 저장
-            Model.CustomerName = ModelEdit.CustomerName;
-            Model.EmailAddress = ModelEdit.EmailAddress;
-
-            if (Model.CustomerId == 0)
-            {
-                await CustomerRepositoryAsync.AddAsync(Model);
-                SaveOrUpdated?.Invoke(); 
-            }
-            else
-            {
-                await CustomerRepositoryAsync.EditAsync(Model); 
-                await ChangeCallback.InvokeAsync(true);
-            }
-            IsShow = false;
+            await CustomerRepositoryAsync.EditAsync(Model); 
+            await ChangeCallback.InvokeAsync(true);
         }
+        IsShow = false;
     }
 }
