f5ce4678976aab643cf436299bf094411d844d5f
Park YongJune <redplus@redplus.net>
2023-09-28 11:02:08 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Administrations/Roles/RoleEdit.razor.cs
+++ Hawaso/Pages/Administrations/Roles/RoleEdit.razor.cs
@@ -4,53 +4,52 @@ using Microsoft.JSInterop;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Administrations.Roles
+namespace Hawaso.Pages.Administrations.Roles;
+
+public partial class RoleEdit
 {
-    public partial class RoleEdit
+    [Parameter]
+    public string Id { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerRef { get; set; }
+
+    [Inject]
+    public RoleManager<ApplicationRole> RoleManager { get; set; }
+
+    public ApplicationRoleViewModel ViewModel { get; set; } = new ApplicationRoleViewModel();
+
+    public List<string> ErrorMessages { get; set; } = new List<string>();
+
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    private ApplicationRole model = new ApplicationRole();
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public string Id { get; set; }
+        model = await RoleManager.FindByIdAsync(Id);
+        ViewModel.Id = model.Id;
+        ViewModel.RoleName = model.Name;
+    }
 
-        [Inject]
-        public NavigationManager NavigationManagerRef { get; set; }
-
-        [Inject]
-        public RoleManager<ApplicationRole> RoleManager { get; set; }
-
-        public ApplicationRoleViewModel ViewModel { get; set; } = new ApplicationRoleViewModel();
-
-        public List<string> ErrorMessages { get; set; } = new List<string>();
-
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
-
-        private ApplicationRole model = new ApplicationRole();
-
-        protected override async Task OnInitializedAsync()
+    public async Task HandleUpdate()
+    {
+        var role = await RoleManager.FindByIdAsync(Id);
+        if (role != null)
         {
-            model = await RoleManager.FindByIdAsync(Id);
-            ViewModel.Id = model.Id;
-            ViewModel.RoleName = model.Name;
-        }
+            role.Name = ViewModel.RoleName;
 
-        public async Task HandleUpdate()
-        {
-            var role = await RoleManager.FindByIdAsync(Id);
-            if (role != null)
+            IdentityResult identityResult = await RoleManager.UpdateAsync(role);
+            if (identityResult.Succeeded)
             {
-                role.Name = ViewModel.RoleName;
-
-                IdentityResult identityResult = await RoleManager.UpdateAsync(role);
-                if (identityResult.Succeeded)
+                NavigationManagerRef.NavigateTo("/Administrations/Roles/RoleDetails/" + Id);
+            }
+            else
+            {
+                foreach (var error in identityResult.Errors)
                 {
-                    NavigationManagerRef.NavigateTo("/Administrations/Roles/RoleDetails/" + Id);
-                }
-                else
-                {
-                    foreach (var error in identityResult.Errors)
-                    {
-                        ErrorMessages.Add(error.Description);
-                    }
+                    ErrorMessages.Add(error.Description);
                 }
             }
         }
