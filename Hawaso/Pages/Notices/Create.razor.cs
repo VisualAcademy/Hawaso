8ca8cfec34f549e6ee5a27054cd0987fd3e36700
Park YongJune <redplus@redplus.net>
2023-10-06 01:47:30 +0900

    파일 범위 네임스페이스로 변환

--- Hawaso/Pages/Notices/Create.razor.cs
+++ Hawaso/Pages/Notices/Create.razor.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NoticeApp.Models;
 
-namespace Hawaso.Pages.Notices
+namespace Hawaso.Pages.Notices;
+
+public partial class Create
 {
-    public partial class Create
+    [Inject]
+    public INoticeRepository NoticeRepositoryAsyncReference { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; }
+
+    protected Notice model = new Notice();
+
+    public string ParentId { get; set; }
+
+    protected int[] parentIds = { 1, 2, 3 };
+
+    protected async void FormSubmit()
     {
-        [Inject]
-        public INoticeRepository NoticeRepositoryAsyncReference { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManagerReference { get; set; }
-
-        protected Notice model = new Notice();
-
-        public string ParentId { get; set; }
-
-        protected int[] parentIds = { 1, 2, 3 };
-
-        protected async void FormSubmit()
-        {
-            int.TryParse(ParentId, out int parentId);
-            model.ParentId = parentId; 
-            await NoticeRepositoryAsyncReference.AddAsync(model);
-            NavigationManagerReference.NavigateTo("/Notices");
-        }
+        int.TryParse(ParentId, out int parentId);
+        model.ParentId = parentId; 
+        await NoticeRepositoryAsyncReference.AddAsync(model);
+        NavigationManagerReference.NavigateTo("/Notices");
     }
 }
