ad2b35459a19dcd42ab4da94d20cd24b5d276858
Park YongJune <redplus@redplus.net>
2023-10-13 10:09:08 +0900

    Code Cleanup

--- Hawaso/Pages/Customers/Edit.razor.cs
+++ Hawaso/Pages/Customers/Edit.razor.cs
@@ -3,30 +3,34 @@ using Microsoft.AspNetCore.Components;
 using System;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Customers
+namespace Hawaso.Pages.Customers;
+
+public partial class Edit
 {
-    public partial class Edit
+    #region Parameters
+    [Parameter]
+    public int CustomerId { get; set; }
+    #endregion
+
+    [Inject]
+    public ICustomerRepository CustomerRepositoryAsync { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private Customer customer = new Customer();
+
+
+    private string[] genders = { "Male", "Female" };
+
+    #region Lifecycle Methods 
+    protected override async Task OnInitializedAsync() => customer = await CustomerRepositoryAsync.GetByIdAsync(CustomerId);
+    #endregion
+
+    protected async Task btnEdit_Click()
     {
-        [Parameter]
-        public int CustomerId { get; set; }
-
-        [Inject]
-        public ICustomerRepository CustomerRepositoryAsync { get; set; }
-
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        private Customer customer = new Customer();
-
-        protected override async Task OnInitializedAsync() => customer = await CustomerRepositoryAsync.GetByIdAsync(CustomerId);
-
-        private string[] genders = { "Male", "Female" };
-
-        protected async Task btnEdit_Click()
-        {
-            customer.Modified = DateTime.Now;
-            await CustomerRepositoryAsync.EditAsync(customer);
-            NavigationManager.NavigateTo($"/Customers/Details/{CustomerId}");
-        }
+        customer.Modified = DateTime.Now;
+        await CustomerRepositoryAsync.EditAsync(customer);
+        NavigationManager.NavigateTo($"/Customers/Details/{CustomerId}");
     }
 }
