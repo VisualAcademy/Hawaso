16d45812184aee127d8c950f73385e8494a39b3a
Park YongJune <redplus@redplus.net>
2023-10-15 00:15:18 +0900

    Convert to file-scoped namespace

--- Hawaso/Pages/Notices/Details.razor.cs
+++ Hawaso/Pages/Notices/Details.razor.cs
@@ -2,26 +2,25 @@
 using NoticeApp.Models;
 using System.Threading.Tasks;
 
-namespace Hawaso.Pages.Notices
+namespace Hawaso.Pages.Notices;
+
+public partial class Details
 {
-    public partial class Details
+    [Parameter]
+    public int Id { get; set; }
+
+    [Inject]
+    public INoticeRepository NoticeRepositoryAsyncReference { get; set; }
+
+    protected Notice model = new Notice();
+
+    protected string content = "";
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public int Id { get; set; }
-
-        [Inject]
-        public INoticeRepository NoticeRepositoryAsyncReference { get; set; }
-
-        protected Notice model = new Notice();
-
-        protected string content = "";
-
-        protected override async Task OnInitializedAsync()
-        {
-            model = await NoticeRepositoryAsyncReference.GetByIdAsync(Id);
-            //content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
-            // HTML 태그 실행을 위한 인코딩없이 바로 전달
-            content = model.Content;
-        }
+        model = await NoticeRepositoryAsyncReference.GetByIdAsync(Id);
+        //content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
+        // HTML 태그 실행을 위한 인코딩없이 바로 전달
+        content = model.Content;
     }
 }
