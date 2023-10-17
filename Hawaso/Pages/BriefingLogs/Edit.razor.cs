7d1810555989f47e8f2a3bb0815deb36ab15796e
Park YongJune <redplus@redplus.net>
2023-10-17 10:06:15 +0900

    Code Cleanup

--- Hawaso/Pages/BriefingLogs/Edit.razor.cs
+++ Hawaso/Pages/BriefingLogs/Edit.razor.cs
@@ -9,34 +9,45 @@ namespace Hawaso.Pages.BriefingLogs;
 
 public partial class Edit
 {
+    #region Fields
+    protected int[] parentIds = { 1, 2, 3 };
+
+    protected string content = "";
+
+    protected BriefingLog Model = new();
+    #endregion
+
+    #region Properties
+    public string ParentId { get; set; }
+    #endregion
+
+    #region Parameters
     [Parameter]
     public int Id { get; set; }
+    #endregion
 
+    #region Injectors
     [Inject]
     public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; }
 
     [Inject]
     public NavigationManager NavigationManagerReference { get; set; }
+    #endregion
 
-    protected BriefingLog model = new BriefingLog();
-
-    public string ParentId { get; set; }
-
-    protected int[] parentIds = { 1, 2, 3 };
-
-    protected string content = "";
-
+    #region Lifecycle Methods
     protected override async Task OnInitializedAsync()
     {
-        model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
-        content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
-        ParentId = model.ParentId.ToString();
+        Model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
+        content = Dul.HtmlUtility.EncodeWithTabAndSpace(Model.Content);
+        ParentId = Model.ParentId.ToString();
     }
-
+    #endregion
+    
+    #region Event Handlers
     protected async void FormSubmit()
     {
         int.TryParse(ParentId, out int parentId);
-        model.ParentId = parentId;
+        Model.ParentId = parentId;
 
         #region 파일 업로드 관련 추가 코드 영역
         if (selectedFiles != null && selectedFiles.Length > 0)
@@ -51,23 +62,26 @@ public partial class Edit
                 fileSize = Convert.ToInt32(file.Size);
 
                 // 첨부 파일 삭제 
-                await FileStorageManager.DeleteAsync(model.FileName, "BriefingLogs");
+                await FileStorageManager.DeleteAsync(Model.FileName, "BriefingLogs");
 
                 // 다시 업로드
                 fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
 
-                model.FileName = fileName;
-                model.FileSize = fileSize;
+                Model.FileName = fileName;
+                Model.FileSize = fileSize;
             }
         }
         #endregion
 
-        await UploadRepositoryAsyncReference.EditAsync(model);
+        await UploadRepositoryAsyncReference.EditAsync(Model);
         NavigationManagerReference.NavigateTo("/BriefingLogs");
     }
+    #endregion
 
+    #region Features - FileUpload
     [Inject]
     public IBriefingLogFileStorageManager FileStorageManager { get; set; }
     private IFileListEntry[] selectedFiles;
     protected void HandleSelection(IFileListEntry[] files) => this.selectedFiles = files;
+    #endregion
 }
