08764a935b949ce682b3868ec6e901926f29c5f3
Park YongJune <redplus@redplus.net>
2024-01-03 01:18:52 +0900

    Convert to file-scoped namespace

--- Hawaso/Models/ErrorViewModel.cs
+++ Hawaso/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Hawaso.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
+namespace Hawaso.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
