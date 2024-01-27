d57bd3b9b2a42780deb8be41d5cd06c592c369e2
Park YongJune <redplus@redplus.net>
2024-01-27 18:25:38 +0900

    Global Usings and Remove Unnecessary Usings

--- Hawaso/Controllers/DotNetNote/DotNetNoteController.cs
+++ Hawaso/Controllers/DotNetNote/DotNetNoteController.cs
@@ -2,13 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
--- Hawaso/GlobalUsings.cs
+++ Hawaso/GlobalUsings.cs
@@ -1,5 +1,9 @@
 ﻿global using Hawaso.Models;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Logging;
+global using System;
+global using System.Collections.Generic;
 global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
 global using VisualAcademy.Models.Uploads;
