5632e4a5300d7ba5ac234e20d23fe6ab71af3773
Park YongJune <redplus@redplus.net>
2024-02-01 10:26:28 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Extensions/Memos/MemoAppStartupExtensions.cs
+++ src/Hawaso/Extensions/Memos/MemoAppStartupExtensions.cs
@@ -1,23 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Hawaso.Extensions.Memos
+namespace Hawaso.Extensions.Memos;
+
+/// <summary>
+/// 메모앱(MemoApp) 관련 의존성(종속성) 주입 관련 코드만 따로 모아서 관리 
+/// </summary>
+public static class MemoAppStartupExtensions
 {
-    /// <summary>
-    /// 메모앱(MemoApp) 관련 의존성(종속성) 주입 관련 코드만 따로 모아서 관리 
-    /// </summary>
-    public static class MemoAppStartupExtensions
+    public static void AddDependencyInjectionContainerForMemoApp(this IServiceCollection services, string connectionString)
     {
-        public static void AddDependencyInjectionContainerForMemoApp(this IServiceCollection services, string connectionString)
-        {
-            // MemoAppDbContext.cs Inject: New DbContext Add
-            services.AddDbContext<MemoAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+        // MemoAppDbContext.cs Inject: New DbContext Add
+        services.AddDbContext<MemoAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
 
-            // IMemoRepository.cs Inject: DI Container에 서비스(리포지토리) 등록 
-            services.AddTransient<IMemoRepository, MemoRepository>();
+        // IMemoRepository.cs Inject: DI Container에 서비스(리포지토리) 등록 
+        services.AddTransient<IMemoRepository, MemoRepository>();
 
-            // 파일 업로드 및 다운로드 서비스(매니저) 등록
-            services.AddTransient<IMemoFileStorageManager, MemoFileStorageManager>(); // Local Upload
-        }
+        // 파일 업로드 및 다운로드 서비스(매니저) 등록
+        services.AddTransient<IMemoFileStorageManager, MemoFileStorageManager>(); // Local Upload
     }
 }
--- src/Hawaso/Managers/Memos/MemoFileStorageManager.cs
+++ src/Hawaso/Managers/Memos/MemoFileStorageManager.cs
@@ -1,122 +1,118 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using System;
-using System.IO;
-using System.Threading.Tasks;
 
-namespace Hawaso.Models
+namespace Hawaso.Models;
+
+public class MemoFileStorageManager : IMemoFileStorageManager
 {
-    public class MemoFileStorageManager : IMemoFileStorageManager
+    private const string moduleName = "Memos";
+    private readonly IWebHostEnvironment _environment;
+    private readonly string _containerName;
+    private readonly string _folderPath;
+
+    public MemoFileStorageManager(IWebHostEnvironment environment)
     {
-        private const string moduleName = "Memos";
-        private readonly IWebHostEnvironment _environment;
-        private readonly string _containerName;
-        private readonly string _folderPath;
-
-        public MemoFileStorageManager(IWebHostEnvironment environment)
-        {
-            this._environment = environment;
-            _containerName = "files";
-            _folderPath = Path.Combine(_environment.WebRootPath, _containerName);
-        }
-
-        public async Task<bool> DeleteAsync(string fileName, string folderPath = moduleName)
-        {
-            if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
-            {
-                File.Delete(Path.Combine(_folderPath, folderPath, fileName));
-            }
-            return await Task.FromResult(true);
-        }
-
-        public async Task<byte[]> DownloadAsync(string fileName, string folderPath = moduleName)
-        {
-            if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
-            {
-                byte[] fileBytes = await File.ReadAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName));
-                return fileBytes;
-            }
-            return null;
-        }
-
-        public string GetFolderPath(string ownerType, string ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, long ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, int ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath = moduleName, bool overwrite = false)
-        {
-            await File.WriteAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName), bytes);
-
-            return fileName;
-        }
-
-        public async Task<string> UploadAsync(Stream stream, string fileName, string folderPath = moduleName, bool overwrite = false)
-        {
-            // 파일명 중복 처리
-            fileName = Dul.FileUtility.GetFileNameWithNumbering(Path.Combine(_folderPath, folderPath), fileName);
-
-            using (var fileStream = new FileStream(Path.Combine(_folderPath, folderPath, fileName), FileMode.Create))
-            {
-                await stream.CopyToAsync(fileStream);
-            }
-
-            return fileName;
-        }
+        this._environment = environment;
+        _containerName = "files";
+        _folderPath = Path.Combine(_environment.WebRootPath, _containerName);
     }
 
-    #region MemoBlobStorageManager
-    public class MemoBlobStorageManager : IMemoFileStorageManager
+    public async Task<bool> DeleteAsync(string fileName, string folderPath = moduleName)
     {
-        private const string moduleName = "Memos";
-
-        public MemoBlobStorageManager()
+        if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
         {
+            File.Delete(Path.Combine(_folderPath, folderPath, fileName));
         }
-
-        public Task<bool> DeleteAsync(string fileName, string folderPath = moduleName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<byte[]> DownloadAsync(string fileName, string folderPath = moduleName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, string ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, long ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, int ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath = moduleName, bool overwrite = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> UploadAsync(Stream stream, string fileName, string folderPath = moduleName, bool overwrite = false)
-        {
-            throw new NotImplementedException();
-        }
+        return await Task.FromResult(true);
     }
-    #endregion
+
+    public async Task<byte[]> DownloadAsync(string fileName, string folderPath = moduleName)
+    {
+        if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
+        {
+            byte[] fileBytes = await File.ReadAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName));
+            return fileBytes;
+        }
+        return null;
+    }
+
+    public string GetFolderPath(string ownerType, string ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, long ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, int ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath = moduleName, bool overwrite = false)
+    {
+        await File.WriteAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName), bytes);
+
+        return fileName;
+    }
+
+    public async Task<string> UploadAsync(Stream stream, string fileName, string folderPath = moduleName, bool overwrite = false)
+    {
+        // 파일명 중복 처리
+        fileName = Dul.FileUtility.GetFileNameWithNumbering(Path.Combine(_folderPath, folderPath), fileName);
+
+        using (var fileStream = new FileStream(Path.Combine(_folderPath, folderPath, fileName), FileMode.Create))
+        {
+            await stream.CopyToAsync(fileStream);
+        }
+
+        return fileName;
+    }
 }
+
+#region MemoBlobStorageManager
+public class MemoBlobStorageManager : IMemoFileStorageManager
+{
+    private const string moduleName = "Memos";
+
+    public MemoBlobStorageManager()
+    {
+    }
+
+    public Task<bool> DeleteAsync(string fileName, string folderPath = moduleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<byte[]> DownloadAsync(string fileName, string folderPath = moduleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, string ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, long ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, int ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath = moduleName, bool overwrite = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> UploadAsync(Stream stream, string fileName, string folderPath = moduleName, bool overwrite = false)
+    {
+        throw new NotImplementedException();
+    }
+}
+#endregion
