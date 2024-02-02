8ea96273c280ed4a71b20b444ad9db85edb0163f
Park YongJune <redplus@redplus.net>
2024-02-02 15:47:55 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Extensions/BriefingLogs/BriefingLogAppExtensions.cs
+++ src/Hawaso/Extensions/BriefingLogs/BriefingLogAppExtensions.cs
@@ -2,15 +2,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using Zero.Models;
 
-namespace Hawaso.Extensions.BriefingLogs
+namespace Hawaso.Extensions.BriefingLogs;
+
+public static class BriefingLogAppExtensions
 {
-    public static class BriefingLogAppExtensions
+    public static void AddDiForBriefingLogs(this IServiceCollection services, string connectionString)
     {
-        public static void AddDiForBriefingLogs(this IServiceCollection services, string connectionString)
-        {
-            services.AddDbContext<BriefingLogAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
-            services.AddTransient<IBriefingLogRepository, BriefingLogRepository>();
-            services.AddTransient<IBriefingLogFileStorageManager, BriefingLogFileStorageManager>();
-        }
+        services.AddDbContext<BriefingLogAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+        services.AddTransient<IBriefingLogRepository, BriefingLogRepository>();
+        services.AddTransient<IBriefingLogFileStorageManager, BriefingLogFileStorageManager>();
     }
 }
