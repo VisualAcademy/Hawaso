768b51289890d886170d30764e5d2dd950203c2f
Park YongJune <redplus@redplus.net>
2024-02-07 12:45:31 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Models/BannedTypes/01_BannedTypeModel.cs
+++ src/Hawaso/Models/BannedTypes/01_BannedTypeModel.cs
@@ -2,39 +2,38 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VisualAcademy.Models.BannedTypes
+namespace VisualAcademy.Models.BannedTypes;
+
+/// <summary>
+/// 테이블과 일대일로 매핑되는 모델 클래스: BannedType, BannedTypeModel, ...
+/// </summary>
+[Table("BannedTypes")]
+public class BannedTypeModel
 {
     /// <summary>
-    /// 테이블과 일대일로 매핑되는 모델 클래스: BannedType, BannedTypeModel, ...
+    /// BannedType 고유 아이디, 자동 증가
     /// </summary>
-    [Table("BannedTypes")]
-    public class BannedTypeModel
-    {
-        /// <summary>
-        /// BannedType 고유 아이디, 자동 증가
-        /// </summary>
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
 
-        /// <summary>
-        /// 활성 상태 표시, 기본값 true (활성)
-        /// </summary>
-        public bool? Active { get; set; }
+    /// <summary>
+    /// 활성 상태 표시, 기본값 true (활성)
+    /// </summary>
+    public bool? Active { get; set; }
 
-        /// <summary>
-        /// 레코드 생성 시간
-        /// </summary>
-        public DateTimeOffset CreatedAt { get; set; }
+    /// <summary>
+    /// 레코드 생성 시간
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
 
-        /// <summary>
-        /// 레코드 생성자 이름
-        /// </summary>
-        public string? CreatedBy { get; set; }
+    /// <summary>
+    /// 레코드 생성자 이름
+    /// </summary>
+    public string? CreatedBy { get; set; }
 
-        /// <summary>
-        /// BannedType명
-        /// </summary>
-        public string? Name { get; set; }
-    }
+    /// <summary>
+    /// BannedType명
+    /// </summary>
+    public string? Name { get; set; }
 }
