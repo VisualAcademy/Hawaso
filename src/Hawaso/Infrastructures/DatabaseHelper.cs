71107ee1c58052101e878c0bc5af2c4af97374f5
Park YongJune <redplus@redplus.net>
2024-02-20 06:33:53 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Infrastructures/DatabaseHelper.cs
+++ src/Hawaso/Infrastructures/DatabaseHelper.cs
@@ -1,20 +1,20 @@
 ﻿using Microsoft.Data.SqlClient;
 
-namespace VisualAcademy
-{
-    public class DatabaseHelper
-    {
-        public static async Task AddOrUpdateRegistrationDate(string connectionString)
-        {
-            using (var con = new SqlConnection(connectionString))
-            {
-                await con.OpenAsync();
+namespace VisualAcademy;
 
-                // 컬럼 추가
-                var alterCmd = new SqlCommand
-                {
-                    Connection = con,
-                    CommandText = @"
+public class DatabaseHelper
+{
+    public static async Task AddOrUpdateRegistrationDate(string connectionString)
+    {
+        using (var con = new SqlConnection(connectionString))
+        {
+            await con.OpenAsync();
+
+            // 컬럼 추가
+            var alterCmd = new SqlCommand
+            {
+                Connection = con,
+                CommandText = @"
                         IF NOT EXISTS (
                             SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
                             WHERE TABLE_NAME = N'AspNetUsers' AND COLUMN_NAME = N'RegistrationDate'
@@ -23,23 +23,22 @@ namespace VisualAcademy
                             ALTER TABLE AspNetUsers ADD RegistrationDate DATETIMEOFFSET NULL;
                         END;
                     ",
-                    CommandType = System.Data.CommandType.Text
-                };
-                await alterCmd.ExecuteNonQueryAsync();
+                CommandType = System.Data.CommandType.Text
+            };
+            await alterCmd.ExecuteNonQueryAsync();
 
-                // 업데이트
-                var updateCmd = new SqlCommand
-                {
-                    Connection = con,
-                    CommandText = @"
+            // 업데이트
+            var updateCmd = new SqlCommand
+            {
+                Connection = con,
+                CommandText = @"
                         UPDATE AspNetUsers
                         SET RegistrationDate = SYSDATETIMEOFFSET()
                         WHERE RegistrationDate IS NULL;
                     ",
-                    CommandType = System.Data.CommandType.Text
-                };
-                await updateCmd.ExecuteNonQueryAsync();
-            }
+                CommandType = System.Data.CommandType.Text
+            };
+            await updateCmd.ExecuteNonQueryAsync();
         }
     }
 }
