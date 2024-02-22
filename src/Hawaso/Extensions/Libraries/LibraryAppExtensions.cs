abe7af934370b492058cea59a04373ee690563d2
Park YongJune <redplus@redplus.net>
2024-02-22 13:13:52 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Extensions/Libraries/LibraryAppExtensions.cs
+++ src/Hawaso/Extensions/Libraries/LibraryAppExtensions.cs
@@ -2,15 +2,14 @@
 using VisualAcademy.Models.Libraries;
 using Zero.Models;
 
-namespace Hawaso.Extensions.Libraries
+namespace Hawaso.Extensions.Libraries;
+
+public static class LibraryAppExtensions
 {
-    public static class LibraryAppExtensions
+    public static void AddDiForLibries(this IServiceCollection services, string connectionString)
     {
-        public static void AddDiForLibries(this IServiceCollection services, string connectionString)
-        {
-            services.AddDbContext<LibraryAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
-            services.AddTransient<ILibraryRepository, LibraryRepository>();
-            services.AddTransient<ILibraryFileStorageManager, LibraryFileStorageManager>();
-        }
+        services.AddDbContext<LibraryAppDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+        services.AddTransient<ILibraryRepository, LibraryRepository>();
+        services.AddTransient<ILibraryFileStorageManager, LibraryFileStorageManager>();
     }
 }
