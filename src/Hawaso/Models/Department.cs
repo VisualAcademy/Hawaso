599faa9f2ec209edf396c0088790440de51d5cdb
Park YongJune <redplus@redplus.net>
2024-03-02 01:11:57 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Models/Department.cs
+++ src/Hawaso/Models/Department.cs
@@ -1,17 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Hawaso.Models
+namespace Hawaso.Models;
+
+[Table("Department")]
+public partial class Department
 {
-    [Table("Department")]
-    public partial class Department
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [Display(Name = "Department")]
-        public string Dept { get; set; }
+    [Display(Name = "Department")]
+    public string Dept { get; set; }
 
-        public bool Active { get; set; }
-    }
+    public bool Active { get; set; }
 }
