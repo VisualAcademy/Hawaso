2610ebbefd801057e2a00f5d4bce3b3d351e7308
Park YongJune <redplus@redplus.net>
2024-03-08 17:26:22 +0900

    Remove unnecessary usings

--- src/Hawaso/Pages/Administrations/Roles/RoleList.razor.cs
+++ src/Hawaso/Pages/Administrations/Roles/RoleList.razor.cs
@@ -1,9 +1,5 @@
-﻿using Hawaso.Models;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hawaso.Pages.Administrations.Roles
 {
