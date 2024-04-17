f33cfc85f6f374ef57ddb0c6198fc6970d7f0cf9
Park YongJune <redplus@redplus.net>
2024-04-17 14:15:49 +0900

    Use collection expression

--- src/Hawaso/Data/WeatherForecastService.cs
+++ src/Hawaso/Data/WeatherForecastService.cs
@@ -2,10 +2,10 @@ namespace Hawaso.Data;
 
 public class WeatherForecastService
 {
-    private static readonly string[] Summaries = new[]
-    {
+    private static readonly string[] Summaries =
+    [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    ];
 
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
