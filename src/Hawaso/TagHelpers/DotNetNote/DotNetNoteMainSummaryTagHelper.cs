2a8609e0d730eb0505eb85500cb333c3a2950726
Park YongJune <redplus@redplus.net>
2024-04-21 21:48:37 +0900

    Convert to file-scoped namespace

--- src/Hawaso/TagHelpers/DotNetNote/DotNetNoteMainSummaryTagHelper.cs
+++ src/Hawaso/TagHelpers/DotNetNote/DotNetNoteMainSummaryTagHelper.cs
@@ -1,45 +1,44 @@
 ﻿using Hawaso.Models.Notes;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace DotNetNote.TagHelpers
+namespace DotNetNote.TagHelpers;
+
+[HtmlTargetElement("dnn-main-summary")]
+public class DotNetNoteMainSummaryTagHelper : TagHelper
 {
-    [HtmlTargetElement("dnn-main-summary")]
-    public class DotNetNoteMainSummaryTagHelper : TagHelper
+    private readonly INoteRepository _repository;
+
+    /// <summary>
+    /// 게시판 카테고리: Notice, Free, Data, Qna, ...
+    /// </summary>
+    public string Category { get; set; }
+    
+    public DotNetNoteMainSummaryTagHelper(INoteRepository repository)
     {
-        private readonly INoteRepository _repository;
+        _repository = repository;
+    }
 
-        /// <summary>
-        /// 게시판 카테고리: Notice, Free, Data, Qna, ...
-        /// </summary>
-        public string Category { get; set; }
-        
-        public DotNetNoteMainSummaryTagHelper(INoteRepository repository)
+    public override void Process(
+        TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";            
+
+        string s = "";
+
+        //var list = _repository.GetNoteSummaryByCategory(Category);
+        var list = _repository.GetNoteSummaryByCategoryCache(Category);
+
+        foreach (var l in list)
         {
-            _repository = repository;
+            s += $"<div class='post_item'><div class='post_item_text'>" 
+                + $"<span class='post_date'>" 
+                + l.PostDate.ToString("yyyy-MM-dd") 
+                + "</span><span class='post_title'>" 
+                + "<a href = '/DotNetNote/Details/" + l.Id + "'>" 
+                + Dul.StringLibrary.CutStringUnicode(l.Title, 33) 
+                + "</a></span></div></div>";
         }
 
-        public override void Process(
-            TagHelperContext context, TagHelperOutput output)
-        {
-            output.TagName = "div";            
-
-            string s = "";
-
-            //var list = _repository.GetNoteSummaryByCategory(Category);
-            var list = _repository.GetNoteSummaryByCategoryCache(Category);
-
-            foreach (var l in list)
-            {
-                s += $"<div class='post_item'><div class='post_item_text'>" 
-                    + $"<span class='post_date'>" 
-                    + l.PostDate.ToString("yyyy-MM-dd") 
-                    + "</span><span class='post_title'>" 
-                    + "<a href = '/DotNetNote/Details/" + l.Id + "'>" 
-                    + Dul.StringLibrary.CutStringUnicode(l.Title, 33) 
-                    + "</a></span></div></div>";
-            }
-
-            output.Content.AppendHtml(s);
-        }
+        output.Content.AppendHtml(s);
     }
 }
