88ddd94c75eb46e78a27fb7e0407067015145ad5
Park YongJune <redplus@redplus.net>
2024-04-24 17:40:35 +0900

    Use primary constructor (and remove fields)

--- src/Hawaso/TagHelpers/DotNetNote/DotNetNoteMainSummaryTagHelper.cs
+++ src/Hawaso/TagHelpers/DotNetNote/DotNetNoteMainSummaryTagHelper.cs
@@ -4,19 +4,12 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace DotNetNote.TagHelpers;
 
 [HtmlTargetElement("dnn-main-summary")]
-public class DotNetNoteMainSummaryTagHelper : TagHelper
+public class DotNetNoteMainSummaryTagHelper(INoteRepository repository) : TagHelper
 {
-    private readonly INoteRepository _repository;
-
     /// <summary>
     /// 게시판 카테고리: Notice, Free, Data, Qna, ...
     /// </summary>
     public string Category { get; set; }
-    
-    public DotNetNoteMainSummaryTagHelper(INoteRepository repository)
-    {
-        _repository = repository;
-    }
 
     public override void Process(
         TagHelperContext context, TagHelperOutput output)
@@ -26,7 +19,7 @@ public class DotNetNoteMainSummaryTagHelper : TagHelper
         string s = "";
 
         //var list = _repository.GetNoteSummaryByCategory(Category);
-        var list = _repository.GetNoteSummaryByCategoryCache(Category);
+        var list = repository.GetNoteSummaryByCategoryCache(Category);
 
         foreach (var l in list)
         {
