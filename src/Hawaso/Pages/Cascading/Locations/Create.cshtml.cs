f221a41f077fb4747ed8f8a368e3e29cbfce3b06
Park YongJune <redplus@redplus.net>
2024-05-17 21:48:17 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Locations/Create.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Locations/Create.cshtml.cs
@@ -2,31 +2,30 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Locations
+namespace VisualAcademy.Pages.Cascading.Locations;
+
+public class CreateModel(Hawaso.Data.ApplicationDbContext context) : PageModel
 {
-    public class CreateModel(Hawaso.Data.ApplicationDbContext context) : PageModel
+    public IActionResult OnGet()
     {
-        public IActionResult OnGet()
+        ViewData["PropertyId"] = new SelectList(context.Properties, "Id", "Name");
+        return Page();
+    }
+
+    [BindProperty]
+    public Location Location { get; set; }
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
-            ViewData["PropertyId"] = new SelectList(context.Properties, "Id", "Name");
             return Page();
         }
 
-        [BindProperty]
-        public Location Location { get; set; }
+        context.Locations.Add(Location);
+        await context.SaveChangesAsync();
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            context.Locations.Add(Location);
-            await context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
 }
