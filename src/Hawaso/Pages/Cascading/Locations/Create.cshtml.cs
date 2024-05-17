d74770b6b2114de5269f143960e14bc0f8570060
Park YongJune <redplus@redplus.net>
2024-05-17 21:47:55 +0900

    Use primary constructor (and remove fields)

--- src/Hawaso/Pages/Cascading/Locations/Create.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Locations/Create.cshtml.cs
@@ -4,18 +4,11 @@ using VisualAcademy.Models;
 
 namespace VisualAcademy.Pages.Cascading.Locations
 {
-    public class CreateModel : PageModel
+    public class CreateModel(Hawaso.Data.ApplicationDbContext context) : PageModel
     {
-        private readonly Hawaso.Data.ApplicationDbContext _context;
-
-        public CreateModel(Hawaso.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         public IActionResult OnGet()
         {
-            ViewData["PropertyId"] = new SelectList(_context.Properties, "Id", "Name");
+            ViewData["PropertyId"] = new SelectList(context.Properties, "Id", "Name");
             return Page();
         }
 
@@ -30,8 +23,8 @@ namespace VisualAcademy.Pages.Cascading.Locations
                 return Page();
             }
 
-            _context.Locations.Add(Location);
-            await _context.SaveChangesAsync();
+            context.Locations.Add(Location);
+            await context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
