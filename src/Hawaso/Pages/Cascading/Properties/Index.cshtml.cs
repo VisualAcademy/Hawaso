17567b741bce369799f0e379b7554e8236f5a2ca
Park YongJune <redplus@redplus.net>
2024-05-20 17:46:51 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Properties/Index.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Properties/Index.cshtml.cs
@@ -2,22 +2,21 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Properties
+namespace VisualAcademy.Pages.Cascading.Properties;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly Hawaso.Data.ApplicationDbContext _context;
+
+    public IndexModel(Hawaso.Data.ApplicationDbContext context)
     {
-        private readonly Hawaso.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public IndexModel(Hawaso.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IList<Property> Property { get;set; }
 
-        public IList<Property> Property { get;set; }
-
-        public async Task OnGetAsync()
-        {
-            Property = await _context.Properties.ToListAsync();
-        }
+    public async Task OnGetAsync()
+    {
+        Property = await _context.Properties.ToListAsync();
     }
 }
