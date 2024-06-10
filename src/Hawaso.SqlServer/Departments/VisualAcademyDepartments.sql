95d8fbe1cde37a0821dc39aa58e6863593cafd25
Park YongJune <redplus@redplus.net>
2024-06-11 03:59:46 +0900

    .

--- src/Hawaso.SqlServer/Departments/VisualAcademyDepartments.sql
+++ src/Hawaso.SqlServer/Departments/VisualAcademyDepartments.sql
@@ -5,5 +5,5 @@ CREATE TABLE [dbo].[VisualAcademyDepartments]
     [Active]    BIT                DEFAULT ((1)) NOT NULL,                  -- 활성 상태 표시, 기본값 1 (활성)
     [CreatedAt] DATETIMEOFFSET (7) NOT NULL,                                -- 레코드 생성 시간
     [CreatedBy] NVARCHAR (255)     NULL,                                    -- 레코드 생성자 이름
-    [Name]      NVARCHAR (MAX)     NULL                                     -- 부서명
+    [Name]      NVARCHAR (MAX)     NULL                                     -- 이름
 );
