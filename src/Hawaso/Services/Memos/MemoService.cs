c76527dc2cfc2465740e3a7abd5f2e8f45eddc32
Park YongJune <redplus@redplus.net>
2024-06-16 07:36:17 +0900

    .

--- src/Hawaso/Services/Memos/MemoService.cs
+++ src/Hawaso/Services/Memos/MemoService.cs
@@ -1,7 +1,31 @@
-﻿namespace Hawaso.Services.Memos
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hawaso.Models;
+
+namespace Hawaso.Services.Memos
 {
     public class MemoService : IMemoService
     {
+        private readonly IMemoRepository _memoRepository;
+
+        public MemoService(IMemoRepository memoRepository)
+        {
+            _memoRepository = memoRepository;
+        }
+
+        //public Task<IEnumerable<Memo>> GetAllMemosAsync() => _memoRepository.GetAllAsync();
+        public Task<IEnumerable<Memo>> GetAllMemosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Memo> GetMemoByIdAsync(long id) => _memoRepository.GetByIdAsync(id);
+
+        public Task CreateMemoAsync(Memo memo) => _memoRepository.AddAsync(memo);
+
+        public Task UpdateMemoAsync(Memo memo) => _memoRepository.UpdateAsync(memo);
+
+        public Task DeleteMemoAsync(long id) => _memoRepository.DeleteAsync(id);
 
     }
 }
