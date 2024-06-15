34d9be34edfc1c4e5b52463bc26aa4e74733e4b5
Park YongJune <redplus@redplus.net>
2024-06-16 07:34:25 +0900

    IMemoService interface add

--- src/Hawaso/Services/Memos/IMemoService.cs
+++ src/Hawaso/Services/Memos/IMemoService.cs
@@ -1,7 +1,15 @@
-﻿namespace Hawaso.Services.Memos
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hawaso.Models;
+
+namespace Hawaso.Services.Memos
 {
     public interface IMemoService
     {
-
+        Task<IEnumerable<Memo>> GetAllMemosAsync();
+        Task<Memo> GetMemoByIdAsync(long id);
+        Task CreateMemoAsync(Memo memo);
+        Task UpdateMemoAsync(Memo memo);
+        Task DeleteMemoAsync(long id);
     }
 }
