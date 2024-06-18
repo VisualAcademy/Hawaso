88866ba2b7d3d4d122a0664bf8495c20feb21f84
Park YongJune <redplus@redplus.net>
2024-06-18 20:56:13 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Services/Memos/MemoService.cs
+++ src/Hawaso/Services/Memos/MemoService.cs
@@ -1,27 +1,26 @@
-﻿namespace Hawaso.Services.Memos
+﻿namespace Hawaso.Services.Memos;
+
+public class MemoService : IMemoService
 {
-    public class MemoService : IMemoService
+    private readonly IMemoRepository _memoRepository;
+
+    public MemoService(IMemoRepository memoRepository)
     {
-        private readonly IMemoRepository _memoRepository;
-
-        public MemoService(IMemoRepository memoRepository)
-        {
-            _memoRepository = memoRepository;
-        }
-
-        //public Task<IEnumerable<Memo>> GetAllMemosAsync() => _memoRepository.GetAllAsync();
-        public Task<IEnumerable<Memo>> GetAllMemosAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Memo> GetMemoByIdAsync(long id) => _memoRepository.GetByIdAsync(id);
-
-        public Task CreateMemoAsync(Memo memo) => _memoRepository.AddAsync(memo);
-
-        public Task UpdateMemoAsync(Memo memo) => _memoRepository.UpdateAsync(memo);
-
-        public Task DeleteMemoAsync(long id) => _memoRepository.DeleteAsync(id);
-
+        _memoRepository = memoRepository;
     }
+
+    //public Task<IEnumerable<Memo>> GetAllMemosAsync() => _memoRepository.GetAllAsync();
+    public Task<IEnumerable<Memo>> GetAllMemosAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Memo> GetMemoByIdAsync(long id) => _memoRepository.GetByIdAsync(id);
+
+    public Task CreateMemoAsync(Memo memo) => _memoRepository.AddAsync(memo);
+
+    public Task UpdateMemoAsync(Memo memo) => _memoRepository.UpdateAsync(memo);
+
+    public Task DeleteMemoAsync(long id) => _memoRepository.DeleteAsync(id);
+
 }
