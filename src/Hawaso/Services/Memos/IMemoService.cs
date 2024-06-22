cc1a3af347b97ec659ab823896254c068bf437e0
Park YongJune <redplus@redplus.net>
2024-06-23 06:58:14 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Services/Memos/IMemoService.cs
+++ src/Hawaso/Services/Memos/IMemoService.cs
@@ -1,11 +1,10 @@
-﻿namespace Hawaso.Services.Memos
+﻿namespace Hawaso.Services.Memos;
+
+public interface IMemoService
 {
-    public interface IMemoService
-    {
-        Task<IEnumerable<Memo>> GetAllMemosAsync();
-        Task<Memo> GetMemoByIdAsync(long id);
-        Task CreateMemoAsync(Memo memo);
-        Task UpdateMemoAsync(Memo memo);
-        Task DeleteMemoAsync(long id);
-    }
+    Task<IEnumerable<Memo>> GetAllMemosAsync();
+    Task<Memo> GetMemoByIdAsync(long id);
+    Task CreateMemoAsync(Memo memo);
+    Task UpdateMemoAsync(Memo memo);
+    Task DeleteMemoAsync(long id);
 }
