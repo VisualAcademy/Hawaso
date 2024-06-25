4ff46e2266cc4f689fced9e46674bdfd5fd3ddb6
Park YongJune <redplus@redplus.net>
2024-06-25 09:36:02 +0900

    .

---
+++ src/Hawaso/Controllers/ApplicationFormDataViewerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    /// <summary>
+    /// 특정 Application의 JSON Form Data를 HTML로 변환하여 보여주는 컨트롤러
+    /// </summary>
+    public class ApplicationFormDataViewerController : Controller
+    {
+        public IActionResult Index(string formName = "", long id = 0, long applicationId = 0)
+        {
+            return View();
+        }
+    }
+}
