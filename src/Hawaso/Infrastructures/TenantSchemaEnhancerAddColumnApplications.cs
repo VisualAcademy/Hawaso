a54cc4679217b11937ffdf64414bbf56e630ef05
Park YongJune <redplus@redplus.net>
2024-07-05 06:20:00 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Infrastructures/TenantSchemaEnhancerAddColumnApplications.cs
+++ src/Hawaso/Infrastructures/TenantSchemaEnhancerAddColumnApplications.cs
@@ -1,78 +1,77 @@
-﻿namespace Hawaso.Infrastructures
+﻿namespace Hawaso.Infrastructures;
+
+public class TenantSchemaEnhancerAddColumnApplications
 {
-    public class TenantSchemaEnhancerAddColumnApplications
+    private readonly string _masterConnectionString;
+
+    public TenantSchemaEnhancerAddColumnApplications(string masterConnectionString)
     {
-        private readonly string _masterConnectionString;
+        _masterConnectionString = masterConnectionString;
+    }
 
-        public TenantSchemaEnhancerAddColumnApplications(string masterConnectionString)
+    public void EnhanceAllTenantDatabases()
+    {
+        List<string> tenantConnectionStrings = GetTenantConnectionStrings();
+
+        foreach (string connStr in tenantConnectionStrings)
         {
-            _masterConnectionString = masterConnectionString;
+            EnsureNewColumns(connStr);
         }
+    }
 
-        public void EnhanceAllTenantDatabases()
+    private List<string> GetTenantConnectionStrings()
+    {
+        List<string> result = new List<string>();
+
+        using (SqlConnection connection = new SqlConnection(_masterConnectionString))
         {
-            List<string> tenantConnectionStrings = GetTenantConnectionStrings();
+            connection.Open();
 
-            foreach (string connStr in tenantConnectionStrings)
+            SqlCommand cmd = new SqlCommand("SELECT ConnectionString FROM dbo.Tenants", connection);
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                EnsureNewColumns(connStr);
-            }
-        }
-
-        private List<string> GetTenantConnectionStrings()
-        {
-            List<string> result = new List<string>();
-
-            using (SqlConnection connection = new SqlConnection(_masterConnectionString))
-            {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand("SELECT ConnectionString FROM dbo.Tenants", connection);
-                using (SqlDataReader reader = cmd.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
-                    {
-                        result.Add(reader["ConnectionString"].ToString());
-                    }
+                    result.Add(reader["ConnectionString"].ToString());
                 }
-
-                connection.Close();
             }
 
-            return result;
+            connection.Close();
         }
 
-        private void EnsureNewColumns(string connectionString)
+        return result;
+    }
+
+    private void EnsureNewColumns(string connectionString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            connection.Open();
+
+            List<(string Name, string Type)> columnsToAdd = new List<(string, string)>
             {
-                connection.Open();
+                ("PortalName", "nvarchar(max) Default('Hawaso') NULL"),
+            };
 
-                List<(string Name, string Type)> columnsToAdd = new List<(string, string)>
-                {
-                    ("PortalName", "nvarchar(max) Default('Hawaso') NULL"),
-                };
-
-                foreach (var column in columnsToAdd)
-                {
-                    SqlCommand cmdCheck = new SqlCommand($@"
+            foreach (var column in columnsToAdd)
+            {
+                SqlCommand cmdCheck = new SqlCommand($@"
                         SELECT COUNT(*) 
                         FROM INFORMATION_SCHEMA.COLUMNS 
                         WHERE TABLE_NAME = 'Applications' 
                         AND COLUMN_NAME = '{column.Name}'", connection);
 
-                    int columnCount = (int)cmdCheck.ExecuteScalar();
-                    if (columnCount == 0)
-                    {
-                        SqlCommand cmdAddColumn = new SqlCommand($@"
+                int columnCount = (int)cmdCheck.ExecuteScalar();
+                if (columnCount == 0)
+                {
+                    SqlCommand cmdAddColumn = new SqlCommand($@"
                             ALTER TABLE [dbo].[Applications]
                             ADD [{column.Name}] {column.Type}", connection);
-                        cmdAddColumn.ExecuteNonQuery();
-                    }
+                    cmdAddColumn.ExecuteNonQuery();
                 }
-
-                connection.Close();
             }
+
+            connection.Close();
         }
     }
 }
