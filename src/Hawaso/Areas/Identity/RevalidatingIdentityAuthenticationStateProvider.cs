2cb5503a78eb354797667290c700cd0400441a38
Park YongJune <redplus@redplus.net>
2024-07-19 09:49:17 +0900

    불필요한 Using 제거 Remove unnecessary usings

--- src/Hawaso/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ src/Hawaso/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
 using System.Security.Claims;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Hawaso.Areas.Identity;
 
+/// <summary>
+/// 재검증 신원 인증 상태 제공자
+/// </summary>
 public class RevalidatingIdentityAuthenticationStateProvider<TUser>
     : RevalidatingServerAuthenticationStateProvider where TUser : class
 {
