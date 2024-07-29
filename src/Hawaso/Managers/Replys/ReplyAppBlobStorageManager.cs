7e2acff49c31af85cd9d95e3723886efa45e4ba5
Park YongJune <redplus@redplus.net>
2024-07-29 10:00:20 +0900

    파일 범위 네임스페이스로 변환 Convert to file-scoped namespace

--- src/Hawaso/Managers/Replys/ReplyAppBlobStorageManager.cs
+++ src/Hawaso/Managers/Replys/ReplyAppBlobStorageManager.cs
@@ -3,43 +3,42 @@ using System.IO;
 using System.Threading.Tasks;
 using VisualAcademy.Models.Replys;
 
-namespace ReplyApp.Managers
+namespace ReplyApp.Managers;
+
+public class ReplyAppBlobStorageManager : IFileStorageManager
 {
-    public class ReplyAppBlobStorageManager : IFileStorageManager
+    public Task<bool> DeleteAsync(string fileName, string folderPath)
     {
-        public Task<bool> DeleteAsync(string fileName, string folderPath)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public Task<byte[]> DownloadAsync(string fileName, string folderPath)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<byte[]> DownloadAsync(string fileName, string folderPath)
+    {
+        throw new NotImplementedException();
+    }
 
-        public string GetFolderPath(string ownerType, string ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
+    public string GetFolderPath(string ownerType, string ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
 
-        public string GetFolderPath(string ownerType, long ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
+    public string GetFolderPath(string ownerType, long ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
 
-        public string GetFolderPath(string ownerType, int ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
+    public string GetFolderPath(string ownerType, int ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite)
+    {
+        throw new NotImplementedException();
     }
 }
