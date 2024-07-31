773e3c85438c886786798e95192f2322548089b5
Park YongJune <redplus@redplus.net>
2024-07-31 09:16:07 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Managers/Replys/ReplyAppFileStorageManager.cs
+++ src/Hawaso/Managers/Replys/ReplyAppFileStorageManager.cs
@@ -4,73 +4,72 @@ using System.IO;
 using System.Threading.Tasks;
 using VisualAcademy.Models.Replys;
 
-namespace ReplyApp.Managers
+namespace ReplyApp.Managers;
+
+public class ReplyAppFileStorageManager : IFileStorageManager
 {
-    public class ReplyAppFileStorageManager : IFileStorageManager
+    private readonly IWebHostEnvironment _environment;
+    private readonly string _containerName;
+    private readonly string _folderPath;
+
+    public ReplyAppFileStorageManager(IWebHostEnvironment environment)
     {
-        private readonly IWebHostEnvironment _environment;
-        private readonly string _containerName;
-        private readonly string _folderPath;
+        this._environment = environment;
+        _containerName = "files";
+        _folderPath = Path.Combine(_environment.WebRootPath, _containerName);
+    }
 
-        public ReplyAppFileStorageManager(IWebHostEnvironment environment)
+    public async Task<bool> DeleteAsync(string fileName, string folderPath)
+    {
+        if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
         {
-            this._environment = environment;
-            _containerName = "files";
-            _folderPath = Path.Combine(_environment.WebRootPath, _containerName);
+            File.Delete(Path.Combine(_folderPath, folderPath, fileName));
+        }
+        return await Task.FromResult(true); 
+    }
+
+    public async Task<byte[]> DownloadAsync(string fileName, string folderPath)
+    {
+        if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
+        {
+            byte[] fileBytes = await File.ReadAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName));
+            return fileBytes;
+        }
+        return null; 
+    }
+
+    public string GetFolderPath(string ownerType, string ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, long ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFolderPath(string ownerType, int ownerId, string fileType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite)
+    {
+        await File.WriteAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName), bytes);
+
+        return fileName;
+    }
+
+    public async Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite)
+    {
+        // 파일명 중복 처리
+        fileName = Dul.FileUtility.GetFileNameWithNumbering(Path.Combine(_folderPath, folderPath), fileName);
+
+        using (var fileStream = new FileStream(Path.Combine(_folderPath, folderPath, fileName), FileMode.Create))
+        {
+            await stream.CopyToAsync(fileStream); 
         }
 
-        public async Task<bool> DeleteAsync(string fileName, string folderPath)
-        {
-            if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
-            {
-                File.Delete(Path.Combine(_folderPath, folderPath, fileName));
-            }
-            return await Task.FromResult(true); 
-        }
-
-        public async Task<byte[]> DownloadAsync(string fileName, string folderPath)
-        {
-            if (File.Exists(Path.Combine(_folderPath, folderPath, fileName)))
-            {
-                byte[] fileBytes = await File.ReadAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName));
-                return fileBytes;
-            }
-            return null; 
-        }
-
-        public string GetFolderPath(string ownerType, string ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, long ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetFolderPath(string ownerType, int ownerId, string fileType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite)
-        {
-            await File.WriteAllBytesAsync(Path.Combine(_folderPath, folderPath, fileName), bytes);
-
-            return fileName;
-        }
-
-        public async Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite)
-        {
-            // 파일명 중복 처리
-            fileName = Dul.FileUtility.GetFileNameWithNumbering(Path.Combine(_folderPath, folderPath), fileName);
-
-            using (var fileStream = new FileStream(Path.Combine(_folderPath, folderPath, fileName), FileMode.Create))
-            {
-                await stream.CopyToAsync(fileStream); 
-            }
-
-            return fileName;
-        }
+        return fileName;
     }
 }
