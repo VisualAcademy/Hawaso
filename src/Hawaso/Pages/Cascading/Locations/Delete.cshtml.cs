af5848387adb34c5893467c01bf30170fc5542bf
Park YongJune <redplus@redplus.net>
2024-08-14 09:50:08 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Locations/Delete.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Locations/Delete.cshtml.cs
@@ -9,53 +9,52 @@ using Microsoft.EntityFrameworkCore;
 using Hawaso.Data;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Locations
+namespace VisualAcademy.Pages.Cascading.Locations;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly Hawaso.Data.ApplicationDbContext _context;
+
+    public DeleteModel(Hawaso.Data.ApplicationDbContext context)
     {
-        private readonly Hawaso.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public DeleteModel(Hawaso.Data.ApplicationDbContext context)
+    [BindProperty]
+    public Location Location { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [BindProperty]
-        public Location Location { get; set; }
+        Location = await _context.Locations
+            .Include(l => l.PropertyRef).FirstOrDefaultAsync(m => m.Id == id);
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        if (Location == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
+        return Page();
+    }
 
-            Location = await _context.Locations
-                .Include(l => l.PropertyRef).FirstOrDefaultAsync(m => m.Id == id);
-
-            if (Location == null)
-            {
-                return NotFound();
-            }
-            return Page();
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        Location = await _context.Locations.FindAsync(id);
+
+        if (Location != null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Location = await _context.Locations.FindAsync(id);
-
-            if (Location != null)
-            {
-                _context.Locations.Remove(Location);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            _context.Locations.Remove(Location);
+            await _context.SaveChangesAsync();
         }
+
+        return RedirectToPage("./Index");
     }
 }
