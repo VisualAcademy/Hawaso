eb1428974f2eeecbb5a8e2f293be6f7093f34207
Park YongJune <redplus@redplus.net>
2024-08-19 07:44:34 +0900

    Use primary constructor (and remove fields)

--- src/Hawaso/Pages/Cascading/Locations/Details.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Locations/Details.cshtml.cs
@@ -11,15 +11,8 @@ using VisualAcademy.Models;
 
 namespace VisualAcademy.Pages.Cascading.Locations;
 
-public class DetailsModel : PageModel
+public class DetailsModel(Hawaso.Data.ApplicationDbContext context) : PageModel
 {
-    private readonly Hawaso.Data.ApplicationDbContext _context;
-
-    public DetailsModel(Hawaso.Data.ApplicationDbContext context)
-    {
-        _context = context;
-    }
-
     public Location Location { get; set; }
 
     public async Task<IActionResult> OnGetAsync(int? id)
@@ -29,7 +22,7 @@ public class DetailsModel : PageModel
             return NotFound();
         }
 
-        Location = await _context.Locations
+        Location = await context.Locations
             .Include(l => l.PropertyRef).FirstOrDefaultAsync(m => m.Id == id);
 
         if (Location == null)
