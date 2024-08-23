638cd20e466593539530f034d57145889331fc08
Park YongJune <redplus@redplus.net>
2024-08-23 21:56:41 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Properties/Delete.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Properties/Delete.cshtml.cs
@@ -9,45 +9,44 @@ using Microsoft.EntityFrameworkCore;
 using Hawaso.Data;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Properties
+namespace VisualAcademy.Pages.Cascading.Properties;
+
+public class DeleteModel(ApplicationDbContext context) : PageModel
 {
-    public class DeleteModel(ApplicationDbContext context) : PageModel
+    [BindProperty]
+    public Property Property { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
     {
-        [BindProperty]
-        public Property Property { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Property = await context.Properties.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (Property == null)
-            {
-                return NotFound();
-            }
-            return Page();
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        Property = await context.Properties.FirstOrDefaultAsync(m => m.Id == id);
+
+        if (Property == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Property = await context.Properties.FindAsync(id);
-
-            if (Property != null)
-            {
-                context.Properties.Remove(Property);
-                await context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            return NotFound();
         }
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Property = await context.Properties.FindAsync(id);
+
+        if (Property != null)
+        {
+            context.Properties.Remove(Property);
+            await context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
     }
 }
