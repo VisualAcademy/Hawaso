885b464c00ace9cf26f185e9589fb134c6947568
Park YongJune <redplus@redplus.net>
2024-08-31 02:52:50 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Sublocations/Edit.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Sublocations/Edit.cshtml.cs
@@ -10,64 +10,63 @@ using Microsoft.EntityFrameworkCore;
 using Hawaso.Data;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Sublocations
+namespace VisualAcademy.Pages.Cascading.Sublocations;
+
+public class EditModel(ApplicationDbContext context) : PageModel
 {
-    public class EditModel(ApplicationDbContext context) : PageModel
+    [BindProperty]
+    public Sublocation Sublocation { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
     {
-        [BindProperty]
-        public Sublocation Sublocation { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
+        if (id == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
 
-            Sublocation = await context.Sublocations
-                .Include(s => s.LocationRef).FirstOrDefaultAsync(m => m.Id == id);
+        Sublocation = await context.Sublocations
+            .Include(s => s.LocationRef).FirstOrDefaultAsync(m => m.Id == id);
 
-            if (Sublocation == null)
-            {
-                return NotFound();
-            }
-           ViewData["LocationId"] = new SelectList(context.Locations, "Id", "Id");
+        if (Sublocation == null)
+        {
+            return NotFound();
+        }
+       ViewData["LocationId"] = new SelectList(context.Locations, "Id", "Id");
+        return Page();
+    }
+
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        context.Attach(Sublocation).State = EntityState.Modified;
+
+        try
         {
-            if (!ModelState.IsValid)
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!SublocationExists(Sublocation.Id))
             {
-                return Page();
+                return NotFound();
             }
-
-            context.Attach(Sublocation).State = EntityState.Modified;
-
-            try
+            else
             {
-                await context.SaveChangesAsync();
+                throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SublocationExists(Sublocation.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
         }
 
-        private bool SublocationExists(int id)
-        {
-            return context.Sublocations.Any(e => e.Id == id);
-        }
+        return RedirectToPage("./Index");
+    }
+
+    private bool SublocationExists(int id)
+    {
+        return context.Sublocations.Any(e => e.Id == id);
     }
 }
