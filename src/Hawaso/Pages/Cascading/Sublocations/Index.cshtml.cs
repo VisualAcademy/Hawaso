1eec4ebc3d465cf6f6b457626eb42d8b9fc141c9
Park YongJune <redplus@redplus.net>
2024-09-03 23:51:55 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Cascading/Sublocations/Index.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Sublocations/Index.cshtml.cs
@@ -9,16 +9,15 @@ using Microsoft.EntityFrameworkCore;
 using Hawaso.Data;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Sublocations
-{
-    public class IndexModel(ApplicationDbContext context) : PageModel
-    {
-        public IList<Sublocation> Sublocation { get;set; }
+namespace VisualAcademy.Pages.Cascading.Sublocations;
 
-        public async Task OnGetAsync()
-        {
-            Sublocation = await context.Sublocations
-                .Include(s => s.LocationRef).ToListAsync();
-        }
+public class IndexModel(ApplicationDbContext context) : PageModel
+{
+    public IList<Sublocation> Sublocation { get;set; }
+
+    public async Task OnGetAsync()
+    {
+        Sublocation = await context.Sublocations
+            .Include(s => s.LocationRef).ToListAsync();
     }
 }
