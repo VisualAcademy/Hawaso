6806b0ebb98f5149314a29b076b1d8df2acff3c4
Park YongJune <redplus@redplus.net>
2024-09-07 02:19:02 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Uploads/Edit.razor.cs
+++ src/Hawaso/Pages/Uploads/Edit.razor.cs
@@ -5,73 +5,72 @@ using System.Linq;
 using System;
 using VisualAcademy.Models.Replys;
 
-namespace Hawaso.Pages.Uploads
+namespace Hawaso.Pages.Uploads;
+
+public partial class Edit
 {
-    public partial class Edit
+    [Parameter]
+    public int Id { get; set; }
+
+    [Inject]
+    public IUploadRepository UploadRepositoryAsyncReference { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; }
+
+    protected Upload model = new Upload();
+
+    public string ParentId { get; set; }
+
+    protected int[] parentIds = { 1, 2, 3 };
+
+    protected string content = "";
+
+    protected override async Task OnInitializedAsync()
     {
-        [Parameter]
-        public int Id { get; set; }
+        model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
+        content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
+        ParentId = model.ParentId.ToString(); 
+    }
 
-        [Inject]
-        public IUploadRepository UploadRepositoryAsyncReference { get; set; }
+    protected async void FormSubmit()
+    {
+        int.TryParse(ParentId, out int parentId);
+        model.ParentId = parentId;
 
-        [Inject]
-        public NavigationManager NavigationManagerReference { get; set; }
-
-        protected Upload model = new Upload();
-
-        public string ParentId { get; set; }
-
-        protected int[] parentIds = { 1, 2, 3 };
-
-        protected string content = "";
-
-        protected override async Task OnInitializedAsync()
+        #region 파일 업로드 관련 추가 코드 영역
+        if (selectedFiles != null && selectedFiles.Length > 0)
         {
-            model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
-            content = Dul.HtmlUtility.EncodeWithTabAndSpace(model.Content);
-            ParentId = model.ParentId.ToString(); 
-        }
-
-        protected async void FormSubmit()
-        {
-            int.TryParse(ParentId, out int parentId);
-            model.ParentId = parentId;
-
-            #region 파일 업로드 관련 추가 코드 영역
-            if (selectedFiles != null && selectedFiles.Length > 0)
+            // 파일 업로드
+            var file = selectedFiles.FirstOrDefault();
+            var fileName = "";
+            int fileSize = 0;
+            if (file != null)
             {
-                // 파일 업로드
-                var file = selectedFiles.FirstOrDefault();
-                var fileName = "";
-                int fileSize = 0;
-                if (file != null)
-                {
-                    fileName = file.Name;
-                    fileSize = Convert.ToInt32(file.Size);
+                fileName = file.Name;
+                fileSize = Convert.ToInt32(file.Size);
 
-                    // 첨부 파일 삭제 
-                    await FileStorageManager.DeleteAsync(model.FileName, "");
+                // 첨부 파일 삭제 
+                await FileStorageManager.DeleteAsync(model.FileName, "");
 
-                    // 다시 업로드
-                    fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
+                // 다시 업로드
+                fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
 
-                    model.FileName = fileName;
-                    model.FileSize = fileSize;
-                } 
-            }
-            #endregion
-
-            await UploadRepositoryAsyncReference.EditAsync(model);
-            NavigationManagerReference.NavigateTo("/Uploads");
+                model.FileName = fileName;
+                model.FileSize = fileSize;
+            } 
         }
+        #endregion
 
-        [Inject]
-        public IFileStorageManager FileStorageManager { get; set; }
-        private IFileListEntry[] selectedFiles;
-        protected void HandleSelection(IFileListEntry[] files)
-        {
-            this.selectedFiles = files;
-        }
+        await UploadRepositoryAsyncReference.EditAsync(model);
+        NavigationManagerReference.NavigateTo("/Uploads");
+    }
+
+    [Inject]
+    public IFileStorageManager FileStorageManager { get; set; }
+    private IFileListEntry[] selectedFiles;
+    protected void HandleSelection(IFileListEntry[] files)
+    {
+        this.selectedFiles = files;
     }
 }
