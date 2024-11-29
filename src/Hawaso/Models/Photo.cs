d4e367ea6674e55754068acde56506208555b1f3
Park YongJune <redplus@redplus.net>
2024-11-29 13:53:25 +0900

    Photo class file add

---
+++ src/Hawaso/Models/Photo.cs
@@ -0,0 +1,8 @@
+﻿namespace Hawaso.Models;
+
+public class Photo
+{
+    public long Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public long? EmployeeId { get; set; }
+}
