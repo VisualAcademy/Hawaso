9748d095dadb63f72a3156088a72b2a96587cd5c
Park YongJune <redplus@redplus.net>
2024-12-05 12:15:19 +0900

    WebApiFileUploadDownloadTestController initial add

---
+++ src/Hawaso/Apis/Demos/WebApiFileUploadDownloadTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Apis.Demos
+{
+    public class WebApiFileUploadDownloadTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
