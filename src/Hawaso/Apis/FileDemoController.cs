a9d5c3aeb0dabb70034f7adb24b0071f1db28770
Park YongJune <redplus@redplus.net>
2024-12-09 20:38:31 +0900

    FileDemoController initial add

---
+++ src/Hawaso/Apis/FileDemoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileDemoController : ControllerBase
+    {
+    }
+}
