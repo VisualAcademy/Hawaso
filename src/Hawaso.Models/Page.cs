5b5c3ae1e8f8d1d21b876093b928880996c9e355
Park YongJune <redplus@redplus.net>
2024-12-10 05:32:52 +0900

    Page model class add

---
+++ src/Hawaso.Models/Page.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hawaso.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string TenantName { get; set; } = "Hawaso"; // 기본 테넌트명 Hawaso
+        public string PageName { get; set; } = "Contact"; // 기본 페이지명 Contact
+        public string Title { get; set; } = string.Empty; // 페이지 제목
+        public string Content { get; set; } = string.Empty; // 페이지 본문
+        public DateTime LastUpdated { get; set; } // 마지막 수정 날짜
+    }
+}
