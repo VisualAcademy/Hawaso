493c723aee3aab6cadb5e643a359315d0bd88b18
Park YongJune <redplus@redplus.net>
2024-12-09 20:37:39 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Apis/ApplicationsNotifications/BroadcastMessageController.cs
+++ src/Hawaso/Apis/ApplicationsNotifications/BroadcastMessageController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Hawaso.Apis.ApplicationsNotifications
+﻿namespace Hawaso.Apis.ApplicationsNotifications
 {
     [Route("api/[controller]")]
     [ApiController]
