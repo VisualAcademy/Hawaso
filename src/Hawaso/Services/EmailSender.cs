251efbe168c016fd2f6884e9eb51c6aa89e21a35
Park YongJune <redplus@redplus.net>
2024-12-25 19:51:42 +0900

    EmailSender test class add

---
+++ src/Hawaso/Services/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Hawaso.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            Console.WriteLine(htmlMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
