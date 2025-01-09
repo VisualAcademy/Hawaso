84bdee8f19716e77ae8b7ab2ffbd8fee8d64f7d4
Park YongJune <redplus@redplus.net>
2025-01-10 01:03:15 +0900

    Convert to file-scoped namespace

--- src/Hawaso.Web/Data/ApplicationDbContext.cs
+++ src/Hawaso.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Hawaso.Web.Data
+namespace Hawaso.Web.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+
 }
