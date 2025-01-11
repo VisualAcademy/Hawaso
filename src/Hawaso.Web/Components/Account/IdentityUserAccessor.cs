1136a916eda58a7dfc6c78d395e7a91d1d639aee
Park YongJune <redplus@redplus.net>
2025-01-11 22:36:27 +0900

    Convert to file-scoped namespace

--- src/Hawaso.Web/Components/Account/IdentityUserAccessor.cs
+++ src/Hawaso.Web/Components/Account/IdentityUserAccessor.cs
@@ -1,20 +1,19 @@
 using Hawaso.Web.Data;
 using Microsoft.AspNetCore.Identity;
 
-namespace Hawaso.Web.Components.Account
+namespace Hawaso.Web.Components.Account;
+
+internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
 {
-    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
     {
-        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        var user = await userManager.GetUserAsync(context.User);
+
+        if (user is null)
         {
-            var user = await userManager.GetUserAsync(context.User);
-
-            if (user is null)
-            {
-                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
-            }
-
-            return user;
+            redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
         }
+
+        return user;
     }
 }
