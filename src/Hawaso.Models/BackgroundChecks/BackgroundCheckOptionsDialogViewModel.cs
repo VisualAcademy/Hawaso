463c327a78d98f2ee9a081d379a8f3c07d2d7907
Park YongJune <redplus@redplus.net>
2025-01-15 03:27:10 +0900

    BackgroundCheckOptionsDialogViewModel class file add

---
+++ src/Hawaso.Models/BackgroundChecks/BackgroundCheckOptionsDialogViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Hawaso.Models
+{
+    public class BackgroundCheckOptionsDialogViewModel
+    {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public string BGCheckID { get; set; }
+
+        public BackgroundCheckOptionsDialogViewModel(string controller, string action, string bgCheckID)
+        {
+            Controller = controller;
+            Action = action;
+            BGCheckID = bgCheckID;
+        }
+    }
+
+}
