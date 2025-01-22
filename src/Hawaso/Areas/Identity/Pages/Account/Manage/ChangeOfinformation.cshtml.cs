9a9dd9d6b57040a67af4e0a83c3db9e99efe8602
Park YongJune <redplus@redplus.net>
2025-01-23 05:38:43 +0900

    ChangeOfinformation file add

---
+++ src/Hawaso/Areas/Identity/Pages/Account/Manage/ChangeOfinformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hawaso.Areas.Identity.Pages.Account.Manage
+{
+    public class ChangeOfinformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
