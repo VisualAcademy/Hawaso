e100bf672bdfe4c372bc230b36467d0f3b757ae4
Park YongJune <redplus@redplus.net>
2025-01-29 17:24:34 +0900

    ExampleModel model class file add

---
+++ src/Hawaso.Web/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models
+{
+    public class ExampleModel
+    {
+        [Required(ErrorMessage = "이름은 필수입니다.")]
+        [StringLength(10, ErrorMessage = "이름을 10자 이하로 입력하세요.")]
+        public string Name { get; set; }
+    }
+}
