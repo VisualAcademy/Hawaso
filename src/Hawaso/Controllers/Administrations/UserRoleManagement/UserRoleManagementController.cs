77c46ec75d9b577349907a8a9d4e4bb32c74770e
Park YongJune <redplus@redplus.net>
2025-02-05 13:04:13 +0900

    UserRoleManagementController class file initial add.

---
+++ src/Hawaso/Controllers/Administrations/UserRoleManagement/UserRoleManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers.Administrations.UserRoleManagement
+{
+    public class UserRoleManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
