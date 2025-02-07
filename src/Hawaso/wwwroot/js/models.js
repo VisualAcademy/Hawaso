7d3ddd3685c8d374d64082228f8aae664dfebfed
Park YongJune <redplus@redplus.net>
2025-02-08 04:48:11 +0900

    .

--- src/Hawaso/wwwroot/js/models.js
+++ src/Hawaso/wwwroot/js/models.js
@@ -3,5 +3,15 @@
         return ({
             license_type: null, 
         });
+    },
+
+    Person: function () {
+        return ({
+            last_name: null,
+            first_name: null,
+            middle_name: null, 
+            name_suffix: null, 
+
+        });
     }
 };
