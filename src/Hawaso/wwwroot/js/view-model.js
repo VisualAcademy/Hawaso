0c946291af4d292f20a580393503a44824fad74c
Park YongJune <redplus@redplus.net>
2025-02-08 03:30:23 +0900

    certification and oath of applicant

--- src/Hawaso/wwwroot/js/view-model.js
+++ src/Hawaso/wwwroot/js/view-model.js
@@ -33,6 +33,10 @@
         authorization_signature: null,
         authorization_signature_date: null,
 
+        // CERTIFICATION AND OATH OF APPLICANT: 지원자가 제공한 모든 정보가 정확하고 진실됨을 확인하는 공식 선언입니다. 신청자는 허위 정보 제공 시 법적 책임을 질 수 있음을 인정합니다.
+        certification_signature: null,
+        certification_signature_date: null, 
+
         // RELEASE OF ALL CLAIMS: 신청자가 특정 상황에서 법적 청구권을 포기하는 문서입니다. 예를 들어, 기관이나 회사에 대한 소송을 제기하지 않겠다는 동의를 포함할 수 있습니다.
         release_of_claims_signature: null,
         release_of_claims_signature_date: null, 
