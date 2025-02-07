adb64a9a20e34df540b2506d5c3b9026da6ec3a8
Park YongJune <redplus@redplus.net>
2025-02-08 03:28:30 +0900

    fix json key

--- src/Hawaso/wwwroot/js/view-model.js
+++ src/Hawaso/wwwroot/js/view-model.js
@@ -30,9 +30,16 @@
         signature_date: null,
 
 
-
         authorization_signature: null,
-        authorization_signature_date: null 
+        authorization_signature_date: null,
+
+        // RELEASE OF ALL CLAIMS: 신청자가 특정 상황에서 법적 청구권을 포기하는 문서입니다. 예를 들어, 기관이나 회사에 대한 소송을 제기하지 않겠다는 동의를 포함할 수 있습니다.
+        release_of_claims_signature: null,
+        release_of_claims_signature_date: null, 
+
+        // AUTHORIZATION FOR RELEASE OF INFORMATION: 신청자가 특정 기관 또는 개인에게 자신의 정보를 공개할 수 있도록 허가하는 동의서입니다. 이는 배경 조사, 신원 확인, 또는 고용 검토 등에 사용될 수 있습니다.
+        release_of_information_signature: null,
+        release_of_information_signature_date: null
     }
 
 
