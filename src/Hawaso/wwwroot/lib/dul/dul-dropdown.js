8a618a19984ac6c8181273f50eec9f027988ec56
Park YongJune <redplus@redplus.net>
2025-02-09 23:00:04 +0900

    dul-dropdown.css and dul-dropdown.js file add.

---
+++ src/Hawaso/wwwroot/lib/dul/dul-dropdown.js
@@ -0,0 +1,17 @@
+﻿/* 드롭다운 열고 닫기 함수 */
+function dulToggleDropdown() {
+    document.getElementById("dulDropdownContent").classList.toggle("dul-show");
+}
+
+// 드롭다운 외부를 클릭하면 리스트 닫기
+window.onclick = function (event) {
+    if (!event.target.matches('.dul-btn')) {
+        var dropdowns = document.getElementsByClassName("dul-dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('dul-show')) {
+                openDropdown.classList.remove('dul-show');
+            }
+        }
+    }
+}
