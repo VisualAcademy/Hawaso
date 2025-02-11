9dd21201d32b5552cf0b77be6f388c7cc6547665
Park YongJune <redplus@redplus.net>
2025-02-12 02:22:04 +0900

    데이터베이스 컨텍스트

---
+++ src/Hawaso.Web/Components/Pages/VendorPages/Models/VendorsClientsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hawaso.Web.Components.Pages.VendorPages.Models
+{
+    public class VendorsClientsDbContext : DbContext
+    {
+        public VendorsClientsDbContext(DbContextOptions<VendorsClientsDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Vendor> Vendors { get; set; }
+    }
+}
