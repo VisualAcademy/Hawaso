982524c50df6f4f022e0d8bf2ea38f3eff88f133
Park YongJune <redplus@redplus.net>
2025-02-12 02:09:28 +0900

    데이터베이스 테이블 생성

---
+++ src/Hawaso.SqlServer/dbo/Tables/Vendors.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Vendors] (
+    [ID]                         BIGINT             IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    [Active]                     BIT                NOT NULL DEFAULT 1,
+    [Name]                       NVARCHAR (MAX)     NOT NULL,
+    [Alias]                      NVARCHAR (MAX)     NULL,
+    [LicenseNumber]              NVARCHAR (35)      NULL,
+    [LicenseDate]                DATETIME2 (7)      NULL,
+    [LicenseRenewalDate]         DATETIME2 (7)      NULL,
+    [LicenseExpirationDate]      DATETIME2 (7)      NULL
+);
