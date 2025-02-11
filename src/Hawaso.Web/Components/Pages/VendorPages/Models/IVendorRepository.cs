c0815786ae8b1cd3ba39dc663be8c2f9375a0a39
Park YongJune <redplus@redplus.net>
2025-02-12 02:32:59 +0900

    리포지토리 인터페이스

---
+++ src/Hawaso.Web/Components/Pages/VendorPages/Models/IVendorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Hawaso.Web.Components.Pages.VendorPages.Models
+{
+    public interface IVendorRepository
+    {
+        Task AddAsync(Vendor vendor, string connectionString);
+        Task<List<Vendor>> GetAllAsync(string connectionString);
+        Task UpdateAsync(Vendor vendor, string connectionString);
+        Task ToggleActiveAsync(long id, string connectionString);
+        Task DeleteAsync(long id, string connectionString);
+    }
+}
