709811694a2f8300e15eda1a60a794f0a5bd2443
Park YongJune <redplus@redplus.net>
2025-02-12 02:29:56 +0900

    UserService 클래스

---
+++ src/Hawaso.Web/Components/Pages/VendorPages/Models/UserService.cs
@@ -0,0 +1,20 @@
+﻿namespace Hawaso.Web.Components.Pages.VendorPages.Models
+{
+    public class UserService : IUserService
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Tenant GetUserNotCached()
+        {
+            return new Tenant
+            {
+                ConnectionString = _configuration.GetConnectionString("TenantDbConnection")
+            };
+        }
+    }
+}
