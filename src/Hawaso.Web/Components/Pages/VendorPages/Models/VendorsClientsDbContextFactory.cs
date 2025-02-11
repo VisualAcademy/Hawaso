1ac8ab39e811c80f856a67d9245a984cafde85ee
Park YongJune <redplus@redplus.net>
2025-02-12 02:24:28 +0900

    데이터베이스 컨텍스트 팩터리

---
+++ src/Hawaso.Web/Components/Pages/VendorPages/Models/VendorsClientsDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hawaso.Web.Components.Pages.VendorPages.Models
+{
+    public class VendorsClientsDbContextFactory : IDbContextFactory<VendorsClientsDbContext>
+    {
+        private readonly IConfiguration _configuration;
+
+        public VendorsClientsDbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public VendorsClientsDbContext CreateDbContext(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                connectionString = _configuration.GetConnectionString("TenantDbConnection");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<VendorsClientsDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new VendorsClientsDbContext(optionsBuilder.Options);
+        }
+
+        public VendorsClientsDbContext CreateDbContext()
+        {
+            var defaultConnectionString = _configuration.GetConnectionString("TenantDbConnection");
+            return CreateDbContext(defaultConnectionString);
+        }
+    }
+}
