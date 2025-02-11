da9f642255d5dd5a0f4b1235df95d44c07367318
Park YongJune <redplus@redplus.net>
2025-02-12 02:13:20 +0900

    모델 클래스

---
+++ src/Hawaso.Web/Components/Pages/VendorPages/Models/Vendor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hawaso.Web.Components.Pages.VendorPages.Models
+{
+    public class Vendor
+    {
+        [Key]
+        public long ID { get; set; }
+
+        [Required]
+        public bool Active { get; set; } = true;
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
