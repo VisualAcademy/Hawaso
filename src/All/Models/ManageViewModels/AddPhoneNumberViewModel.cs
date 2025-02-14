dece9b46fa4ca9fb5149936500f3896c51fe1459
Park YongJune <redplus@redplus.net>
2025-02-14 17:20:48 +0900

    All 프로젝트에 AddPhoneNumberViewModel 뷰모델 클래스 파일 추가

---
+++ src/All/Models/ManageViewModels/AddPhoneNumberViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace All.Models.ManageViewModels
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
