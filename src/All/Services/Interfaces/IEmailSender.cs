34f2fec82d36a3ce8bff63071930d1a897655cc4
Park YongJune <redplus@redplus.net>
2025-02-14 17:15:48 +0900

    IEmailSender interface file add.

---
+++ src/All/Services/Interfaces/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message, bool isBodyHtml = true);
+    }
+}
