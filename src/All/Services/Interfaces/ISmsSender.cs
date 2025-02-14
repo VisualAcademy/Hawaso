f8ce2680643791bd12d80cc34e5a9e7fe40ea30f
Park YongJune <redplus@redplus.net>
2025-02-14 21:03:45 +0900

    ISmsSender interface file add.

---
+++ src/All/Services/Interfaces/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
