df5fdf51fe31ce21bde05fbea0f5ab793241d7bb
Park YongJune <redplus@redplus.net>
2025-02-14 17:24:28 +0900

    All 프로젝트에 VerifyPhoneNumberViewModel 이름의 뷰모델 클래스 파일 추가

---
+++ src/All/Models/ManageViewModels/VerifyPhoneNumberViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace All.Models.ManageViewModels
+{
+    public class VerifyPhoneNumberViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
