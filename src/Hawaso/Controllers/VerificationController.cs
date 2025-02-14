74099f952215959ec72dc18d32dc44323d402a7e
Park YongJune <redplus@redplus.net>
2025-02-14 21:23:30 +0900

    VerificationController default action method add.

---
+++ src/Hawaso/Controllers/VerificationController.cs
@@ -0,0 +1,46 @@
+﻿using All.Models.ManageViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class VerificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddPhoneNumber()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPhoneNumber(AddPhoneNumberViewModel model)
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VerifyPhoneNumber()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult VerifyPhoneNumber(VerifyPhoneNumberViewModel model)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RemovePhoneNumber()
+        {
+            return RedirectToAction(nameof(Index)); 
+        }
+    }
+}
