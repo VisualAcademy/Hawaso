5ee86f81b662bfc8e282fb3a3b9ace6b6d83a59a
Park YongJune <redplus@redplus.net>
2025-02-15 02:32:01 +0900

    ITwilioSender interface file add.

---
+++ src/All/Services/Interfaces/ITwilioSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.Services
+{
+    public interface ITwilioSender
+    {
+        Task SendSmsAsync(string phoneNumber, string message);
+    }
+}
