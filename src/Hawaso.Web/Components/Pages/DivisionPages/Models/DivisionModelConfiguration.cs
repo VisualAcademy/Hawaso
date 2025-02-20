f1d208ed5c685785f4a079601dc9bb3fa0bb060c
Park YongJune <redplus@redplus.net>
2025-02-21 03:42:58 +0900

    13. IEntityTypeConfiguration 인터페이스로 별도의 클래스로 엔터티 형식에 대한 모든 구성 구현

---
+++ src/Hawaso.Web/Components/Pages/DivisionPages/Models/DivisionModelConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hawaso.Web.Components.Pages.DivisionPages.Models
+{
+    public class DivisionModelConfiguration : IEntityTypeConfiguration<DivisionModel>
+    {
+        public void Configure(EntityTypeBuilder<DivisionModel> builder)
+        {
+            builder.ToTable("Divisions")
+                   .HasKey(dm => dm.Id);
+
+            builder.Property(dm => dm.Id)
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(dm => dm.CreatedAt)
+                   .HasDefaultValueSql("GetDate()");
+
+            builder.Property(dm => dm.Name)
+                   .IsRequired()
+                   .HasMaxLength(255);
+        }
+    }
+}
