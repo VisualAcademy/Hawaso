4903b0527a0b209acc34f5fb2af266dd36e8fd0a
Park YongJune <redplus@redplus.net>
2025-02-21 04:06:46 +0900

    14. 순수 엔터티 클래스인 Division 클래스 만들고 이를 DivisionModel 클래스에 상속하기

---
+++ src/Domain/Entities/Division.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Division
+    {
+        /// <summary>
+        /// 부서 고유 아이디, 자동 증가
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 활성 상태 표시, 기본값 true (활성)
+        /// </summary>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// 레코드 생성 시간
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// 레코드 생성자 이름
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// 부서명
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
--- src/Hawaso.Web/Components/Pages/DivisionPages/Models/DivisionModel.cs
+++ src/Hawaso.Web/Components/Pages/DivisionPages/Models/DivisionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Domain.Entities;
 
 namespace Hawaso.Web.Components.Pages.DivisionPages.Models
 {
@@ -7,33 +8,35 @@ namespace Hawaso.Web.Components.Pages.DivisionPages.Models
     /// 테이블과 일대일로 매핑되는 모델 클래스: Division, DivisionModel, ...
     /// </summary>
     //[Table("Divisions")]
-    public class DivisionModel
+    public class DivisionModel : Division
     {
-        /// <summary>
-        /// 부서 고유 아이디, 자동 증가
-        /// </summary>
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long Id { get; set; }
+        #region +
+        ///// <summary>
+        ///// 부서 고유 아이디, 자동 증가
+        ///// </summary>
+        ////[Key]
+        ////[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public long Id { get; set; }
 
-        /// <summary>
-        /// 활성 상태 표시, 기본값 true (활성)
-        /// </summary>
-        public bool? Active { get; set; }
+        ///// <summary>
+        ///// 활성 상태 표시, 기본값 true (활성)
+        ///// </summary>
+        //public bool? Active { get; set; }
 
-        /// <summary>
-        /// 레코드 생성 시간
-        /// </summary>
-        public DateTimeOffset CreatedAt { get; set; }
+        ///// <summary>
+        ///// 레코드 생성 시간
+        ///// </summary>
+        //public DateTimeOffset CreatedAt { get; set; }
 
-        /// <summary>
-        /// 레코드 생성자 이름
-        /// </summary>
-        public string? CreatedBy { get; set; }
+        ///// <summary>
+        ///// 레코드 생성자 이름
+        ///// </summary>
+        //public string? CreatedBy { get; set; }
 
-        /// <summary>
-        /// 부서명
-        /// </summary>
-        public string? Name { get; set; }
+        ///// <summary>
+        ///// 부서명
+        ///// </summary>
+        //public string? Name { get; set; } 
+        #endregion
     }
 }
