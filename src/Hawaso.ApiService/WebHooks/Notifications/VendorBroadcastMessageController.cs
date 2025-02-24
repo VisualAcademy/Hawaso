69179b88b11f772e0f20c9565bd7ac03b36d6601
Park YongJune <redplus@redplus.net>
2025-02-25 08:47:03 +0900

    VendorBroadcastMessage 컨트롤러 클래스 만들고 HTTP 파일 사용 Get과 GetById 메서드 호출하기

---
+++ src/Hawaso.ApiService/WebHooks/Notifications/VendorBroadcastMessageController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hawaso.ApiService.WebHooks.Notifications
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorBroadcastMessageController : ControllerBase
+    {
+        // GET: api/<VendorBroadcastMessageController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Vendor 1", "Vendor 2" };
+        }
+
+        // GET api/<VendorBroadcastMessageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "안녕하세요";
+        }
+
+        // POST api/<VendorBroadcastMessageController>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<VendorBroadcastMessageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<VendorBroadcastMessageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
