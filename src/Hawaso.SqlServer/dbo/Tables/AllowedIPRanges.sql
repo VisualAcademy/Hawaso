b8080680fcbcb66a8032724903a20045197ecd89
Park YongJune <redplus@redplus.net>
2025-02-26 14:45:30 +0900

    Add AllowedIPRanges table

---
+++ src/Hawaso.SqlServer/dbo/Tables/AllowedIPRanges.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE AllowedIPRanges (
+    ID INT PRIMARY KEY IDENTITY(1,1),       -- 자동 증가하는 기본 키
+    StartIPRange VARCHAR(15),               -- IP 범위 시작 주소
+    EndIPRange VARCHAR(15),                 -- IP 범위 끝 주소
+    Description NVarChar(Max),              -- IP 범위에 대한 설명
+    CreateDate DATETIME Default(GetDate()), -- 범위가 추가된 날짜
+    TenantId BIGINT                         -- 테넌트 ID
+);
