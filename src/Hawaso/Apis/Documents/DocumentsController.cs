0264ad85c5c4170d1ebf4a8bd1195d536b6e8b23
Park YongJune <redplus@redplus.net>
2025-03-05 03:23:42 +0900

    Add DocumentsController class file

---
+++ src/Hawaso/Apis/Documents/DocumentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Apis.Documents
+{
+    public class DocumentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
