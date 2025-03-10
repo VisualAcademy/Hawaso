126cd6de9eada1ef66cb2bf592bec5d3905db90b
Park YongJune <redplus@redplus.net>
2025-03-10 22:55:56 +0900

    AgendaController

---
+++ src/Hawaso/Areas/Conferences/Controllers/AgendaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Areas.Conferences.Controllers
+{
+    public class AgendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
