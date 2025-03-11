c6a034ea39406eb0fb3c0be24e18b3630f158404
Park YongJune <redplus@redplus.net>
2025-03-11 21:01:23 +0900

    Add Sesseion class file.

---
+++ src/Hawaso/Areas/Conferences/Models/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace Hawaso.Areas.Conferences.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+        public string Time { get; set; }
+    }
+}
