400fe9b12fbb52d905cfa3d47e587f805c003b48
Park YongJune <redplus@redplus.net>
2025-03-11 20:56:57 +0900

    Add Ticket class file

---
+++ src/Hawaso/Areas/Conferences/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace Hawaso.Areas.Conferences.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
