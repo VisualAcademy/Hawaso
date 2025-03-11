dd8612b9d1e492d7136fc47c287b7f8bc3d59a7e
Park YongJune <redplus@redplus.net>
2025-03-11 21:01:57 +0900

    Add Speaker class file.

---
+++ src/Hawaso/Areas/Conferences/Models/Speaker.cs
@@ -0,0 +1,10 @@
+﻿namespace Hawaso.Areas.Conferences.Models
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Topic { get; set; }
+    }
+}
