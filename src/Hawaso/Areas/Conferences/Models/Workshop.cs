107e2625403ec746f305c4bd06fc86d2271bb105
Park YongJune <redplus@redplus.net>
2025-03-11 21:02:27 +0900

    Add Workshop class file.

---
+++ src/Hawaso/Areas/Conferences/Models/Workshop.cs
@@ -0,0 +1,10 @@
+﻿namespace Hawaso.Areas.Conferences.Models
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Instructor { get; set; }
+        public string Time { get; set; }
+    }
+}
