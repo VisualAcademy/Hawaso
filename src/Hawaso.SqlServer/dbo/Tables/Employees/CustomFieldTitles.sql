add10e06e8b7066df6a41b559b9b1613f4034a9c
Park YongJune <redplus@redplus.net>
2025-03-22 00:37:10 +0900

    Add CustomFieldTitles table script.

---
+++ src/Hawaso.SqlServer/dbo/Tables/Employees/CustomFieldTitles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CustomFieldTitles] (
+    [ID] bigint IDENTITY(1,1) PRIMARY KEY,
+    [Type] nvarchar(50) NOT NULL,
+    [Field] nvarchar(max) NOT NULL,
+    [Title] nvarchar(max) NULL,
+    [Visible] bit NOT NULL DEFAULT 0,
+    [Searchable] bit NOT NULL DEFAULT 0
+);
