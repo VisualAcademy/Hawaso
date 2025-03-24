bc79d70a14caae8a8053c7730bb878d6c7fd9259
Park YongJune <redplus@redplus.net>
2025-03-24 15:09:26 +0900

    Roll Back

--- src/All/Models/Enums/ApplicantType.cs
+++ src/All/Models/Enums/ApplicantType.cs
@@ -6,10 +6,10 @@ namespace All.Models.Enums
     [Flags]
     public enum ApplicantType
     {
-        None = 0,
-        Vendor = 1,      // 0001
-        Employee = 2,    // 0010
-        Manager = 4,     // 0100
-        Admin = 8        // 1000
+        [Description("Vendor")]
+        Vendor = 1, // 0001
+
+        [Description("Employee")]
+        Employee = 2 // 0010
     }
 }
