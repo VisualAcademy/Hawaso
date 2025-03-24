67ae71808bccb2cc03b8a728b930af25a836502c
Park YongJune <redplus@redplus.net>
2025-03-24 14:42:15 +0900

    Add CertificateReports table script.

---
+++ src/Hawaso.SqlServer/Vendors/CertificateReports.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[CertificateReports] (
+    [ID]                       BIGINT             IDENTITY (1, 1) NOT NULL,
+    [Active]                   BIT                DEFAULT ((1)) NOT NULL,
+    [ApprovedBy]               NVARCHAR (MAX)     NULL,
+    [Conclusion]               NVARCHAR (MAX)     NULL,
+    [CreatedAt]                DATETIMEOFFSET (7) NOT NULL,
+    [CreatedBy]                NVARCHAR (70)      NULL,
+    [DateApproved]             DATETIMEOFFSET (7) NULL,
+    [EligibilityDetermination] NVARCHAR (MAX)     NULL,
+    [EmployeeID]               BIGINT             NULL,
+    [File]                     NVARCHAR (MAX)     NULL,
+    [InvestigationID]          BIGINT             NULL,
+    [Reason]                   NVARCHAR (MAX)     NULL,
+    [ReviewNotes]              NVARCHAR (MAX)     NULL,
+    [Status]                   NVARCHAR (MAX)     NULL,
+    [Type]                     INT                DEFAULT ((0)) NOT NULL,
+    [VendorID]                 BIGINT             NULL,
+    CONSTRAINT [PK_CertificateReports] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
