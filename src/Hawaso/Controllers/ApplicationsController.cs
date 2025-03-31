931acf6e78dd834ac4a198f9fbf22c891ab0d768
Park YongJune <redplus@redplus.net>
2025-04-01 07:18:41 +0900

    Add ApplicationsController and related views.

---
+++ src/Hawaso/Controllers/ApplicationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
