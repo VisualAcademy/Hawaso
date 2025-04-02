67de075443d7d4f52eef83a37b747b3aaf64c973
Park YongJune <redplus@redplus.net>
2025-04-03 05:46:06 +0900

    Add ContactTypes script file.

---
+++ src/Hawaso.SqlServer/Vendors/ContactTypes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ContactTypes] (
+    [ID] BIGINT IDENTITY (1, 1) NOT NULL,
+    [Active] BIT DEFAULT ((1)) NOT NULL,
+    [CreatedAt] DATETIMEOFFSET (7) NOT NULL,
+    [CreatedBy] NVARCHAR (70) NULL,
+    [Label] NVARCHAR (255) NULL,
+    [Description] NVARCHAR(MAX) NULL,
+    CONSTRAINT [PK_ContactTypes] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
