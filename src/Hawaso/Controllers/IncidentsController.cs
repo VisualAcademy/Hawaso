f156e7c98a7e98ad46d23ee872f63ea8735e283f
Park YongJune <redplus@redplus.net>
2025-04-17 01:26:23 +0900

    Add IncidentsController test class file

---
+++ src/Hawaso/Controllers/IncidentsController.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ReportGeneration(int id)
+        {
+            return View();
+        }
+    }
+}
