5ed0c92cac9d061c7d4c95f63ddb4626d8b7c2ad
Park YongJune <redplus@redplus.net>
2025-04-23 16:24:22 +0900

    .

--- src/All/Entities/AllowedIPRange.cs
+++ src/All/Entities/AllowedIPRange.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace All.Entities
 {
     public class AllowedIpRange
     {
         public int Id { get; set; }
-        public string StartIPRange { get; set; }
-        public string EndIPRange { get; set; }
+
+        [Display(Name = "Start IP Range")]
+        public string StartIpRange { get; set; }
+
+        [Display(Name = "End IP Range")]
+        public string EndIpRange { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Create Date")]
         public DateTime CreateDate { get; set; } = DateTime.Now; // 기본값 설정
+
+        [Display(Name = "Tenant ID")]
         public long TenantId { get; set; }
     }
 }
