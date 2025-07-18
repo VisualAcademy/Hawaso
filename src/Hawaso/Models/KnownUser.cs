399e8185f2183a2142cdd9bd592cfdb7ac695804
Park YongJune <redplus@redplus.net>
2025-07-19 02:28:27 +0900

    Add KnownUser model class

---
+++ src/Hawaso/Models/KnownUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hawaso.Models
+{
+    public class KnownUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
