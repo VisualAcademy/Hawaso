19589f9d6ea87998405188078bf60b8250d8b4b0
Park YongJune <redplus@redplus.net>
2025-08-10 20:52:25 +0900

    .

--- src/Hawaso.Models/Geography/Address.cs
+++ src/Hawaso.Models/Geography/Address.cs
@@ -6,11 +6,11 @@ namespace Azunt.Geography
     public class Address
     {
         public string Line1 { get; set; } = "";
-        public string? Line2 { get; set; }
+        public string Line2 { get; set; }
         public string City { get; set; } = "";                 // Locality
         public string PostalCode { get; set; } = "";
         public string CountryCode { get; set; } = "US";        // FK -> Country
-        public string? SubdivisionCode { get; set; }           // FK part -> Subdivision
-        public string? RegionFreeText { get; set; }            // Subdivision 미보유 국가 대비
+        public string SubdivisionCode { get; set; }           // FK part -> Subdivision
+        public string RegionFreeText { get; set; }            // Subdivision 미보유 국가 대비
     }
 }
--- src/Hawaso.Models/Geography/Country.cs
+++ src/Hawaso.Models/Geography/Country.cs
@@ -3,10 +3,10 @@
     public class Country
     {
         // ISO 3166-1 alpha-2
-        public string Code { get; set; } = null!; // "US", "CA", "KR"
-        public string Name { get; set; } = null!;
-        public string? FullName { get; set; }       // 정식 명칭(선택)
-        public string? Alpha3 { get; set; }         // ISO 3166-1 alpha-3 (선택)
+        public string Code { get; set; } // "US", "CA", "KR"
+        public string Name { get; set; }
+        public string FullName { get; set; }       // 정식 명칭(선택)
+        public string Alpha3 { get; set; }         // ISO 3166-1 alpha-3 (선택)
         public int? NumericCode { get; set; }       // 840, 124, 410...
     }
 }
