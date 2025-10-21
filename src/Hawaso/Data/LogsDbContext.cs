1fef7bff67f7d8829890312b150476322f697574
Park YongJune <redplus@redplus.net>
2025-10-22 02:13:05 +0900

    LogsDbContext.cs

---
+++ src/Hawaso/Data/LogsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Azunt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Azunt.Data
+{
+    public class LogsDbContext : DbContext
+    {
+        public LogsDbContext(DbContextOptions<LogsDbContext> options) : base(options) { }
+
+        public DbSet<AppLog> AppLogs => Set<AppLog>();
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AppLog>(e =>
+            {
+                e.ToTable("AppLogs"); // dbo.AppLogs
+                e.HasNoKey();         // No Id key in schema
+                e.HasIndex(nameof(AppLog.TimeStamp));
+                e.HasIndex(nameof(AppLog.Level));
+            });
+        }
+    }
+}
