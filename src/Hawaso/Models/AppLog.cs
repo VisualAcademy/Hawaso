6ed5fd34707068244a3266c3feaca5a1a98031ab
Park YongJune <redplus@redplus.net>
2025-10-22 02:12:49 +0900

    AppLog.cs

---
+++ src/Hawaso/Models/AppLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Azunt.Models
+{
+    // Serilog standard table schema (no Id)
+    public class AppLog
+    {
+        public string? Message { get; set; }
+        public string? MessageTemplate { get; set; }
+        public string? Level { get; set; }
+        public DateTimeOffset? TimeStamp { get; set; }
+        public string? Exception { get; set; }
+        public string? Properties { get; set; }
+    }
+}
