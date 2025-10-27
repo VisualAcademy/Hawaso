cccbdeab20a81cdf5603fbb6dafbf492eee918ec
Park YongJune <redplus@redplus.net>
2025-10-28 07:43:29 +0900

    Add [MailingAddress_City], [MailingAddress_Country] colums

--- src/Hawaso.SqlServer/dbo/Tables/Vendors.sql
+++ src/Hawaso.SqlServer/dbo/Tables/Vendors.sql
@@ -6,5 +6,8 @@
     [LicenseNumber]              NVARCHAR (35)      NULL,
     [LicenseDate]                DATETIME2 (7)      NULL,
     [LicenseRenewalDate]         DATETIME2 (7)      NULL,
-    [LicenseExpirationDate]      DATETIME2 (7)      NULL
+    [LicenseExpirationDate]      DATETIME2 (7)      NULL,
+
+    [MailingAddress_City]        NVARCHAR (MAX)     NULL,
+    [MailingAddress_Country]     NVARCHAR (MAX)     NULL,
 );
