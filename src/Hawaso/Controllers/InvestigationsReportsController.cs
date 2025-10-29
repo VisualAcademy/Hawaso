ad51747c23f47763843a0b7ce57c1b06e0004883
Park YongJune <redplus@redplus.net>
2025-10-29 13:43:49 +0900

    InvestigationsReportsController

---
+++ src/Hawaso/Controllers/InvestigationsReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class InvestigationsReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
