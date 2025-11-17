8475b15b04701927346b0a5be53de1f40bfb58a7
Park YongJune <redplus@redplus.net>
2025-11-18 03:29:54 +0900

    Add LogoutRedirectController for signing out and redirecting to Login with returnUrl

---
+++ src/Hawaso/Controllers/LogoutRedirectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hawaso.Controllers
+{
+    [IgnoreAntiforgeryToken]
+    public class LogoutRedirectController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LogoutRedirectController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet("/LogoutRedirect")]
+        public async Task<IActionResult> Index(string returnUrl = "/")
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                await _signInManager.SignOutAsync();
+            }
+
+            var loginUrl = $"/Identity/Account/Login?returnUrl={Uri.EscapeDataString(returnUrl)}";
+
+            return Redirect(loginUrl);
+        }
+    }
+}
