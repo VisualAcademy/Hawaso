4f6e288283884d5915f4932f0092ca790330dfe8
Park YongJune <redplus@redplus.net>
2025-11-25 13:29:27 +0900

    Create AspNetUserPasskeys table

---
+++ src/Hawaso.SqlServer/01_Identity/AspNetUserPasskeys.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[AspNetUserPasskeys] (
+    [CredentialId] VARBINARY (1024) NOT NULL,
+    [UserId]       NVARCHAR (450)   NOT NULL,
+    [Data]         NVARCHAR (MAX)   NOT NULL,
+    CONSTRAINT [PK_AspNetUserPasskeys] PRIMARY KEY CLUSTERED ([CredentialId] ASC),
+    CONSTRAINT [FK_AspNetUserPasskeys_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AspNetUserPasskeys_UserId]
+    ON [dbo].[AspNetUserPasskeys]([UserId] ASC);
+
