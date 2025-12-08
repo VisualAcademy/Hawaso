0a87409477dbccf23afe616138eea6bfd218d86b
Park YongJune <redplus@redplus.net>
2025-12-08 23:44:03 +0900

    Add StripeSettings.cs class file

---
+++ src/Hawaso/Settings/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Azunt.Web.Settings
+{
+    public class StripeSettings
+    {
+        public string PublishableKey { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+    }
+}
