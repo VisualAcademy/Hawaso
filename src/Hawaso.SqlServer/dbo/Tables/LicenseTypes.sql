c24fa718cb6abb1aff1da6e7da2d4140aee818e7
Park YongJune <redplus@redplus.net>
2025-12-09 07:40:03 +0900

    .

--- src/Hawaso.SqlServer/dbo/Tables/LicenseTypes.sql
+++ src/Hawaso.SqlServer/dbo/Tables/LicenseTypes.sql
@@ -6,7 +6,7 @@
     [Type]                  NVARCHAR(450) NULL,                        -- 라이선스 유형
     [Description]           NVARCHAR(MAX) NULL,                        -- 설명
     [ApplicantType]         INT NULL,                                  -- 신청자 유형
-    [BgRequired]            BIT NULL,                                  -- 백그라운드 체크 필요 여부
+    [BgRequired]            BIT NULL Default 0,                        -- 백그라운드 체크 필요 여부
     [IsApplicationRequired] BIT NOT NULL DEFAULT 1,                    -- 신청서 필요 여부 (기본값: 체크됨)
     [IsCertificateRequired] BIT NULL DEFAULT 0
 );
