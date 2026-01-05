0a198f5e63942282f53f3d99ba2d273031055f45
Park YongJune <redplus@redplus.net>
2026-01-05 23:25:04 +0900

    Add StripePaymentsController class file

---
+++ src/Hawaso/Controllers/StripePaymentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class StripePaymentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
