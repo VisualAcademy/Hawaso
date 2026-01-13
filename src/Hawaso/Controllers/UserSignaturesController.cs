3f18e77d00a7614124a277e69206fabcd77afca7
Park YongJune <redplus@redplus.net>
2026-01-14 05:25:40 +0900

    Add UserSignaturesController class file

---
+++ src/Hawaso/Controllers/UserSignaturesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class UserSignaturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
