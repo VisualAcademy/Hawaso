222a6bada1ce5b83a717ebff8102d2edfd6a5487
Park YongJune <redplus@redplus.net>
2026-01-24 10:10:51 +0900

    Use injected ILogger in HomeController to resolve CS9113 warning

--- src/Hawaso/Controllers/HomeController.cs
+++ src/Hawaso/Controllers/HomeController.cs
@@ -2,10 +2,33 @@
 
 public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    public IActionResult Index() => View();
+    private readonly ILogger<HomeController> _logger = logger;
 
-    public IActionResult Privacy() => View();
+    public IActionResult Index()
+    {
+        _logger.LogInformation("Home/Index accessed");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        _logger.LogInformation("Home/Privacy accessed");
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    public IActionResult Error()
+    {
+        var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+        _logger.LogError(
+            "Error page accessed. RequestId: {RequestId}",
+            requestId
+        );
+
+        return View(new ErrorViewModel
+        {
+            RequestId = requestId
+        });
+    }
 }
