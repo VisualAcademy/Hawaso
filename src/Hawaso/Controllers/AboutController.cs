fe5f4cc63e4fca45dcb2d39a0a3addde6f1978a0
Park YongJune <redplus@redplus.net>
2026-01-27 03:57:53 +0900

    Add logging to AboutController index action

--- src/Hawaso/Controllers/AboutController.cs
+++ src/Hawaso/Controllers/AboutController.cs
@@ -1,6 +1,32 @@
-﻿namespace Hawaso.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-public class AboutController : Controller
+namespace Hawaso.Controllers
 {
-    public IActionResult Index() => View();
+    public class AboutController : Controller
+    {
+        private readonly ILogger<AboutController> _logger;
+
+        public AboutController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("About page visited at {Time}", DateTime.UtcNow);
+
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while loading About page");
+
+                // 사용자에게는 일반 메시지
+                return StatusCode(500);
+            }
+        }
+    }
 }
