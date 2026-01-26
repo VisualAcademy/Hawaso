8c59a44b1d2cd03c64d31799f784a3f6f6047ce7
Park YongJune <redplus@redplus.net>
2026-01-27 03:58:35 +0900

    Add logging to AnalyticsController index action

--- src/Hawaso/Controllers/AnalyticsController.cs
+++ src/Hawaso/Controllers/AnalyticsController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Hawaso.Controllers
 {
     public class AnalyticsController : Controller
     {
+        private readonly ILogger<AnalyticsController> _logger;
+
+        public AnalyticsController(ILogger<AnalyticsController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Analytics Index page visited at {Time}", DateTime.UtcNow);
+
             return View();
         }
     }
