446025c22757a2f455a395073a30c01b917b0209
Park YongJune <redplus@redplus.net>
2026-01-26 19:49:39 +0900

    Initialize ConnectionString with string.Empty to fix nullability warning

--- src/Hawaso/Components/Pages/VendorPages/Models/05_TenantPermanentDelete.cs
+++ src/Hawaso/Components/Pages/VendorPages/Models/05_TenantPermanentDelete.cs
@@ -2,6 +2,6 @@
 {
     public class TenantPermanentDelete
     {
-        public string ConnectionString { get; set; }
+        public string ConnectionString { get; set; } = string.Empty;
     }
 }
