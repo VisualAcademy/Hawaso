2411317c5100d3fba40836276091b0636218e705
Park YongJune <redplus@redplus.net>
2026-02-22 04:00:20 +0900

    Add EmployeePhotosController

---
+++ src/Hawaso/Controllers/EmployeePhotosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class EmployeePhotosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
