a24a3290583c972cea225038efa1cdb47bf82acb
Park YongJune <redplus@redplus.net>
2026-03-02 15:34:30 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/AccessDeniedDueToIP.cshtml.cs
+++ src/Hawaso/Pages/AccessDeniedDueToIP.cshtml.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Hawaso.Pages
-{
-    [AllowAnonymous]
-    public class AccessDeniedDueToIPModel : PageModel
-    {
-        public string ClientIp { get; set; } = string.Empty;
+namespace Hawaso.Pages;
 
-        public void OnGet()
-        {
-            ClientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown";
-            ViewData["ClientIp"] = ClientIp;
-        }
+[AllowAnonymous]
+public class AccessDeniedDueToIPModel : PageModel
+{
+    public string ClientIp { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+        ClientIp = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown";
+        ViewData["ClientIp"] = ClientIp;
     }
 }
