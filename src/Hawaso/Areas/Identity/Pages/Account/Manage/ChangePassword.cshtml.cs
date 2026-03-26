f88d292820a57252087e41b9300762329b33be14
Park YongJune <redplus@redplus.net>
2026-03-27 02:23:33 +0900

    Initialize ChangePassword input model to resolve non-nullable property warning

--- src/Hawaso/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ src/Hawaso/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Hawaso.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 
@@ -22,28 +23,28 @@ public class ChangePasswordModel : PageModel
     }
 
     [BindProperty]
-    public InputModel Input { get; set; }
+    public InputModel Input { get; set; } = new();
 
     [TempData]
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 
     public class InputModel
     {
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
+        public string OldPassword { get; set; } = string.Empty;
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
-        public string NewPassword { get; set; }
+        public string NewPassword { get; set; } = string.Empty;
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -83,6 +84,7 @@ public class ChangePasswordModel : PageModel
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
+
             return Page();
         }
 
