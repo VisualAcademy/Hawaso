6ee8fc8d49d7b8a9f18a3517680aace1a5b1e4c0
Park YongJune <redplus@redplus.net>
2026-04-01 20:50:07 +0900

    Fix nullable warnings in UserAndRoleInitializer

--- src/Hawaso/Infrastructures/UserAndRoleInitializer.cs
+++ src/Hawaso/Infrastructures/UserAndRoleInitializer.cs
@@ -5,7 +5,7 @@ namespace Hawaso.Infrastructures;
 
 public class UserAndRoleInitializer
 {
-    #region Create BuiltIn Users and Roles 
+    #region Create BuiltIn Users and Roles
     /// <summary>
     /// 내장 사용자 및 그룹(역할) 생성
     /// </summary>
@@ -13,7 +13,7 @@ public class UserAndRoleInitializer
     {
         //[0] DbContext 개체 생성
         var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-        dbContext.Database.EnsureCreated(); // 데이터베이스가 생성되어 있는지 확인 
+        dbContext.Database.EnsureCreated(); // 데이터베이스가 생성되어 있는지 확인
 
         // 기본 내장 사용자 및 역할이 하나도 없으면(즉, 처음 데이터베이스 생성이라면)
         if (!dbContext.Users.Any() && !dbContext.Roles.Any())
@@ -22,19 +22,7 @@ public class UserAndRoleInitializer
 
             //[1] Groups(Roles)
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
-            //[1][1] ('Administrators', '관리자 그룹', 'Group', '응용 프로그램을 총 관리하는 관리 그룹 계정')
-            //[1][2] ('Everyone', '전체 사용자 그룹', 'Group', '응용 프로그램을 사용하는 모든 사용자 그룹 계정')
-            //[1][3] ('Users', '일반 사용자 그룹', 'Group', '일반 사용자 그룹 계정')
-            //[1][4] ('Guests', '게스트 그룹', 'Group', '게스트 사용자 그룹 계정')
-            //string[] roleNames = { Dul.Roles.Administrators.ToString(), Dul.Roles.Everyone.ToString(), Dul.Roles.Users.ToString(), Dul.Roles.Guests.ToString() };
-            //foreach (var roleName in roleNames)
-            //{
-            //    var roleExist = await roleManager.RoleExistsAsync(roleName);
-            //    if (!roleExist)
-            //    {
-            //        await roleManager.CreateAsync(new ApplicationRole { Name = roleName, NormalizedName = roleName.ToUpper(), Description = "" }); // 빌트인 그룹 생성
-            //    }
-            //}
+
             //[1][1] Administrators
             var administrators = new ApplicationRole
             {
@@ -42,10 +30,11 @@ public class UserAndRoleInitializer
                 NormalizedName = Dul.Roles.Administrators.ToString().ToUpper(),
                 Description = "응용 프로그램을 총 관리하는 관리 그룹 계정"
             };
-            if (!(await roleManager.RoleExistsAsync(administrators.Name)))
+            if (!await roleManager.RoleExistsAsync(administrators.Name!))
             {
                 await roleManager.CreateAsync(administrators);
             }
+
             //[1][2] Everyone
             var everyone = new ApplicationRole
             {
@@ -53,10 +42,11 @@ public class UserAndRoleInitializer
                 NormalizedName = Dul.Roles.Everyone.ToString().ToUpper(),
                 Description = "응용 프로그램을 사용하는 모든 사용자 그룹 계정"
             };
-            if (!(await roleManager.RoleExistsAsync(everyone.Name)))
+            if (!await roleManager.RoleExistsAsync(everyone.Name!))
             {
                 await roleManager.CreateAsync(everyone);
             }
+
             //[1][3] Users
             var users = new ApplicationRole
             {
@@ -64,10 +54,11 @@ public class UserAndRoleInitializer
                 NormalizedName = Dul.Roles.Users.ToString().ToUpper(),
                 Description = "일반 사용자 그룹 계정"
             };
-            if (!(await roleManager.RoleExistsAsync(users.Name)))
+            if (!await roleManager.RoleExistsAsync(users.Name!))
             {
                 await roleManager.CreateAsync(users);
             }
+
             //[1][4] Guests
             var guests = new ApplicationRole
             {
@@ -75,33 +66,34 @@ public class UserAndRoleInitializer
                 NormalizedName = Dul.Roles.Guests.ToString().ToUpper(),
                 Description = "게스트 사용자 그룹 계정"
             };
-            if (!(await roleManager.RoleExistsAsync(guests.Name)))
+            if (!await roleManager.RoleExistsAsync(guests.Name!))
             {
                 await roleManager.CreateAsync(guests);
             }
 
             //[2] Users
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
             //[2][1] Administrator
             // ('Administrator', '관리자', 'User', '응용 프로그램을 총 관리하는 사용자 계정')
-            ApplicationUser administrator = await userManager.FindByEmailAsync($"administrator@{domainName}");
-            if (administrator == null)
+            ApplicationUser? administrator = await userManager.FindByEmailAsync($"administrator@{domainName}");
+            if (administrator is null)
             {
-                administrator = new ApplicationUser()
+                administrator = new ApplicationUser
                 {
                     UserName = $"administrator@{domainName}",
                     Email = $"administrator@{domainName}",
-                    EmailConfirmed = true, // 기본 템플릿에는 이메일 인증 과정을 거치기때문에 EmailConfirmed 속성을 true로 설정해야 함.
+                    EmailConfirmed = true, // 기본 템플릿에는 이메일 인증 과정을 거치기 때문에 true
                 };
                 await userManager.CreateAsync(administrator, "Pa$$w0rd");
             }
 
             //[2][2] Guest
             // ('Guest', '게스트 사용자', 'User', '게스트 사용자 계정')
-            ApplicationUser guest = await userManager.FindByEmailAsync($"guest@{domainName}");
-            if (guest == null)
+            ApplicationUser? guest = await userManager.FindByEmailAsync($"guest@{domainName}");
+            if (guest is null)
             {
-                guest = new ApplicationUser()
+                guest = new ApplicationUser
                 {
                     UserName = "Guest",
                     Email = $"guest@{domainName}",
@@ -112,10 +104,10 @@ public class UserAndRoleInitializer
 
             //[2][3] Anonymous
             // ('Anonymous', '익명 사용자', 'User', '익명 사용자 계정')
-            ApplicationUser anonymous = await userManager.FindByEmailAsync($"anonymous@{domainName}");
-            if (anonymous == null)
+            ApplicationUser? anonymous = await userManager.FindByEmailAsync($"anonymous@{domainName}");
+            if (anonymous is null)
             {
-                anonymous = new ApplicationUser()
+                anonymous = new ApplicationUser
                 {
                     UserName = "Anonymous",
                     Email = $"anonymous@{domainName}",
