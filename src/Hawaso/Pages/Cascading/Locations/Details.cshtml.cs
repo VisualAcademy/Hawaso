c14d580b96d40c9e43f41c719d41d8e44a4c71dd
Park YongJune <redplus@redplus.net>
2026-04-24 01:57:38 +0900

    Fix null reference warning in Location Details

--- src/Hawaso/Pages/Cascading/Locations/Details.cshtml.cs
+++ src/Hawaso/Pages/Cascading/Locations/Details.cshtml.cs
@@ -1,27 +1,31 @@
-﻿#nullable disable
+﻿#nullable enable
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
 namespace VisualAcademy.Pages.Cascading.Locations;
 
 public class DetailsModel(Hawaso.Data.ApplicationDbContext context) : PageModel
 {
-    public Location Location { get; set; }
+    public Location? Location { get; private set; }
 
     public async Task<IActionResult> OnGetAsync(int? id)
     {
-        if (id == null)
+        if (id is null)
         {
             return NotFound();
         }
 
         Location = await context.Locations
-            .Include(l => l.PropertyRef).FirstOrDefaultAsync(m => m.Id == id);
+            .Include(l => l.PropertyRef)
+            .FirstOrDefaultAsync(m => m.Id == id.Value);
 
-        if (Location == null)
+        if (Location is null)
         {
             return NotFound();
         }
+
         return Page();
     }
 }
