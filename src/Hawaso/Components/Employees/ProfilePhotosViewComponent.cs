0e4bfb03fb7430f8c210369d90898f46db3349e0
Park YongJune <redplus@redplus.net>
2026-04-25 23:28:05 +0900

    Add ProfilePhotosViewComponent

---
+++ src/Hawaso/Components/Employees/ProfilePhotosViewComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Hawaso.Components.Employees
+{
+    public class ProfilePhotosViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
