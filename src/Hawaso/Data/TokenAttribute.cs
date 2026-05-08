4263c32531e69f677e9adaa7ed1a91eb84a2b020
Park YongJune <redplus@redplus.net>
2026-05-08 20:13:01 +0900

    Add Token Attribute class file

---
+++ src/Hawaso/Data/TokenAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Hawaso.Data
+{
+    public class TokenAttribute : Attribute
+    {
+        public TokenAttribute()
+        {
+        }
+
+        public TokenAttribute(string token)
+        {
+            this.Token = token;
+        }
+
+        public string Token { get; set; }
+
+        public string Prefix { get; set; }
+    }
+}
