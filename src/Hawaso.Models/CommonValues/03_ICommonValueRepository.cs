94bbdd8dc3f018d2184fd0d4ed5f2ca63d7c9d07
Park YongJune <redplus@redplus.net>
2026-05-14 10:04:12 +0900

    Convert to file-scoped namespace

--- src/Hawaso.Models/CommonValues/03_ICommonValueRepository.cs
+++ src/Hawaso.Models/CommonValues/03_ICommonValueRepository.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Hawaso.Models.CommonValues
-{
-    /// <summary>
-    /// [3] Repository Interface
-    /// </summary>
-    public interface ICommonValueRepository : ICommonValueCrudRepository<CommonValue>
-    {
-        Task<Tuple<int, int>> GetStatus(int parentId);
-        Task<bool> DeleteAllByParentId(int parentId);
-        Task<SortedList<int, double>> GetMonthlyCreateCountAsync();
+namespace Hawaso.Models.CommonValues;
 
-        Task<string> FindValueByTest(string text);
-    }
+/// <summary>
+/// [3] Repository Interface
+/// </summary>
+public interface ICommonValueRepository : ICommonValueCrudRepository<CommonValue>
+{
+    Task<Tuple<int, int>> GetStatus(int parentId);
+    Task<bool> DeleteAllByParentId(int parentId);
+    Task<SortedList<int, double>> GetMonthlyCreateCountAsync();
+
+    Task<string> FindValueByTest(string text);
 }
