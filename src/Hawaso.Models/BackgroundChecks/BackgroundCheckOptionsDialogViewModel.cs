b9cbcfa9813d850ae8d951e76485142a5e0ef2bd
Park YongJune <redplus@redplus.net>
2026-05-14 10:03:10 +0900

    Convert to file-scoped namespace

--- src/Hawaso.Models/BackgroundChecks/BackgroundCheckOptionsDialogViewModel.cs
+++ src/Hawaso.Models/BackgroundChecks/BackgroundCheckOptionsDialogViewModel.cs
@@ -1,19 +1,17 @@
-﻿namespace Hawaso.Models
+﻿namespace Hawaso.Models;
+
+public class BackgroundCheckOptionsDialogViewModel
 {
-    public class BackgroundCheckOptionsDialogViewModel
+    public string Controller { get; set; }
+
+    public string Action { get; set; }
+
+    public string BGCheckID { get; set; }
+
+    public BackgroundCheckOptionsDialogViewModel(string controller, string action, string bgCheckID)
     {
-        public string Controller { get; set; }
-
-        public string Action { get; set; }
-
-        public string BGCheckID { get; set; }
-
-        public BackgroundCheckOptionsDialogViewModel(string controller, string action, string bgCheckID)
-        {
-            Controller = controller;
-            Action = action;
-            BGCheckID = bgCheckID;
-        }
+        Controller = controller;
+        Action = action;
+        BGCheckID = bgCheckID;
     }
-
 }
