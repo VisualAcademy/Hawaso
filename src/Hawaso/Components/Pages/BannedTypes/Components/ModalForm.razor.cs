e9cf5b692d019d84285b53b858ed59d523723227
Park YongJune <redplus@redplus.net>
2026-05-30 02:22:05 +0900

    Fix nullability warnings in banned type model form

--- src/Hawaso/Components/Pages/BannedTypes/Components/ModalForm.razor.cs
+++ src/Hawaso/Components/Pages/BannedTypes/Components/ModalForm.razor.cs
@@ -6,101 +6,115 @@ namespace VisualAcademy.Components.Pages.BannedTypes.Components;
 public partial class ModalForm
 {
     #region Properties
+
     /// <summary>
-    /// (글쓰기/글수정)모달 다이얼로그를 표시할건지 여부 
+    /// (글쓰기/글수정) 모달 다이얼로그를 표시할지 여부
     /// </summary>
     public bool IsShow { get; set; } = false;
+
     #endregion
 
     #region Public Methods
+
     /// <summary>
-    /// 폼 보이기 
+    /// 폼 보이기
     /// </summary>
-    public void Show() => IsShow = true; // 현재 인라인 모달 폼 보이기
+    public void Show() => IsShow = true;
 
     /// <summary>
     /// 폼 닫기
     /// </summary>
-    public void Hide() => IsShow = false; // 현재 인라인 모달 폼 숨기기
+    public void Hide() => IsShow = false;
+
     #endregion
 
     #region Parameters
+
     [Parameter]
-    public string UserName { get; set; }
+    public string UserName { get; set; } = "Anonymous";
 
     /// <summary>
     /// 폼의 제목 영역
     /// </summary>
     [Parameter]
-    public RenderFragment EditorFormTitle { get; set; }
+    public RenderFragment? EditorFormTitle { get; set; }
 
     /// <summary>
-    /// 넘어온 모델 개체 
+    /// 넘어온 모델 개체
     /// </summary>
     [Parameter]
-    public BannedTypeModel ModelSender { get; set; }
+    public BannedTypeModel ModelSender { get; set; } = new();
 
-    public BannedTypeModel ModelEdit { get; set; }
-
-    #region Lifecycle Methods
-    // 넘어온 Model 값을 수정 전용 ModelEdit에 담기 
-    protected override void OnParametersSet()
-    {
-        ModelEdit = new BannedTypeModel();
-        ModelEdit.Id = ModelSender.Id;
-        ModelEdit.Name = ModelSender.Name;
-        // 더 많은 정보는 여기에서...
-    }
-    #endregion
+    public BannedTypeModel ModelEdit { get; set; } = new();
 
     /// <summary>
-    /// 부모 컴포넌트에게 생성(Create)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
-    /// 학습 목적으로 Action 대리자 사용
+    /// 부모 컴포넌트에게 생성(Create)이 완료되었다고 보고
     /// </summary>
     [Parameter]
-    public Action CreateCallback { get; set; }
+    public Action? CreateCallback { get; set; }
 
     /// <summary>
-    /// 부모 컴포넌트에게 수정(Edit)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
-    /// 학습 목적으로 EventCallback 구조체 사용
+    /// 부모 컴포넌트에게 수정(Edit)이 완료되었다고 보고
     /// </summary>
     [Parameter]
     public EventCallback<bool> EditCallback { get; set; }
 
     [Parameter]
     public string ParentKey { get; set; } = "";
+
     #endregion
 
     #region Injectors
+
     /// <summary>
-    /// 리포지토리 클래스에 대한 참조 
+    /// 리포지토리 클래스에 대한 참조
     /// </summary>
     [Inject]
-    public IBannedTypeRepository RepositoryReference { get; set; }
+    public IBannedTypeRepository RepositoryReference { get; set; } = default!;
+
+    #endregion
+
+    #region Lifecycle Methods
+
+    /// <summary>
+    /// 넘어온 Model 값을 수정 전용 ModelEdit에 복사
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        ModelEdit = new BannedTypeModel
+        {
+            Id = ModelSender.Id,
+            Name = ModelSender.Name
+        };
+    }
 
     #endregion
 
     #region Event Handlers
-    protected async void CreateOrEditClick()
+
+    protected async Task CreateOrEditClick()
     {
-        // 변경 내용 저장
         ModelSender.Active = true;
         ModelSender.Name = ModelEdit.Name;
-        ModelSender.CreatedBy = UserName ?? "Anonymous";
+        ModelSender.CreatedBy = string.IsNullOrWhiteSpace(UserName)
+            ? "Anonymous"
+            : UserName;
 
         if (ModelSender.Id == 0)
         {
-            // Create
             ModelSender.CreatedAt = DateTime.UtcNow;
+
             await RepositoryReference.AddAsync(ModelSender);
+
             CreateCallback?.Invoke();
         }
         else
         {
-            // Edit
             await RepositoryReference.UpdateAsync(ModelSender);
+
             await EditCallback.InvokeAsync(true);
         }
     }
+
     #endregion
 }
