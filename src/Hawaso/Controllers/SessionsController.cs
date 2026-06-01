5cb5b00d76241c74f9546add1a29491876dbca93
Park YongJune <redplus@redplus.net>
2026-06-02 04:08:32 +0900

    Add SessionsController class file and view

---
+++ src/Hawaso/Controllers/SessionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
