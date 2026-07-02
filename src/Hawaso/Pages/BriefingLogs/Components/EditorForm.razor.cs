5cf78add223b681643e3760dddbbb2d156671cdb
Park YongJune <redplus@redplus.net>
2026-07-03 05:28:55 +0900

    Handle nullable editor form parameters and parent IDs safely.

--- src/Hawaso/Pages/BriefingLogs/Components/EditorForm.razor.cs
+++ src/Hawaso/Pages/BriefingLogs/Components/EditorForm.razor.cs
@@ -7,19 +7,19 @@ namespace Hawaso.Pages.BriefingLogs.Components;
 public partial class EditorForm
 {
     /// <summary>
-    /// 모달 다이얼로그를 표시할건지 여부 
+    /// 모달 다이얼로그를 표시할건지 여부
     /// </summary>
-    public bool IsShow { get; set; } = false;
+    public bool IsShow { get; set; }
 
-    private string parentId = "0";
+    private string parentId = string.Empty;
 
     [Parameter]
-    public string ParentKey { get; set; } = "";
+    public string ParentKey { get; set; } = string.Empty;
 
     protected int[] parentIds = { 1, 2, 3 };
 
     /// <summary>
-    /// 폼 보이기 
+    /// 폼 보이기
     /// </summary>
     public void Show() => IsShow = true;
 
@@ -32,19 +32,19 @@ public partial class EditorForm
     /// 폼의 제목 영역
     /// </summary>
     [Parameter]
-    public RenderFragment EditorFormTitle { get; set; }
+    public RenderFragment? EditorFormTitle { get; set; }
 
     /// <summary>
-    /// 넘어온 모델 개체 
+    /// 넘어온 모델 개체
     /// </summary>
     [Parameter]
-    public BriefingLog Model { get; set; }
+    public BriefingLog Model { get; set; } = new();
 
     /// <summary>
     /// 부모 컴포넌트에게 생성(Create)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
     /// </summary>
     [Parameter]
-    public Action CreateCallback { get; set; }
+    public Action? CreateCallback { get; set; }
 
     /// <summary>
     /// 부모 컴포넌트에게 수정(Edit)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
@@ -53,55 +53,65 @@ public partial class EditorForm
     public EventCallback<bool> EditCallback { get; set; }
 
     /// <summary>
-    /// 리포지토리 클래스에 대한 참조 
+    /// 리포지토리 클래스에 대한 참조
     /// </summary>
     [Inject]
-    public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; }
+    public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; } = default!;
+
+    [Inject]
+    public IBriefingLogFileStorageManager FileStorageManager { get; set; } = default!;
+
+    private IFileListEntry[]? selectedFiles;
 
     protected override void OnParametersSet()
     {
-        parentId = Model.ParentId.ToString();
+        parentId = Model.ParentId?.ToString() ?? string.Empty;
+
         if (parentId == "0")
         {
-            parentId = "";
+            parentId = string.Empty;
         }
     }
 
-    protected async void CreateOrEditClick()
+    protected async Task CreateOrEditClick()
     {
         #region 파일 업로드 관련 추가 코드 영역
-        if (selectedFiles != null && selectedFiles.Length > 0)
+
+        if (selectedFiles is { Length: > 0 })
         {
-            // 파일 업로드
             var file = selectedFiles.FirstOrDefault();
-            var fileName = "";
-            int fileSize = 0;
-            if (file != null)
+
+            if (file is not null)
             {
-                //file.Name = $"{DateTime.Now.ToString("yyyyMMddhhmmss")}{file.Name}";
-                fileName = file.Name;
-                fileSize = Convert.ToInt32(file.Size);
+                var fileName = file.Name ?? string.Empty;
+                var fileSize = Convert.ToInt32(file.Size);
 
                 //[A] byte[] 형태
                 //var ms = new MemoryStream();
                 //await file.Data.CopyToAsync(ms);
                 //await FileStorageManager.UploadAsync(ms.ToArray(), file.Name, "", true);
+
                 //[B] Stream 형태
                 //string folderPath = Path.Combine(WebHostEnvironment.WebRootPath, "files");
-                await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
+                await FileStorageManager.UploadAsync(file.Data, fileName, string.Empty, true);
 
                 Model.FileName = fileName;
                 Model.FileSize = fileSize;
             }
         }
+
         #endregion
 
-        if (!int.TryParse(parentId, out int newParentId))
+        if (int.TryParse(parentId, out var parsedParentId))
         {
-            newParentId = 0;
+            Model.ParentId = parsedParentId;
         }
-        Model.ParentId = newParentId;
-        Model.ParentKey = Model.ParentKey;
+        else
+        {
+            Model.ParentId = null;
+        }
+
+        Model.ParentKey = ParentKey;
 
         if (Model.Id == 0)
         {
@@ -115,12 +125,12 @@ public partial class EditorForm
             await UploadRepositoryAsyncReference.EditAsync(Model);
             await EditCallback.InvokeAsync(true);
         }
+
         //IsShow = false; // this.Hide()
     }
 
-    private IFileListEntry[] selectedFiles;
-    protected void HandleSelection(IFileListEntry[] files) => this.selectedFiles = files;
-
-    [Inject]
-    public IBriefingLogFileStorageManager FileStorageManager { get; set; }
+    protected void HandleSelection(IFileListEntry[] files)
+    {
+        selectedFiles = files;
+    }
 }
