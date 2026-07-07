ba74994b166c11879564a1e22f04d2f6e3383557
Park YongJune <redplus@redplus.net>
2026-07-07 19:10:17 +0900

    Use expression body for constructor

--- src/Hawaso/Controllers/AnalyticsController.cs
+++ src/Hawaso/Controllers/AnalyticsController.cs
@@ -4,10 +4,7 @@ public class AnalyticsController : Controller
 {
     private readonly ILogger<AnalyticsController> _logger;
 
-    public AnalyticsController(ILogger<AnalyticsController> logger)
-    {
-        _logger = logger;
-    }
+    public AnalyticsController(ILogger<AnalyticsController> logger) => _logger = logger;
 
     public IActionResult Index()
     {
