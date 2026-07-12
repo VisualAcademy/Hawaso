6136193239adaacfae986204cbbf33315e3dc8f0
Park YongJune <redplus@redplus.net>
2026-07-12 21:43:36 +0900

    Convert to file-scoped namespace

--- src/Hawaso/Pages/Libraries/Index.razor.cs
+++ src/Hawaso/Pages/Libraries/Index.razor.cs
@@ -1,115 +1,114 @@
 ﻿using Microsoft.AspNetCore.Components;
 using VisualAcademy.Models.Libraries;
 
-namespace Hawaso.Pages.Libraries
+namespace Hawaso.Pages.Libraries;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public ILibraryRepository UploadRepositoryAsyncReference { get; set; } = default!;
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; } = default!;
+
+    protected List<LibraryModel>? models;
+
+    protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
     {
-        [Inject]
-        public ILibraryRepository UploadRepositoryAsyncReference { get; set; } = default!;
+        PageNumber = 1,
+        PageIndex = 0,
+        PageSize = 10,
+        PagerButtonCount = 5
+    };
 
-        [Inject]
-        public NavigationManager NavigationManagerReference { get; set; } = default!;
-
-        protected List<LibraryModel>? models;
-
-        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
-        {
-            PageNumber = 1,
-            PageIndex = 0,
-            PageSize = 10,
-            PagerButtonCount = 5
-        };
-
-        protected override async Task OnInitializedAsync()
-        {
-            await DisplayData();
-        }
-
-        private async Task DisplayData()
-        {
-            var articleSet = await UploadRepositoryAsyncReference.GetArticles<int>(
-                pager.PageIndex,
-                pager.PageSize,
-                "",
-                this.searchQuery,
-                this.sortOrder,
-                0);
-
-            pager.RecordCount = articleSet.TotalCount;
-            models = articleSet.Items.ToList();
-
-            StateHasChanged();
-        }
-
-        protected void NameClick(int id)
-        {
-            NavigationManagerReference.NavigateTo($"/Libraries/Details/{id}");
-        }
-
-        protected async void PageIndexChanged(int pageIndex)
-        {
-            pager.PageIndex = pageIndex;
-            pager.PageNumber = pageIndex + 1;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-
-        #region Search
-        private string searchQuery = "";
-
-        protected async void Search(string query)
-        {
-            pager.PageIndex = 0;
-
-            this.searchQuery = query;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-        #endregion
-
-        #region Sorting
-        private string sortOrder = "";
-
-        protected async void SortByName()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Name";
-            }
-            else if (sortOrder == "Name")
-            {
-                sortOrder = "NameDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-
-        protected async void SortByTitle()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Title";
-            }
-            else if (sortOrder == "Title")
-            {
-                sortOrder = "TitleDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-        #endregion
+    protected override async Task OnInitializedAsync()
+    {
+        await DisplayData();
     }
+
+    private async Task DisplayData()
+    {
+        var articleSet = await UploadRepositoryAsyncReference.GetArticles<int>(
+            pager.PageIndex,
+            pager.PageSize,
+            "",
+            this.searchQuery,
+            this.sortOrder,
+            0);
+
+        pager.RecordCount = articleSet.TotalCount;
+        models = articleSet.Items.ToList();
+
+        StateHasChanged();
+    }
+
+    protected void NameClick(int id)
+    {
+        NavigationManagerReference.NavigateTo($"/Libraries/Details/{id}");
+    }
+
+    protected async void PageIndexChanged(int pageIndex)
+    {
+        pager.PageIndex = pageIndex;
+        pager.PageNumber = pageIndex + 1;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #region Search
+    private string searchQuery = "";
+
+    protected async void Search(string query)
+    {
+        pager.PageIndex = 0;
+
+        this.searchQuery = query;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+    #endregion
+
+    #region Sorting
+    private string sortOrder = "";
+
+    protected async void SortByName()
+    {
+        if (sortOrder == "")
+        {
+            sortOrder = "Name";
+        }
+        else if (sortOrder == "Name")
+        {
+            sortOrder = "NameDesc";
+        }
+        else
+        {
+            sortOrder = "";
+        }
+
+        await DisplayData();
+    }
+
+    protected async void SortByTitle()
+    {
+        if (sortOrder == "")
+        {
+            sortOrder = "Title";
+        }
+        else if (sortOrder == "Title")
+        {
+            sortOrder = "TitleDesc";
+        }
+        else
+        {
+            sortOrder = "";
+        }
+
+        await DisplayData();
+    }
+    #endregion
 }
