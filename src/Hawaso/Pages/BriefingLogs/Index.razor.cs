d2d0876f34724976ad668e79b7dfb385144cba75
Park YongJune <redplus@redplus.net>
2026-08-01 21:14:42 +0900

    Fix nullable briefing log list and async data handlers

--- src/Hawaso/Pages/BriefingLogs/Index.razor.cs
+++ src/Hawaso/Pages/BriefingLogs/Index.razor.cs
@@ -1,111 +1,114 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Zero.Models;
 
-namespace Hawaso.Pages.BriefingLogs
+namespace Hawaso.Pages.BriefingLogs;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; }
+        = default!;
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; }
+        = default!;
+
+    // 데이터가 로딩되기 전에는 null 상태를 사용합니다.
+    protected List<BriefingLog>? models;
+
+    protected readonly DulPager.DulPagerBase pager = new()
     {
-        [Inject]
-        public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; }
+        PageNumber = 1,
+        PageIndex = 0,
+        PageSize = 10,
+        PagerButtonCount = 5
+    };
 
-        [Inject]
-        public NavigationManager NavigationManagerReference { get; set; }
+    private string searchQuery = string.Empty;
+    private string sortOrder = string.Empty;
 
-        protected List<BriefingLog> models;
+    protected override async Task OnInitializedAsync()
+    {
+        await DisplayData();
+    }
 
-        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase()
+    private async Task DisplayData()
+    {
+        var articleSet =
+            await UploadRepositoryAsyncReference.GetArticles<int>(
+                pager.PageIndex,
+                pager.PageSize,
+                string.Empty,
+                searchQuery,
+                sortOrder,
+                0);
+
+        pager.RecordCount = articleSet.TotalCount;
+        models = articleSet.Items.ToList();
+    }
+
+    protected void NameClick(int id)
+    {
+        NavigationManagerReference.NavigateTo(
+            $"/BriefingLogs/Details/{id}");
+    }
+
+    // DulPagerComponent 콜백 형식과의 호환성을 위해
+    // async void를 유지합니다.
+    protected async void PageIndexChanged(int pageIndex)
+    {
+        pager.PageIndex = pageIndex;
+        pager.PageNumber = pageIndex + 1;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #region Search
+
+    // SearchBox 콜백 형식과의 호환성을 위해
+    // async void를 유지합니다.
+    protected async void Search(string query)
+    {
+        pager.PageIndex = 0;
+        pager.PageNumber = 1;
+
+        searchQuery = query ?? string.Empty;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #endregion
+
+    #region Sorting
+
+    // Razor onclick 이벤트에서 호출되므로 Task를 반환할 수 있습니다.
+    protected async Task SortByName()
+    {
+        sortOrder = sortOrder switch
         {
-            PageNumber = 1,
-            PageIndex = 0,
-            PageSize = 10,
-            PagerButtonCount = 5
+            "" => "Name",
+            "Name" => "NameDesc",
+            _ => string.Empty
         };
 
-        protected override async Task OnInitializedAsync()
-        {
-            await DisplayData();
-        }
-
-        private async Task DisplayData()
-        {
-            var articleSet = await UploadRepositoryAsyncReference.GetArticles<int>(pager.PageIndex, pager.PageSize, "", this.searchQuery, this.sortOrder, 0);
-            pager.RecordCount = articleSet.TotalCount;
-            models = articleSet.Items.ToList();
-
-            StateHasChanged();
-        }
-
-        protected void NameClick(int id)
-        {
-            NavigationManagerReference.NavigateTo($"/BriefingLogs/Details/{id}");
-        }
-
-        protected async void PageIndexChanged(int pageIndex)
-        {
-            pager.PageIndex = pageIndex;
-            pager.PageNumber = pageIndex + 1;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-
-        #region Search
-        private string searchQuery = "";
-
-        protected async void Search(string query)
-        {
-            pager.PageIndex = 0;
-
-            this.searchQuery = query;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-        #endregion
-
-        #region Sorting
-        private string sortOrder = "";
-
-        protected async void SortByName()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Name";
-            }
-            else if (sortOrder == "Name")
-            {
-                sortOrder = "NameDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-
-        protected async void SortByTitle()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Title";
-            }
-            else if (sortOrder == "Title")
-            {
-                sortOrder = "TitleDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-        #endregion
+        await DisplayData();
     }
+
+    protected async Task SortByTitle()
+    {
+        sortOrder = sortOrder switch
+        {
+            "" => "Title",
+            "Title" => "TitleDesc",
+            _ => string.Empty
+        };
+
+        await DisplayData();
+    }
+
+    #endregion
 }
