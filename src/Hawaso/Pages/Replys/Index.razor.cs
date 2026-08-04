c186bdb8e39265c6833a23718e653839edcdbd6a
Park YongJune <redplus@redplus.net>
2026-08-04 15:04:51 +0900

    Fix nullable reply list and async data handlers

--- src/Hawaso/Pages/Replys/Index.razor.cs
+++ src/Hawaso/Pages/Replys/Index.razor.cs
@@ -1,116 +1,117 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using VisualAcademy.Models.Replys;
 
-namespace Hawaso.Pages.Replys
+namespace Hawaso.Pages.Replys;
+
+public partial class Index
 {
-    public partial class Index
+    [Inject]
+    public IReplyRepository RepositoryReference { get; set; }
+        = default!;
+
+    [Inject]
+    public NavigationManager Nav { get; set; }
+        = default!;
+
+    // 데이터 로딩 전에는 null 상태를 사용합니다.
+    protected List<Reply>? models;
+
+    protected readonly DulPager.DulPagerBase pager = new()
     {
-        [Inject]
-        public IReplyRepository RepositoryReference { get; set; }
+        PageNumber = 1,
+        PageIndex = 0,
+        PageSize = 10,
+        PagerButtonCount = 5
+    };
 
-        [Inject]
-        public NavigationManager Nav { get; set; }
+    private string searchQuery = string.Empty;
+    private string sortOrder = string.Empty;
 
-        protected List<Reply> models;
+    #region Lifecycle Methods
 
-        protected DulPager.DulPagerBase pager = new DulPager.DulPagerBase() 
-        { 
-            PageNumber = 1,
-            PageIndex = 0,
-            PageSize = 10,
-            PagerButtonCount = 5
+    /// <summary>
+    /// 페이지 초기화 이벤트 처리기
+    /// </summary>
+    protected override async Task OnInitializedAsync()
+    {
+        await DisplayData();
+    }
+
+    #endregion
+
+    private async Task DisplayData()
+    {
+        var articleSet =
+            await RepositoryReference.GetArticlesAsync<int>(
+                pager.PageIndex,
+                pager.PageSize,
+                string.Empty,
+                searchQuery,
+                sortOrder,
+                0);
+
+        pager.RecordCount = articleSet.TotalCount;
+        models = articleSet.Items.ToList();
+    }
+
+    protected void NameClick(int id)
+    {
+        Nav.NavigateTo($"/Replys/Details/{id}");
+    }
+
+    // DulPagerComponent의 콜백 형식과 호환되도록 async void를 유지합니다.
+    protected async void PageIndexChanged(int pageIndex)
+    {
+        pager.PageIndex = pageIndex;
+        pager.PageNumber = pageIndex + 1;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #region Search
+
+    // SearchBox의 콜백 형식과 호환되도록 async void를 유지합니다.
+    protected async void Search(string query)
+    {
+        pager.PageIndex = 0;
+        pager.PageNumber = 1;
+
+        searchQuery = query ?? string.Empty;
+
+        await DisplayData();
+
+        StateHasChanged();
+    }
+
+    #endregion
+
+    #region Sorting
+
+    protected async Task SortByName()
+    {
+        sortOrder = sortOrder switch
+        {
+            "" => "Name",
+            "Name" => "NameDesc",
+            _ => string.Empty
         };
 
-        #region Lifecycle Methods
-        /// <summary>
-        /// 페이지 초기화 이벤트 처리기
-        /// </summary>
-        protected override async Task OnInitializedAsync()
-        {
-            await DisplayData();
-        } 
-        #endregion
-
-        private async Task DisplayData()
-        {
-            var articleSet = await RepositoryReference.GetArticlesAsync<int>(pager.PageIndex, pager.PageSize, "", this.searchQuery, this.sortOrder, 0);
-            pager.RecordCount = articleSet.TotalCount;
-            models = articleSet.Items.ToList();
-
-            StateHasChanged();
-        }
-
-        protected void NameClick(int id)
-        {
-            Nav.NavigateTo($"/Replys/Details/{id}");
-        }
-
-        protected async void PageIndexChanged(int pageIndex)
-        {
-            pager.PageIndex = pageIndex;
-            pager.PageNumber = pageIndex + 1;
-
-            await DisplayData();
-
-            StateHasChanged();
-        }
-
-        #region Search
-        private string searchQuery = "";
-
-        protected async void Search(string query)
-        {
-            pager.PageIndex = 0;
-
-            this.searchQuery = query;
-
-            await DisplayData();
-
-            StateHasChanged();
-        } 
-        #endregion
-
-        #region Sorting
-        private string sortOrder = "";
-
-        protected async void SortByName()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Name";
-            }
-            else if (sortOrder == "Name")
-            {
-                sortOrder = "NameDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        }
-
-        protected async void SortByTitle()
-        {
-            if (sortOrder == "")
-            {
-                sortOrder = "Title";
-            }
-            else if (sortOrder == "Title")
-            {
-                sortOrder = "TitleDesc";
-            }
-            else
-            {
-                sortOrder = "";
-            }
-
-            await DisplayData();
-        } 
-        #endregion
+        await DisplayData();
     }
+
+    protected async Task SortByTitle()
+    {
+        sortOrder = sortOrder switch
+        {
+            "" => "Title",
+            "Title" => "TitleDesc",
+            _ => string.Empty
+        };
+
+        await DisplayData();
+    }
+
+    #endregion
 }
