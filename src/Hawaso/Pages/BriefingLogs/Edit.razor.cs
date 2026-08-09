d2fd3e96ae26d174bcf844fce01599bcef66ee06
Park YongJune <redplus@redplus.net>
2026-08-09 20:58:48 +0900

    Fix nullable warnings in BriefingLogs Edit page

--- src/Hawaso/Pages/BriefingLogs/Edit.razor.cs
+++ src/Hawaso/Pages/BriefingLogs/Edit.razor.cs
@@ -7,78 +7,111 @@ namespace Hawaso.Pages.BriefingLogs;
 public partial class Edit
 {
     #region Fields
+
     protected int[] parentIds = { 1, 2, 3 };
 
-    protected string content = "";
+    protected string content = string.Empty;
 
     protected BriefingLog Model = new();
+
     #endregion
 
     #region Properties
-    public string ParentId { get; set; }
+
+    public string ParentId { get; set; } = string.Empty;
+
     #endregion
 
     #region Parameters
+
     [Parameter]
     public int Id { get; set; }
+
     #endregion
 
     #region Injectors
-    [Inject]
-    public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; }
 
     [Inject]
-    public NavigationManager NavigationManagerReference { get; set; }
+    public IBriefingLogRepository UploadRepositoryAsyncReference { get; set; } = default!;
+
+    [Inject]
+    public NavigationManager NavigationManagerReference { get; set; } = default!;
+
     #endregion
 
     #region Lifecycle Methods
+
     protected override async Task OnInitializedAsync()
     {
-        Model = await UploadRepositoryAsyncReference.GetByIdAsync(Id);
-        content = Dul.HtmlUtility.EncodeWithTabAndSpace(Model.Content);
-        ParentId = Model.ParentId.ToString();
+        Model = await UploadRepositoryAsyncReference.GetByIdAsync(Id)
+            ?? throw new InvalidOperationException(
+                $"BriefingLog with Id {Id} was not found.");
+
+        content = Dul.HtmlUtility.EncodeWithTabAndSpace(
+            Model.Content ?? string.Empty);
+
+        ParentId = Model.ParentId?.ToString() ?? string.Empty;
     }
+
     #endregion
-    
+
     #region Event Handlers
-    protected async void FormSubmit()
+
+    protected async Task FormSubmit()
     {
         int.TryParse(ParentId, out int parentId);
         Model.ParentId = parentId;
 
         #region 파일 업로드 관련 추가 코드 영역
-        if (selectedFiles != null && selectedFiles.Length > 0)
+
+        if (selectedFiles.Length > 0)
         {
-            // 파일 업로드
             var file = selectedFiles.FirstOrDefault();
-            var fileName = "";
-            int fileSize = 0;
+
             if (file != null)
             {
-                fileName = file.Name;
-                fileSize = Convert.ToInt32(file.Size);
+                var fileSize = Convert.ToInt32(file.Size);
 
-                // 첨부 파일 삭제 
-                await FileStorageManager.DeleteAsync(Model.FileName, "BriefingLogs");
+                // 기존 첨부 파일 삭제
+                if (!string.IsNullOrWhiteSpace(Model.FileName))
+                {
+                    await FileStorageManager.DeleteAsync(
+                        Model.FileName,
+                        "BriefingLogs");
+                }
 
-                // 다시 업로드
-                fileName = await FileStorageManager.UploadAsync(file.Data, file.Name, "", true);
+                // 새 파일 업로드
+                var fileName = await FileStorageManager.UploadAsync(
+                    file.Data,
+                    file.Name,
+                    "",
+                    true);
 
                 Model.FileName = fileName;
                 Model.FileSize = fileSize;
             }
         }
+
         #endregion
 
         await UploadRepositoryAsyncReference.EditAsync(Model);
+
         NavigationManagerReference.NavigateTo("/BriefingLogs");
     }
+
     #endregion
 
     #region Features - FileUpload
+
     [Inject]
-    public IBriefingLogFileStorageManager FileStorageManager { get; set; }
-    private IFileListEntry[] selectedFiles;
-    protected void HandleSelection(IFileListEntry[] files) => this.selectedFiles = files;
+    public IBriefingLogFileStorageManager FileStorageManager { get; set; } = default!;
+
+    private IFileListEntry[] selectedFiles = Array.Empty<IFileListEntry>();
+
+    protected void HandleSelection(IFileListEntry[] files)
+    {
+        selectedFiles = files;
+    }
+
     #endregion
 }
