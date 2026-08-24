0b751d77d83ab2c4c558de1ae3fb57bf1e908451
Park YongJune <redplus@redplus.net>
2026-08-24 20:06:23 +0900

    Fix nullable warnings and async handling in Reply editor form

--- src/Hawaso/Pages/Replys/Components/EditorForm.razor.cs
+++ src/Hawaso/Pages/Replys/Components/EditorForm.razor.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
-using BlazorInputFile;
+﻿using BlazorInputFile;
+using Microsoft.AspNetCore.Components;
 using VisualAcademy.Models.Replys;
 
 namespace Hawaso.Pages.Replys.Components;
@@ -7,100 +7,124 @@ namespace Hawaso.Pages.Replys.Components;
 public partial class EditorForm
 {
     #region Fields
-    private string parentId = "";
+
+    private string parentId = string.Empty;
 
     protected int[] parentIds = { 1, 2, 3 };
 
     /// <summary>
     /// 첨부 파일 리스트 보관
     /// </summary>
-    private IFileListEntry[] selectedFiles;
+    private IFileListEntry[] selectedFiles = Array.Empty<IFileListEntry>();
+
     #endregion
 
     #region Properties
+
     /// <summary>
-    /// 모달 다이얼로그를 표시할건지 여부 
+    /// 모달 다이얼로그를 표시할건지 여부
     /// </summary>
-    public bool IsShow { get; set; } = false;
+    public bool IsShow { get; set; }
+
     #endregion
 
     #region Public Methods
+
     /// <summary>
-    /// 폼 보이기 
+    /// 폼 보이기
     /// </summary>
-    public void Show() => IsShow = true; // 현재 인라인 모달 폼 보이기
+    public void Show() => IsShow = true;
 
     /// <summary>
     /// 폼 닫기
     /// </summary>
-    public void Hide() => IsShow = false; // 현재 인라인 모달 폼 숨기기
+    public void Hide() => IsShow = false;
+
     #endregion
 
     #region Parameters
+
     /// <summary>
     /// 폼의 제목 영역
     /// </summary>
     [Parameter]
-    public RenderFragment EditorFormTitle { get; set; } 
+    public RenderFragment? EditorFormTitle { get; set; }
 
     /// <summary>
-    /// 넘어온 모델 개체 
+    /// 넘어온 모델 개체
     /// </summary>
     [Parameter]
-    public Reply Model { get; set; }
+    public Reply Model { get; set; } = new Reply();
 
-    public Reply ModelEdit { get; set; }
-
-    #region Lifecycle Methods
-    // 넘어온 Model 값을 수정 전용 ModelEdit에 담기 
-    protected override void OnParametersSet()
-    {
-        ModelEdit = new Reply();
-        ModelEdit.Id = Model.Id; 
-        ModelEdit.Name = Model.Name;
-        ModelEdit.Title = Model.Title;
-        ModelEdit.Content = Model.Content;
-
-        // ParentId가 넘어온 값이 있으면... 즉, 0이 아니면 ParentId 드롭다운 리스트 기본값 선택
-        parentId = Model.ParentId.ToString();
-        if (parentId == "0")
-        {
-            parentId = "";
-        }
-    }
-    #endregion
+    public Reply ModelEdit { get; set; } = new Reply();
 
     /// <summary>
-    /// 부모 컴포넌트에게 생성(Create)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
+    /// 부모 컴포넌트에게 생성(Create)이 완료되었다고 보고하는 목적으로
+    /// 부모 컴포넌트에게 알림
     /// 학습 목적으로 Action 대리자 사용
     /// </summary>
     [Parameter]
-    public Action CreateCallback { get; set; }
+    public Action? CreateCallback { get; set; }
 
     /// <summary>
-    /// 부모 컴포넌트에게 수정(Edit)이 완료되었다고 보고하는 목적으로 부모 컴포넌트에게 알림
+    /// 부모 컴포넌트에게 수정(Edit)이 완료되었다고 보고하는 목적으로
+    /// 부모 컴포넌트에게 알림
     /// 학습 목적으로 EventCallback 구조체 사용
     /// </summary>
     [Parameter]
     public EventCallback<bool> EditCallback { get; set; }
 
     [Parameter]
-    public string ParentKey { get; set; } = "";
+    public string ParentKey { get; set; } = string.Empty;
+
     #endregion
 
     #region Injectors
+
     /// <summary>
-    /// 리포지토리 클래스에 대한 참조 
+    /// 리포지토리 클래스에 대한 참조
     /// </summary>
     [Inject]
-    public IReplyRepository RepositoryReference { get; set; }
+    public IReplyRepository RepositoryReference { get; set; } = default!;
 
     [Inject]
-    public IFileStorageManager FileStorageManagerReference { get; set; }
+    public IFileStorageManager FileStorageManagerReference { get; set; } = default!;
+
+    #endregion
+
+    #region Lifecycle Methods
+
+    /// <summary>
+    /// 넘어온 Model 값을 수정 전용 ModelEdit에 담기
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        ModelEdit = new Reply
+        {
+            Id = Model.Id,
+            Name = Model.Name,
+            Title = Model.Title,
+            Content = Model.Content
+        };
+
+        // ParentId가 넘어온 값이 있으면,
+        // 즉 0이 아니면 ParentId 드롭다운 리스트 기본값 선택
+        parentId = Model.ParentId.ToString();
+
+        if (parentId == "0")
+        {
+            parentId = string.Empty;
+        }
+    }
+
     #endregion
 
     #region Event Handlers
-    protected async void CreateOrEditClick()
+
+    /// <summary>
+    /// 생성 또는 수정 버튼 클릭 이벤트 처리기
+    /// </summary>
+    protected async Task CreateOrEditClick()
     {
         // 변경 내용 저장
         Model.Name = ModelEdit.Name;
@@ -108,44 +132,52 @@ public partial class EditorForm
         Model.Content = ModelEdit.Content;
 
         #region 파일 업로드 관련 추가 코드 영역
-        if (selectedFiles != null && selectedFiles.Length > 0)
-        {
-            // 파일 업로드
-            var file = selectedFiles.FirstOrDefault();
-            var fileName = "";
-            int fileSize = 0;
-            if (file != null)
-            {
-                //file.Name = $"{DateTime.Now.ToString("yyyyMMddhhmmss")}{file.Name}";
-                fileName = file.Name;
-                fileSize = Convert.ToInt32(file.Size);
 
-                //[A] byte[] 형태
-                //var ms = new MemoryStream();
-                //await file.Data.CopyToAsync(ms);
-                //await FileStorageManager.ReplyAsync(ms.ToArray(), file.Name, "", true);
-                //[B] Stream 형태
-                //string folderPath = Path.Combine(WebHostEnvironment.WebRootPath, "files");
-                await FileStorageManagerReference.UploadAsync(file.Data, file.Name, "", true);
+        if (selectedFiles.Length > 0)
+        {
+            var file = selectedFiles.FirstOrDefault();
+
+            if (file is not null)
+            {
+                string fileName = file.Name;
+                int fileSize = Convert.ToInt32(file.Size);
+
+                // [A] byte[] 형태
+                // var ms = new MemoryStream();
+                // await file.Data.CopyToAsync(ms);
+                // await FileStorageManager.ReplyAsync(
+                //     ms.ToArray(),
+                //     file.Name,
+                //     "",
+                //     true);
+
+                // [B] Stream 형태
+                await FileStorageManagerReference.UploadAsync(
+                    file.Data,
+                    file.Name,
+                    string.Empty,
+                    true);
 
                 Model.FileName = fileName;
                 Model.FileSize = fileSize;
-            }  
+            }
         }
+
         #endregion
 
         if (!int.TryParse(parentId, out int newParentId))
         {
             newParentId = 0;
         }
+
         Model.ParentId = newParentId;
-        Model.ParentKey = Model.ParentKey; 
+        Model.ParentKey = ParentKey;
 
         if (Model.Id == 0)
         {
             // Create
             await RepositoryReference.AddAsync(Model);
-            CreateCallback?.Invoke(); 
+            CreateCallback?.Invoke();
         }
         else
         {
@@ -155,9 +187,13 @@ public partial class EditorForm
         }
     }
 
+    /// <summary>
+    /// 파일 첨부 이벤트 처리기
+    /// </summary>
     protected void HandleSelection(IFileListEntry[] files)
     {
-        this.selectedFiles = files;
+        selectedFiles = files ?? Array.Empty<IFileListEntry>();
     }
+
     #endregion
 }
