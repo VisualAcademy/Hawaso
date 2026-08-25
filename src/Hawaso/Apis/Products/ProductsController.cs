d18466bd4ebb90029a6f318854310c4a690df086
Park YongJune <redplus@redplus.net>
2026-08-25 18:41:00 +0900

    Remove unnecessary usings

--- src/Hawaso/Apis/Products/ProductsController.cs
+++ src/Hawaso/Apis/Products/ProductsController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Hawaso.Apis.Products
+﻿namespace Hawaso.Apis.Products
 {
     public class ProductsController : Controller
     {
