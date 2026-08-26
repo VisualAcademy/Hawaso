a4897771fe0535bba6318e6837e24064ca60d4ac
Park YongJune <redplus@redplus.net>
2026-08-26 19:24:36 +0900

    Remove unnecessary usings

--- src/Hawaso/Infrastructures/00_Initializers/00_SystemSchemaInitializer.cs
+++ src/Hawaso/Infrastructures/00_Initializers/00_SystemSchemaInitializer.cs
@@ -1,11 +1,6 @@
 ﻿using Azunt.NoteManagement;
 using Azunt.SignInManagement;
 using Azunt.TenantSettingManagement;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Diagnostics;
 
 namespace Azunt.Web.Infrastructures.Initializers
 {
